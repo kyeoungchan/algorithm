673fc634a620cf6a1f4dd9bcc5ec4c1a145935d8
kyeoungchan <wkc1002@naver.com>
2024-01-25 23:49:20 +0900

    docs: setup for Baekjoon 17135 Castle Defense

---
+++ src/baekjoon/castledefense/Baekjoon17135_CastleDefense.java
@@ -0,0 +1,13 @@
+package baekjoon.castledefense;
+
+import java.util.*;
+import java.io.*;
+/*
+* */
+public class Baekjoon17135_CastleDefense {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.close();
+    }
+}
