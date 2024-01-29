0e57f3ca1eb2d05df08ed7d6ee1b46aed79bf6a0
kyeoungchan <wkc1002@naver.com>
2024-01-29 20:23:58 +0900

    feat: Baekjoon 17135 Castle Defense

--- src/baekjoon/castledefense/Baekjoon17135_CastleDefense.java
+++ src/baekjoon/castledefense/Baekjoon17135_CastleDefense.java
@@ -5,40 +5,29 @@ import java.io.*;
 
 public class Baekjoon17135_CastleDefense {
 
-    static int n;
-    static int m;
-    static int d;
+    static int N;
+    static int M;
+    static int D;
     static int[][] map;
-    static int[] dI = {0, -1, 0};
-    static int[] dJ = {-1, 0, 1};
+    static int[] dI = { 0, -1, 0 };
+    static int[] dJ = { -1, 0, 1 };
     static int result;
     static int[] picked = new int[3];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-/*
-        // 첫 번째 궁수가 있을 수 있는 인덱스는 0~m-3
-        for (int a1 = 0; a1 < m - 2; a1++) {
-            // 두 번째 궁수가 있을 수 있는 인덱스는 a1 + 1 ~ m - 2
-            for (int a2 = a1 + 1; a2 < m - 1; a2++) {
-                // 세 번째 궁수가 있을 수 있는 인덱스는 a2 + 1 ~ m - 1
-                for (int a3 = a2 + 1; a3 < m; a3++) {
-                    result = Math.max(result, calculateScore(a1, a2, a3));
-                }
-            }
-        }
-*/
+
         pick(0, 0);
 
         System.out.println(result);
@@ -47,23 +36,23 @@ public class Baekjoon17135_CastleDefense {
 
     static void pick(int cnt, int start) {
         if (cnt == 3) {
-            result = Math.max(result, calculateScore(picked[0], picked[1], picked[2]));
+            result = Math.max(result, calculateScore(picked));
             return;
         }
-        for (int i = start; i < m; i++) {
+        for (int i = start; i < M; i++) {
             picked[cnt] = i;
             pick(cnt + 1, i + 1);
         }
     }
 
-    static int calculateScore(int a1, int a2, int a3) {
+    static int calculateScore(int[] picked) {
         int score = 0;
-        boolean[][] killed = new boolean[n][m];
-        for (int round = 1; round <= n; round++) {
-            int lowerBound = n + 1 - round;
-            int[] killedPos1 = bfs(a1, lowerBound, killed);
-            int[] killedPos2 = bfs(a2, lowerBound, killed);
-            int[] killedPos3 = bfs(a3, lowerBound, killed);
+        boolean[][] killed = new boolean[N][M];
+        for (int round = 1; round <= N; round++) {
+            int lowerBound = N + 1 - round;
+            int[] killedPos1 = bfs(picked[0], lowerBound, killed);
+            int[] killedPos2 = bfs(picked[1], lowerBound, killed);
+            int[] killedPos3 = bfs(picked[2], lowerBound, killed);
             if (killedPos1 != null) {
                 int aI1 = killedPos1[0];
                 int aJ1 = killedPos1[1];
@@ -89,34 +78,26 @@ public class Baekjoon17135_CastleDefense {
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(killed[i]));
-        }
-        System.out.println("a1 = " + a1);
-        System.out.println("a2 = " + a2);
-        System.out.println("a3 = " + a3);
-        System.out.println("score = " + score);
-        System.out.println();
         return score;
     }
 
     // 적을 죽이는 데 성공하면 적의 좌표를, 실패하면 null 반환
     static int[] bfs(int a, int lowerBound, boolean[][] killed) {
         Deque<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
+        boolean[][] visited = new boolean[N][M];
         int nI = lowerBound - 1;
 
         // d가 1인 것과 관계없이 바로 위에 죽이지 않은 1이 있으면 바로 좌표 반환
         if (map[nI][a] == 1 && !killed[nI][a]) {
-            return new int[]{nI, a};
+            return new int[] { nI, a };
         }
 
         // d가 1이고 바로 위가 0이면 바로 null 반환
-        if (d == 1 && map[nI][a] == 0) {
+        if (D == 1 && map[nI][a] == 0) {
             return null;
         }
 
-        queue.addLast(new int[]{nI, a, 1});
+        queue.addLast(new int[] { nI, a, 1 });
         while (!queue.isEmpty()) {
             int[] polled = queue.poll();
             int polledI = polled[0];
@@ -125,18 +106,20 @@ public class Baekjoon17135_CastleDefense {
 
             for (int i = 0; i < 3; i++) {
                 int newI = polledI + dI[i];
-                if (newI < 0 || newI >= n) {
-                    continue;
-                }
                 int newJ = polledJ + dJ[i];
-                if (newJ < 0 || newJ >= m) {
+                if (newI < 0 || newI >= N || newJ < 0 || newJ >= M) {
                     continue;
                 }
                 if (map[newI][newJ] == 1 && !killed[newI][newJ]) {
-                    return new int[]{newI, newJ};
-                } else if (!visited[newI][newJ] && distance < d) {
+                    return new int[] { newI, newJ };
+                } else if (!visited[newI][newJ] && distance + 1 < D) {
+                    // 넣기 전에 체크를 하는 구조고, 이미 넣은 원소는 꺼내는 역할만 하기 때문에, distance + 1 < D여야 한다.
+                    // 만약 distance < D인 상태라면, distance = 1 => 넣을 때는 distance + 1 = 2인 상태로 넣어지고,
+                    // 그러면 newI와 newJ를 탐색하는 과정에서 distance가 3까지 올라가지만, 큐에 넣기도 전에 1에 해당하면 바로 리턴해버린다.
+                    // 큐에 넣기 전에 한 번 더 탐색해도 괜찮은 지를 판단시키는 게 맞다.
+                    // 만약 큐에서 꺼낸 값을 newI, newJ로 확장시키기 전에 kill 여부를 판단하는 로직 구조라면 distance < D 로직이 타당할 수 있다.
                     visited[newI][newJ] = true;
-                    queue.addLast(new int[]{newI, newJ, distance + 1});
+                    queue.addLast(new int[] { newI, newJ, distance + 1 });
                 }
             }
         }
