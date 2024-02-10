d001fc6defd4f928007cc50bdf96af98dde1cf99
kyeoungchan <wkc1002@naver.com>
2024-02-10 18:10:15 +0900

    fix: Baekjoon 14889 스타트와 링크 - 성능 개선 및 배운점 작성

--- src/baekjoon/startandlink/Solution_bj_14889_스타트와링크_서울_20반_우경찬.java
+++ src/baekjoon/startandlink/Solution_bj_14889_스타트와링크_서울_20반_우경찬.java
@@ -5,34 +5,35 @@ import java.util.*;
 
 public class Solution_bj_14889_스타트와링크_서울_20반_우경찬 {
 
-	static int N, min, startScore, linkScore;
-	static int[][] S;
-	static int[] startTeam, linkTeam, startPair, linkPair;
-	static boolean[] v, inStartTeam;
+    static int N, min/*, startScore, linkScore*/;
+    static int[][] S;
+    //	static int[] startTeam, linkTeam, startPair, linkPair;
+    static boolean[] /*v,*/ inStartTeam;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		S = new int[N + 1][N + 1];
-		startTeam = new int[N / 2];
-		linkTeam = new int[N / 2];
-		inStartTeam = new boolean[N + 1];
-		StringTokenizer st;
-		min = Integer.MAX_VALUE;
-		for (int i = 1; i < N + 1; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 1; j < N + 1; j++) {
-				S[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        S = new int[N + 1][N + 1];
+//		startTeam = new int[N / 2];
+//		linkTeam = new int[N / 2];
+        inStartTeam = new boolean[N + 1];
+        StringTokenizer st;
+        min = Integer.MAX_VALUE;
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j < N + 1; j++) {
+                S[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		comb(0, 1);
-		System.out.println(min);
-		br.close();
-	}
+        comb(0, 1);
+        System.out.println(min);
+        br.close();
+    }
 
-	static void comb(int cnt, int start) {
-		if (cnt == N / 2) {
+    static void comb(int cnt, int start) {
+        if (cnt == N / 2) {
+/*
 			int idx = 0;
 			for (int i = 1; i < N + 1; i++) {
 				if (!inStartTeam[i]) {
@@ -43,18 +44,31 @@ public class Solution_bj_14889_스타트와링크_서울_20반_우경찬 {
 			getTeamScore(true);
 			v = new boolean[N / 2];
 			getTeamScore(false);
-			min = Math.min(min, Math.abs(startScore - linkScore));
-			return;
-		}
+*/
+            int startScore = 0;
+            int linkScore = 0;
+            for (int i = 1; i < N; i++) {
+                for (int j = i + 1; j < N + 1; j++) {
+					if (inStartTeam[i] && inStartTeam[j]) {
+						startScore += S[i][j] + S[j][i];
+					} else if (!inStartTeam[i] && !inStartTeam[j]) {
+						linkScore += S[i][j] + S[j][i];
+					}
+                }
+            }
+            min = Math.min(min, Math.abs(startScore - linkScore));
+            return;
+        }
 
-		for (int i = start; i < N + 1; i++) {
-			inStartTeam[i] = true;
-			startTeam[cnt] = i;
-			comb(cnt + 1, i + 1);
-			inStartTeam[i] = false;
-		}
-	}
+        for (int i = start; i < N + 1; i++) {
+            inStartTeam[i] = true;
+//			startTeam[cnt] = i;
+            comb(cnt + 1, i + 1);
+            inStartTeam[i] = false;
+        }
+    }
 
+/*
 	static void getTeamScore(boolean isStartTeam) {
 		if (isStartTeam) {
 			startScore = 0;
@@ -67,7 +81,9 @@ public class Solution_bj_14889_스타트와링크_서울_20반_우경찬 {
 		}
 
 	}
+*/
 
+/*
 	static void perm(int cnt, boolean isStartTeam) {
 		if (cnt == 2) {
 			if (isStartTeam) {
@@ -91,5 +107,6 @@ public class Solution_bj_14889_스타트와링크_서울_20반_우경찬 {
 			v[i] = false;
 		}
 	}
+*/
 
 }
