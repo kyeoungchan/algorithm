2f2fe52daf536467244e1bd9c57bdab1e566db2b
kyeoungchan <wkc1002@naver.com>
2024-02-24 17:18:28 +0900

    feat: Baekjoon 17472 다리 만들기2

--- src/baekjoon/makingbridge2/Solution_bj_17472_다리만들기2_서울_20반_우경찬.java
+++ src/baekjoon/makingbridge2/Solution_bj_17472_다리만들기2_서울_20반_우경찬.java
@@ -1,14 +1,14 @@
-package swexpertacademy;
+package baekjoon.makingbridge2;
 
 import java.util.*;
 import java.io.*;
 
 public class Solution_bj_17472_다리만들기2_서울_20반_우경찬 {
 
-	static int N, M;
+	static int N, M, INF = Integer.MAX_VALUE;
 	static int[] di = {-1, 0, 1, 0}, dj = {0, 1, 0, -1}, parents;
 	static int[][] map, g;
-	static List<int[]> islandPos;
+	static List<int[]> islandPos, edges;
 	static boolean[][] v;
 	
 	static void make() {
@@ -28,7 +28,7 @@ public class Solution_bj_17472_다리만들기2_서울_20반_우경찬 {
 		
 		if (aRoot == bRoot) return false;
 		
-		if (a < b) parents[bRoot] = aRoot;
+		if (aRoot < bRoot) parents[bRoot] = aRoot;
 		else parents[aRoot] = bRoot;
 		return true;
 	}
@@ -63,7 +63,7 @@ public class Solution_bj_17472_다리만들기2_서울_20반_우경찬 {
 				map[i][j] = islandId;
 				checkIsland[i][j] = true;
 				q.offer(new int[] {i, j});
-				
+
 				while (!q.isEmpty()) {
 					int[] cur = q.poll();
 					for (int d = 0; d < 4; d++) {
@@ -78,20 +78,52 @@ public class Solution_bj_17472_다리만들기2_서울_20반_우경찬 {
 				islandId++;
 			}
 		}
-		g = new int[islandId-1][islandId-1];
-		for (int i = 0; i < islandId - 1; i++) {
+
+		int islandCnt = islandId - 1;
+		// 섬이 하나면 그 섬의 islandId = 1, 최종 islandId = 2인 상태로 반복문 탈출
+		makeGraph(islandCnt);
+		make();
+		int result = 0;
+		Collections.sort(edges, Comparator.comparingInt(edges -> edges[2]));
+		for (int[] edge : edges) {
+			if (union(edge[0], edge[1])) {
+				result += edge[2];
+			}
+		}
+		if (result == 0 || notAllIslandConnected()) result = -1;
+		System.out.println(result);
+		br.close();
+	}
+
+	static boolean notAllIslandConnected() {
+		for (int i = 1; i < parents.length; i++) {
+			if (find(i) != 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void makeGraph(int islandCnt) {
+		ArrayDeque<int[]> q;
+		g = new int[islandCnt + 1][islandCnt + 1];
+		for (int[] islandArr : g) {
+			Arrays.fill(islandArr, INF);
+		}
+
+		for (int i = 0; i < islandCnt; i++) {
+			// i == id - 1
 			v = new boolean[N][M];
 			q = new ArrayDeque<>();
 			int startI = islandPos.get(i)[0];
 			int startJ = islandPos.get(i)[1];
 			int id = i + 1;
 			v[startI][startJ] = true;
-		
+
 			for (int d = 0; d < 4; d++) {
 				int ni = startI + di[d];
 				int nj = startJ + dj[d];
-				if (ni < 0 || ni >= N || nj < 0 || nj >= M || v[ni][nj]) continue;
-				v[ni][nj] = true;
+				if (ni < 0 || ni >= N || nj < 0 || nj >= M) continue;
 				q.offer(new int[] {ni, nj, 0, d}); // 좌표들, 0의 개수, 바다일 경우 방향 인덱스
 			}
 
@@ -100,27 +132,39 @@ public class Solution_bj_17472_다리만들기2_서울_20반_우경찬 {
 				int ci = cur[0];
 				int cj = cur[1];
 				if (!v[ci][cj]) {
-					v[ci][cj] = true;
 					if (map[ci][cj] == id) {
+						v[ci][cj] = true;
 						for (int d = 0; d < 4; d++) {
 							int ni = ci + di[d];
 							int nj = cj + dj[d];
 							if (ni < 0 || ni >= N || nj < 0 || nj >= M || v[ni][nj]) continue;
-							v[ni][nj] = true;
 							q.offer(new int[] {ni, nj, 0, d});
 						}
 					} else if (map[ci][cj] == 0) {
 						int d = cur[3];
 						int ni = ci + di[d];
 						int nj = cj + dj[d];
-						
+						if (ni < 0 || ni >= N || nj < 0 || nj >= M) continue;
+						// 바다에서는 visited를 신경쓰지 않는다. 오히려 다른 경로를 방해할 수 있고, 사방 탐색이 아니라 일자 탐색이기 때문에 무한 루프가 생기지 않는다.
+						q.offer(new int[]{ni, nj, cur[2] + 1, d});
 					} else {
-						
+						int cost = cur[2];
+						if (cost > 1) {
+							g[i + 1][map[ci][cj]] = Math.min(cost, g[i + 1][map[ci][cj]]);
+							g[map[ci][cj]][i + 1] = Math.min(cost, g[map[ci][cj]][i + 1]);
+						}
 					}
 				}
 			}
 		}
-		br.close();
+		edges = new ArrayList<>();
+		for (int ii = 1; ii < islandCnt + 1; ii++) {
+			for (int j = ii + 1; j < islandCnt + 1; j++) {
+				if (g[ii][j] != INF) {
+					edges.add(new int[]{ii, j, g[ii][j]});
+				}
+			}
+		}
 	}
 
 }
