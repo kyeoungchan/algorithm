eb0503fc9915c4646f4161459035ccd5e87f1f7c
kyeoungchan <wkc1002@naver.com>
2024-02-28 22:50:56 +0900

    docs: 보물상자 비밀번호, 점심식사 시간, 미생물 격리 주석 추가

--- src/swexpertacademy/Solution_d9_2382_미생물격리_서울_20반_우경찬.java
+++ src/swexpertacademy/Solution_d9_2382_미생물격리_서울_20반_우경찬.java
@@ -27,11 +27,12 @@ public class Solution_d9_2382_미생물격리_서울_20반_우경찬 {
             boolean[] needUpdate = new boolean[K + 1];
             for (int id = 1; id < K + 1; id++) {
                 st = new StringTokenizer(br.readLine(), " ");
+                // 미생물의 좌표
                 microorganisms[id][0] = Integer.parseInt(st.nextToken());
                 microorganisms[id][1] = Integer.parseInt(st.nextToken());
-                map[microorganisms[id][0]][microorganisms[id][1]] = id;
+                map[microorganisms[id][0]][microorganisms[id][1]] = id; // 지도에 미생물의 id 찍어놓기
                 microorganisms[id][2] = Integer.parseInt(st.nextToken()); // 미생물의 수
-                totalMic += microorganisms[id][2];
+                totalMic += microorganisms[id][2]; // 총 미생물의 수
                 microorganisms[id][3] = Integer.parseInt(st.nextToken()); // 이동방향
             }
 
@@ -39,21 +40,24 @@ public class Solution_d9_2382_미생물격리_서울_20반_우경찬 {
 
             for (int time = 0; time < M; time++) {
                 for (int id = 1; id < K + 1; id++) {
-                    if (isMerged[id]) continue;
+                    // 이동에 관한 반복문
+                    if (isMerged[id]) continue; // 합병된 미생물에 대해서는 건너뛴다.
                     int pi = microorganisms[id][0];
                     int pj = microorganisms[id][1];
-                    map[pi][pj] = 0;
+                    map[pi][pj] = 0; // 일단 지도에서 지우기만 하고 지도에 다시 id를 채워넣는 작업은 아래에서 수행한다.
                     int d = microorganisms[id][3];
                     int ni = pi + di[d];
                     int nj = pj + dj[d];
-                    int nCnt = microorganisms[id][2];
-                    int nd = d;
+                    int nCnt = microorganisms[id][2]; // 일단 new Count를 현재와 동이랗게 한다.
+                    int nd = d; // 방향도 일단 동일하게 한다.
 
                     if (ni == 0 || ni == N - 1 || nj == 0 || nj == N - 1) {
-                        nCnt /= 2;
-                        totalMic -= (microorganisms[id][2] - nCnt);
-                        nd = d % 2 == 0 ? d - 1 : d + 1;
+                        // 만약 약을 친 곳으로 갔다면
+                        nCnt /= 2; // 절반으로 줄이고,
+                        totalMic -= (microorganisms[id][2] - nCnt); // 이전의 한 집단의 미생물의 수에서 절반을 뺀 값만큼 totalMic에서 뺀다.
+                        nd = d % 2 == 0 ? d - 1 : d + 1; // 방향이 바뀌어야하는데, 1 -> 2, 2 -> 1 / 3 -> 4, 4 -> 3으로 바뀌어야 한다.
                     }
+                    // 바뀐값들 전원 세팅
                     microorganisms[id][0] = ni;
                     microorganisms[id][1] = nj;
                     microorganisms[id][2] = nCnt;
@@ -61,21 +65,26 @@ public class Solution_d9_2382_미생물격리_서울_20반_우경찬 {
                 }
 
                 for (int id = 1; id < K + 1; id++) {
-                    if (isMerged[id]) continue;
+                    // 병합에 관한 반복문
+                    if (isMerged[id]) continue; // 합병된 집단은 건너뛴다.
                     int i = microorganisms[id][0];
                     int j = microorganisms[id][1];
-                    tempCnt[id] = microorganisms[id][2];
+                    tempCnt[id] = microorganisms[id][2]; // 일단 현재 집단의 수를 임시 저장한다.
                     if (map[i][j] == 0) {
+                        // 아직 그 자리에 다른 집단이 없다면 그 자리에 세팅한다.
                         map[i][j] = id;
                     } else {
+                        // 그 자리에 다른 집단이 있다면
                         int existingId = map[i][j];
                         int totalCnt = tempCnt[existingId] + tempCnt[id];
+                        // 최종적으로 합쳐질 수를 저장한다.
 
                         if (microorganisms[existingId][2] < microorganisms[id][2]) {
+                            // 원래 왔던 애들보다 새로 온 애들이 더 많다면
                             map[i][j] = id;
-                            tempCnt[id] = totalCnt;
+                            tempCnt[id] = totalCnt; // 아직 뒤에 더 오는 애들이 있기 때문에, 합병된 수는 따로 보관해야 한다. 예를 들어 1, 2 중 경쟁해서 2가 자리를 차지했더라도 아직 반복문이 끝나기 전까지는 동일 시간대이기 때문에, 3이 왔을 때 1과 2가 합쳐진 것보다 더 작은 것으로 계산이 되면 안 된다. 즉, 1, 2, 3이 합쳐지면 3이 차지해야지, 2가 차지하면 안 된다는 뜻이다.
                             isMerged[existingId] = true;
-                            needUpdate[id] = true;
+                            needUpdate[id] = true; // 위에 적은 주석을 위해서 업데이트가 나중에 필요함을 알린다.
                         } else {
                             tempCnt[existingId] = totalCnt;
                             isMerged[id] = true;
@@ -85,7 +94,7 @@ public class Solution_d9_2382_미생물격리_서울_20반_우경찬 {
                 }
 
                 for (int id = 1; id < K + 1; id++) {
-                    if (isMerged[id] || !needUpdate[id]) continue;
+                    if (isMerged[id] || !needUpdate[id]) continue; // 합병되지도 않았고, 업데이트가 필요하다면
                     microorganisms[id][2] = tempCnt[id];
                     needUpdate[id] = false;
                 }
--- src/swexpertacademy/Solution_d9_5658_보물상자비밀번호_서울_20반_우경찬.java
+++ src/swexpertacademy/Solution_d9_5658_보물상자비밀번호_서울_20반_우경찬.java
@@ -17,23 +17,23 @@ public class Solution_d9_5658_보물상자비밀번호_서울_20반_우경찬 {
 			int K = Integer.parseInt(st.nextToken());
 			String s = br.readLine();
 			String[] pars = new String[N];
-			for (int i = 0; i < N; i++)
+			for (int i = 0; i < N; i++) // 한 글자씩 pars에 담는다.
 				pars[i] = s.substring(i, i+1);
-			int len = N / 4;
+			int len = N / 4; // 한 숫자당 길이다.
 			List<NumInfo> numbers = new ArrayList<>();
-			StringBuilder tempSb = new StringBuilder();
+			StringBuilder tempSb = new StringBuilder(); // 문자열들을 더하고 빼주기 위한 StringBuilder
 			for (int i = 0; i < len; i++) {
-				tempSb.append(pars[i]);
+				tempSb.append(pars[i]); // 글자의 길이만큼 한 글자씩 넣어준다.
 			}
-			numbers.add(new NumInfo(tempSb.toString()));
+			numbers.add(new NumInfo(tempSb.toString())); // numbers리스트에 NumInfo 객체를 담는다.
 			for (int i = 1; i < N; i++) {
-				int endI = i + len - 1;
+				int endI = i + len - 1; // len 길이만큼의 숫자 중 마지막 인덱스를 담는다.
 				if (endI > N - 1) endI = endI - N;
-				tempSb.deleteCharAt(0);
-				tempSb.append(pars[endI]);
+				tempSb.deleteCharAt(0); // 맨 앞의 글자를 뺀다.
+				tempSb.append(pars[endI]); // 마지막에 다른 글자를 붙인다.
 				boolean isDupl = false;
 				NumInfo numInfo = new NumInfo(tempSb.toString());
-				for (NumInfo n : numbers) {
+				for (NumInfo n : numbers) { // 중복 여부 확인
 					if (n.equals(numInfo)) {
 						isDupl = true;
 						break;
@@ -42,15 +42,16 @@ public class Solution_d9_5658_보물상자비밀번호_서울_20반_우경찬 {
 				if (!isDupl)
 					numbers.add(numInfo);
 			}
-			int ans = 0;
-			int idx = 0;
-			Collections.sort(numbers, Comparator.comparingInt(n -> -n.hex));
-			for (NumInfo n : numbers) {
+//			int ans = 0;
+//			int idx = 0;
+			Collections.sort(numbers, Comparator.comparingInt(n -> -n.hex)); // 내림차순 정렬
+/*			for (NumInfo n : numbers) {
 				if (idx++ == K - 1) {
-					ans = n.hex;
+					ans = n.hex; // 찾는 값의 인덱스 반
 					break;
 				}
-			}
+			}*/
+			int ans = numbers.get(K - 1).hex;
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
 		System.out.println(sb.toString());
@@ -62,6 +63,7 @@ public class Solution_d9_5658_보물상자비밀번호_서울_20반_우경찬 {
 		int hex;
 		
 		NumInfo(String symbol) {
+			// 숫자를 문자열로 받으면, 그 값을 16진수 -> 10진수로 해독한다.
 			this.symbol = symbol;
 			hex = Integer.parseInt(symbol, 16);
 		}
