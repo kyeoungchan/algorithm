4f26869bac2a85fcb866a8b4b8b9e2bcc09f82fd
kyeoungchan <wkc1002@naver.com>
2024-03-01 20:53:18 +0900

    feat: baekjoon 외판원 순회2

---
+++ src/baekjoon/Solution_bj_11559_PuyoPuyo_서울_20반_우경찬.java
@@ -0,0 +1,94 @@
+package a0229;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution_bj_11559_PuyoPuyo_서울_20반_우경찬 {
+	
+	static int[] di = new int[] {-1, 0, 1, 0}, dj = {0, 1, 0, -1};
+	static char[][] map;
+	static boolean hasPuyo;
+	static List<int[]> puyoPos;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		map = new char[12][6];
+		for (int i = 0; i < 12; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < 6; j++) {
+				map[i][j] = s.charAt(j);
+			}
+		}
+		int result = 0;
+		hasPuyo = true;
+		while (hasPuyo) {
+			hasPuyo = false;
+			ArrayDeque<int[]> q = new ArrayDeque<>();
+			boolean[][] v = new boolean[12][6];
+			List<int[]> popPos = new ArrayList<>();
+			for (int i = 0; i < 12; i++) {
+				for (int j = 0; j < 6; j++) {
+					if (map[i][j] == '.' || v[i][j]) continue;
+					v[i][j] = true;
+					q.offer(new int[] {i, j});
+					int cnt = 1;
+					char c = map[i][j];
+					while (!q.isEmpty()) {
+						int[] cur = q.poll();
+						for (int d = 0; d < 4; d++) {
+							int ni = cur[0] + di[d];
+							int nj = cur[1] + dj[d];
+							if (ni < 0 || ni > 11 || nj < 0 || nj > 5 || map[ni][nj] != c || v[ni][nj]) continue;
+							v[ni][nj] = true;
+							q.offer(new int[] {ni, nj});
+							cnt++;
+						}
+					}
+					if (cnt > 3)  {
+						if (!hasPuyo)
+							result++;
+						hasPuyo = true;
+						popPos.add(new int[] {i, j});
+					}
+				}
+			}
+			q.clear();
+			for (int[] pos : popPos) {
+				char c = map[pos[0]][pos[1]];
+				map[pos[0]][pos[1]] = '.';
+				q.offer(new int[] {pos[0], pos[1]});
+				while(!q.isEmpty()) {
+					int[] cur = q.poll();
+					for (int d = 0; d < 4; d++) {
+						int ni = cur[0] + di[d];
+						int nj = cur[1] + dj[d];
+						if (ni < 0 || ni > 11 || nj < 0 || nj > 5 || map[ni][nj] != c) continue;
+						q.offer(new int[] {ni, nj});
+						map[ni][nj] = '.';
+					}
+				}
+			}
+			drop();
+//			for(int i = 0; i < 12; i++)
+//			System.out.println(Arrays.toString(map[i]));
+//			System.out.println();
+		}
+		System.out.println(result);
+		br.close();
+	}
+	
+	static void drop() {
+		for (int j = 0; j < 6; j++) {
+			for (int i = 11; i > 0; i--) {
+				if (map[i][j] == '.' && map[i-1][j] != '.') {
+					char temp = map[i][j];
+					map[i][j] = map[i-1][j];
+					map[i-1][j] = temp;
+					i = 12;
+				}
+			}
+		}
+	}
+	
+	
+}
---
+++ src/baekjoon/travelsalesmanproblem2/Solution_bj_10971_외판원순회2_서울_20반_우경찬.java
@@ -0,0 +1,51 @@
+package baekjoon.travelsalesmanproblem2;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution_bj_10971_외판원순회2_서울_20반_우경찬 {
+	
+	static int N, result;
+	static int[][] W;
+	static boolean[] v;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		W = new int[N + 1][N + 1];
+		for(int i = 1; i < N + 1; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 1; j < N + 1; j++) {
+				W[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		result = Integer.MAX_VALUE;
+		v = new boolean[N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			dfs(i, i, 0, 0);
+		}
+		System.out.println(result);
+		br.close();
+	}
+
+	static void dfs(int start, int from, int cnt, int totalCost) {
+		if (cnt == N - 1) {
+			// 마지막 원소는 더이상 갈 곳이 start 말고는 없으므로 cnt==N이 아니라 cnt==N-1인 경우에 start로 가야한다.
+			if (W[from][start] != 0)
+				result = Math.min(result, totalCost + W[from][start]);
+			return;
+		}
+		
+		
+		v[from] = true;
+		for (int i = 1; i < N + 1; i++) {
+			if (!v[i] && W[from][i] != 0) {
+				dfs(start, i, cnt + 1, totalCost + W[from][i]);
+			}
+		}
+		v[from] = false;
+	}
+
+}
