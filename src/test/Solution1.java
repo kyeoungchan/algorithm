c08db5a019d660e7b6d8853d18a0f9332b14506e
kyeoungchan <wkc1002@naver.com>
2024-03-05 22:15:58 +0900

    feat: A형 테스트 2문제, baekjoon 1600 말이 되고픈 원숭이, 11048 이동하기

---
+++ src/test/Solution1.java
@@ -0,0 +1,90 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+
+class Solution1 {
+	
+	static int N, M, ANS, gI, gJ;
+	static int[] di = {-1, 0, 1, 0}, dj = {0, 1, 0, -1};
+	static int[][] map, count;
+	static boolean[][][] v;
+	
+	public static void main(String args[]) throws Exception	{
+		System.setIn(new FileInputStream("res/input1.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < T + 1; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			map = new int[N][M];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < M; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 3) {
+						gI = i;
+						gJ = j;
+					}
+				}
+			}
+			int startI = N - 1;
+			int startJ = 0;
+			ANS = Integer.MAX_VALUE;
+			v = new boolean[N][M][N];
+			count = new int[N][M];
+
+			v[startI][startJ][0] = true;
+			bfs();
+//			dfs(startI, startJ, 0);
+			sb.append("#").append(tc).append(" ").append(ANS).append("\n");
+		}
+		System.out.println(sb.toString());
+		br.close();		
+	}
+	
+	static void bfs() {
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {N - 1, 0, 0});
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int i = cur[0];
+			int j = cur[1];
+			int hardScore = cur[2];
+			if (hardScore > ANS) continue;
+			if (i == gI && j == gJ) {
+				ANS = Math.min(ANS, hardScore);
+				continue;
+			}
+			
+			for (int d = 0; d < 4; d++) {
+				if (d % 2 == 0) {
+					// 상하
+					int k = 1;
+					while (true) {
+						int ni = i + di[d] * k;
+						int nHardScore = Math.max(hardScore, k);
+						if (ni < 0 || ni >= N || v[ni][j][nHardScore]) break;
+						if (map[ni][j] != 0) {
+							v[ni][j][nHardScore] = true;
+							q.offer(new int[] {ni, j, nHardScore});
+							break;
+						}
+						k++;
+					}
+				} else {
+					// 좌우
+					int nj = j + dj[d];
+					if (nj < 0 || nj >= M || map[i][nj] == 0 || v[i][nj][hardScore]) continue;
+					v[i][nj][hardScore] = true;
+					q.offer(new int[] {i, nj, hardScore});
+				}
+				
+			}
+		}
+	}
+	
+}
---
+++ src/test/Solution2.java
@@ -0,0 +1,127 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution2 {
+
+	static int N, ANS;
+	static int[][] houseInfo, chargeInfo;
+	static List<Integer>[][] idMap;
+	static int[][] cntMap, distanceMap;
+	static int[] di = {-1, 0, 1, 0}, dj = {0, 1, 0, -1};
+	static boolean[] charge;
+	
+	public static void main(String[] args) throws Exception {
+	
+		System.setIn(new FileInputStream("res/input2.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+	
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < T + 1; tc++) {
+			N = Integer.parseInt(br.readLine());
+			houseInfo = new int[N + 1][3];
+			for (int i = 1; i < N + 1; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				houseInfo[i][0] = Integer.parseInt(st.nextToken()) + 15;
+				houseInfo[i][1] = Integer.parseInt(st.nextToken()) + 15;
+				houseInfo[i][2] = Integer.parseInt(st.nextToken());
+			}
+			idMap = new List[31][31];
+			cntMap = new int[31][31];
+			distanceMap = new int[31][31];
+			
+			int max = 0;
+			List<int[]> maxPoses = new ArrayList<>();
+			for (int i = 0; i < 31; i++) {
+				for (int j = 0; j < 31; j++) {
+					for (int id = 1; id < N + 1; id++) {
+						int hi = houseInfo[id][0];
+						int hj = houseInfo[id][1];
+						int dist = getDistance(hi, hj, i, j);
+						if (dist > 0 && dist <= houseInfo[id][2]) {
+							if (idMap[i][j] == null) {
+								idMap[i][j] = new ArrayList<>();
+							}
+							idMap[i][j].add(id);
+							cntMap[i][j]++;
+							distanceMap[i][j] += dist;
+							if (max < cntMap[i][j]) {
+								max = cntMap[i][j];
+								maxPoses.clear();
+								maxPoses.add(new int[] {i, j});
+							} else if (max == cntMap[i][j]) {
+								maxPoses.add(new int[] {i, j});
+							}
+						}
+					}
+				}
+			}
+			ANS = Integer.MAX_VALUE;
+			if (max == N) {
+				// 하나의 충전소로 모든 집이 충전 가능한 경우
+				for (int i = 0; i < maxPoses.size(); i++) {
+					int ni = maxPoses.get(i)[0];
+					int nj = maxPoses.get(i)[1];
+					ANS = Math.min(ANS, distanceMap[ni][nj]);
+				}
+			} else {
+				for (int i = 0; i < maxPoses.size(); i++) {
+					int ni = maxPoses.get(i)[0];
+					int nj = maxPoses.get(i)[1];
+					List<Integer> ids = idMap[ni][nj];
+					charge = new boolean[N + 1];
+					for (int id : ids) {
+						charge[id] = true;
+					}
+					boolean sndPossible = true;
+					
+					for (int ii = 0; ii < 31; ii++) {
+						for (int jj = 0; jj < 31; jj++) {
+							if (idMap[ii][jj] == null) continue;
+							sndPossible = true;
+							for (int id = 1; id < N + 1; id++) {
+								if (!charge[id]) {
+									if (!idMap[ii][jj].contains(id)) {
+										sndPossible = false;
+										break;
+									}
+								}
+							}
+							if (sndPossible) {
+								int temp = 0;
+								for (int id = 1; id < N + 1; id++) {
+									int hi = houseInfo[id][0];
+									int hj = houseInfo[id][1];
+									int a = getDistance(ni, nj, hi, hj);
+									int b = getDistance(ii, jj, hi, hj);
+									if (a != 0 && b != 0) temp += Math.min(a, b);
+									else if (a == 0) temp += b;
+									else if (b == 0) temp += a;
+								}
+								ANS = Math.min(ANS, temp);
+							}
+						}
+					}
+					
+					for (int id : ids) {
+						charge[id] = false;
+					}
+				}
+				if (ANS == Integer.MAX_VALUE) ANS = -1;
+			}
+			sb.append("#").append(tc).append(" ").append(ANS).append("\n");
+			
+		}
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+	static int getDistance(int fromI, int fromJ, int toI, int toJ) {
+		return Math.abs(fromI - toI) + Math.abs(fromJ - toJ);
+	}
+
+}
