fa6577fd7bdf907fa0f23cd4a7421469018963e9
kyeoungchan <wkc1002@naver.com>
2024-03-28 21:10:59 +0900

    Baekjoon - 12865 평범한 배낭

--- src/baekjoon/knapsack/Solution_bj_12865_평범한배낭_서울_20반_우경찬.java
+++ src/baekjoon/knapsack/Solution_bj_12865_평범한배낭_서울_20반_우경찬.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.knapsack;
 
 import java.util.*;
 import java.io.*;
@@ -16,17 +16,27 @@ public class Solution_bj_12865_평범한배낭_서울_20반_우경찬 {
 			items[i][0] = Integer.parseInt(st.nextToken());
 			items[i][1] = Integer.parseInt(st.nextToken());
 		}
+		int[] dp = new int[K + 1];
+		for (int i = 0; i < N; i++) {
+			int wi = items[i][0];
+			int vi = items[i][1];
+			for (int w = K; w > wi - 1; w--) {
+				dp[w] = Math.max(dp[w - wi] + vi, dp[w]);
+			}
+		}
+		System.out.println(dp[K]);
+/*
 		int[][] dp = new int[N][K + 1];
-		
+
 		int tempW = items[0][0];
 		int tempV = items[0][1];
-		
+
 		for (int w = 1; w < K + 1; w++) {
 			if (tempW <= w) {
 				dp[0][w] = tempV;
 			}
 		}
-		
+
 		for (int i = 1; i < N; i++) {
 			// 아이템 선택
 			int wi = items[i][0];
@@ -40,6 +50,7 @@ public class Solution_bj_12865_평범한배낭_서울_20반_우경찬 {
 			}
 		}
 		System.out.println(dp[N-1][K]);
+*/
 		br.close();
 	}
 
