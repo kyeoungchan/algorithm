4afc5e59126857d5555d3f54146ce622a1708be3
kyeoungchan <wkc1002@naver.com>
2024-03-28 22:48:09 +0900

    feat: Baekjoon - 14890 경사로

--- src/baekjoon/ramp/Solution_bj_14890_경사로_서울_20반_우경찬.java
+++ src/baekjoon/ramp/Solution_bj_14890_경사로_서울_20반_우경찬.java
@@ -22,17 +22,17 @@ public class Solution_bj_14890_경사로_서울_20반_우경찬 {
         for (int i = 0; i < N; i++) {
             hasBuilt = new boolean[N];
             allSucceed = true;
-            flag: for (int j = 0; j < N - 1; j++) {
+            end: for (int j = 0; j < N - 1; j++) {
                 if (Math.abs(map[i][j] - map[i][j + 1]) > 1) {
                     allSucceed = false;
                     break;
                 } else if (map[i][j] + 1 == map[i][j + 1]) {
                     int lower = map[i][j];
-                    for (int k = 1; k < L; k++) {
-                        int idx = j-k;
-                        if (map[i][idx] != lower || hasBuilt[idx]) {
+                    for (int k = 0; k < L; k++) {
+                        int idx = j - k;
+                        if (idx < 0 || map[i][idx] != lower || hasBuilt[idx]) {
                             allSucceed = false;
-                            break flag;
+                            break end;
                         }
                     }
                     for (int k = 0; k < L; k++) {
@@ -42,7 +42,15 @@ public class Solution_bj_14890_경사로_서울_20반_우경찬 {
                 } else if (map[i][j] == map[i][j + 1] + 1) {
                     int lower = map[i][j + 1];
                     for (int k = 0; k < L; k++) {
-                        
+                        int idx = j + 1 + k;
+                        if (idx > N - 1 || map[i][idx] != lower || hasBuilt[idx]) {
+                            allSucceed = false;
+                            break end;
+                        }
+                    }
+                    for (int k = 0; k < L; k++) {
+                        int idx = j + 1 + k;
+                        hasBuilt[idx] = true;
                     }
                 }
             }
@@ -50,6 +58,47 @@ public class Solution_bj_14890_경사로_서울_20반_우경찬 {
                 ANS--;
             }
         }
+
+        for (int j = 0; j < N; j++) {
+            hasBuilt = new boolean[N];
+            allSucceed = true;
+            end: for (int i = 0; i < N - 1; i++) {
+                if (Math.abs(map[i][j] - map[i + 1][j]) > 1) {
+                    allSucceed = false;
+                    break;
+                } else if (map[i][j] + 1 == map[i + 1][j]) {
+                    int lower = map[i][j];
+                    for (int k = 0; k < L; k++) {
+                        int idx = i - k;
+                        if (idx < 0 || map[idx][j] != lower || hasBuilt[idx]) {
+                            allSucceed = false;
+                            break end;
+                        }
+                    }
+                    for (int k = 0; k < L; k++) {
+                        int idx = i - k;
+                        hasBuilt[idx] = true;
+                    }
+                } else if (map[i][j] == map[i + 1][j] + 1) {
+                    int lower = map[i + 1][j];
+                    for (int k = 0; k < L; k++) {
+                        int idx = i + 1 + k;
+                        if (idx > N - 1 || map[idx][j] != lower || hasBuilt[idx]) {
+                            allSucceed = false;
+                            break end;
+                        }
+                    }
+                    for (int k = 0; k < L; k++) {
+                        int idx = i + 1 + k;
+                        hasBuilt[idx] = true;
+                    }
+                }
+            }
+            if (!allSucceed) {
+                ANS--;
+            }
+        }
+        System.out.println(ANS);
         br.close();
     }
 }
