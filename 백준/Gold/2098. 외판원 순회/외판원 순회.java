d42e68f8faaee9ddeea6b9e350864faa3a6bc3b6
kyeoungchan <wkc1002@naver.com>
2024-04-01 22:52:00 +0900

    [Gold I] Title: 외판원 순회, Time: 152 ms, Memory: 17108 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,57 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int n, statusFullBit, INF = 987654321;
-	static int[][] w;
-	static int[][] dp;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		n = Integer.parseInt(br.readLine());
-		statusFullBit = (1<<n) -1;
-		w = new int[n][n];
-		dp = new int[n][statusFullBit];
-		for(int i=0; i<n; i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<n; j++) {
-				w[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		System.out.println(tsp(0,1)); // 0번도시 부터 탐색 시작 (check: 0001)
-	}
-	
-	static int tsp(int x, int check) {
-        
-		// 모든 도시 방문 완료
-		if(check == statusFullBit) {
-			if(w[x][0] == 0) return INF; // 경로 없으면 INF로 탐색 무효화 (Math.min)
-			else return w[x][0]; // 경로가 존재하면 w[x][0]
-		}
-        
-		// 이미 방문한 도시 
-		if(dp[x][check] != -1) return dp[x][check];
-        
-		// 해당 도시에 출석 표시
-		dp[x][check] = INF;
-        
-		// 방문하지 않은 도시 탐색 
-		for(int i=0; i<n; i++) {
-			// next : i 도시 방문
-			int next = check | (1<<i); 
-            
-			// 경로가 없거나 i 도시를 이미 방문했을 경우 continue 
-			if(w[x][i] ==0 || (check & (1<<i)) != 0) continue;
-			
-			dp[x][check] = Math.min(dp[x][check], tsp(i, next) + w[x][i]);
-		}
-		
-		return dp[x][check];
-	}
+    static int N, W[][], dp[][], ANS, INF = 987654321;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        W = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                W[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[N][1 << N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        ANS = Integer.MAX_VALUE;
+
+        // 현재 노드, 현재까지 가중치의 합, 현재 방문한 노드들
+        System.out.println(travel(0, 1));
+        br.close();
+    }
+
+    static int travel(int city, int visited) {
+        if (visited == (1 << N) - 1) {
+            if (W[city][0] == 0) {
+                return INF;
+            }
+            return W[city][0];
+        }
+
+        if (dp[city][visited] != -1) {
+            return dp[city][visited];
+        }
+
+        dp[city][visited] = INF;
+
+        for (int i = 0; i < N; i++) {
+            if (W[city][i] == 0 || (visited & (1 << i)) != 0) continue;
+            dp[city][visited] = Math.min(dp[city][visited], travel(i, visited | (1 << i)) + W[city][i]);
+        }
+        return dp[city][visited];
+    }
 }
