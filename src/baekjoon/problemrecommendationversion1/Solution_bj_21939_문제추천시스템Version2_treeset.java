5de7ab0479d511f4ec4638dd0aa98a4020503062
kyeoungchan <wkc1002@naver.com>
2024-04-06 11:05:40 +0900

    feat: Baekjoon - 문제 추천 시스템 Version1 TreeSet 사용

--- src/baekjoon/problemrecommendationversion1/Solution_bj_21939_문제추천시스템Version2_treeset.java
+++ src/baekjoon/problemrecommendationversion1/Solution_bj_21939_문제추천시스템Version2_treeset.java
@@ -1,9 +1,9 @@
 package baekjoon.problemrecommendationversion1;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Solution_bj_21939_문제추천시스템Version1 {
+public class Solution_bj_21939_문제추천시스템Version2_treeset {
 	
 	static class Node implements Comparable<Node> {
 		int p;
@@ -39,18 +39,14 @@ public class Solution_bj_21939_문제추천시스템Version1 {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		HashMap<Node, Boolean> solved = new HashMap<>();
+		TreeSet<Node> recommendList = new TreeSet<>();
 		HashMap<Integer, Node> index = new HashMap<>();
-		PriorityQueue<Node> pqHard = new PriorityQueue<>(Comparator.reverseOrder());
-		PriorityQueue<Node> pqEasy = new PriorityQueue<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int p = Integer.parseInt(st.nextToken());
 			int l = Integer.parseInt(st.nextToken());
 			Node n = new Node(p, l);
-			pqHard.offer(n);
-			pqEasy.offer(n);
-			solved.put(n, false);
+			recommendList.add(n);
 			index.put(p, n);
 		}
 		
@@ -62,30 +58,17 @@ public class Solution_bj_21939_문제추천시스템Version1 {
 				int p = Integer.parseInt(st.nextToken());
 				int l = Integer.parseInt(st.nextToken());
 				Node n = new Node(p, l);
-				solved.put(n, false);
-				pqHard.offer(n);
-				pqEasy.offer(n);
+				recommendList.add(n);
 				index.put(p, n);
 			} else if (op.equals("recommend")) {
 				int select = Integer.parseInt(st.nextToken());
-				if (select == 1) {
-					Node rec = pqHard.poll();
-					while (solved.get(rec)) {
-						rec = pqHard.poll();
-					}
-					pqHard.offer(rec);
-					sb.append(rec.p).append("\n");
-				} else {
-					Node rec = pqEasy.poll();
-					while (solved.get(rec)) {
-						rec = pqEasy.poll();
-					}
-					pqEasy.offer(rec);
-					sb.append(rec.p).append("\n");
-				}
+				Node rec;
+				if (select == 1) rec = recommendList.last();
+				else rec = recommendList.first();
+				sb.append(rec.p).append("\n");
 			} else {
 				int p = Integer.parseInt(st.nextToken());
-				solved.put(index.get(p), true);
+				recommendList.remove(index.get(p));
 			}
 		}
 		System.out.println(sb.toString());
---
+++ src/swexpertacademy/treasurechest/Solution_d9_5658_보물상자비밀번호2.java
@@ -0,0 +1,2 @@
+package swexpertacademy.treasurechest;public class Solution_d9_5658_보물상자비밀번호2 {
+}
