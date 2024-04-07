0337cc6514af7ba87f4e55e9c4c3dd7d68abf666
kyeoungchan <wkc1002@naver.com>
2024-04-07 15:56:35 +0900

    feat: Baekjoon - 23290 마법사 상어와 복제

--- src/baekjoon/마법사상어와복제/Solution_bj_23290_마법사상어와복제.java
+++ src/baekjoon/마법사상어와복제/Solution_bj_23290_마법사상어와복제.java
@@ -33,9 +33,11 @@ import java.io.*;
  *
  * 해결 프로세스: 리스트를 사용하기에는 물고기가 너무 많을 수 있으니 격자의 범위가 적은 이점을 살려서 이차원배열을 사용하도록 한다.
  * 물고기 객체를 사용하자.
- *  - 물고기객체에는 식별자 id를 부여한다.
- *      - 잡아먹힌 물고기는 멤버 변수 boolean dead를 통해서 죽었음을 표시한다.
- *  - 배열에는 리스트를 넣어서, 한 칸에 존재하는 물고기 객체의 id를 담는다.
+ *  - 배열에는 리스트를 넣어서, 한 칸에 존재하는 물고기 객체의 방향정보(d)를 담는다.
+ *      - 복제리스트는 매번 배열을 탐색하며 물고기 객체로 담아준다.
+ *      - 그리고 복제할 때마다 배열을 초기화시켜준다.
+ *      - 이동은 복제해놓은 리스트에서 꺼내서 쓴다.
+ *          - 여기에서 주의할 점은 객체는 건들지 말고 지도에만 표시해야한다는 점이다!
  *  - 상어의 좌표는 지역 변수를 사용한다.
  *  - 상어가 잡아먹으면 남겨질 냄새는 냄새관련 이차원 배열로 표시해두자.
  *      - -2로 표시하고, 매 턴이 지날 때마다 ++을 해주자.
@@ -50,62 +52,42 @@ import java.io.*;
 public class Solution_bj_23290_마법사상어와복제 {
 
     static class Fish {
-        int id, i, j, d;
-        boolean dead;
+        int i, j, d;
 
-        Fish(int id, int i, int j, int d) {
-            this.id = id;
+        Fish(int i, int j, int d) {
             this.i = i;
             this.j = j;
             this.d = d;
-            dead = false;
         }
 
         void move() {
-            if (dead) return; // 이미 잡아먹힌 물고기면 이동하지 않는다.
             boolean canMove = false;
-
-            int ni = i;
-            int nj = j;
             for (int k = 0; k < 8; k++) {
                 int nd = d - k;
                 if (nd < 1) nd += 8;
-                ni = i + dfi[nd];
-                nj = j + dfj[nd];
+
+                int ni = i + dfi[nd];
+                int nj = j + dfj[nd];
+
                 if (ni < 1 || ni > 4 || nj < 1 || nj > 4 || (ni == si && nj == sj) || smellMap[ni][nj] != 0) continue;
-                d = nd;
                 canMove = true;
+                setMap(ni, nj, nd);
                 break;
             }
 
-            if (canMove) {
-                // 움직일 수 있어야 원래 좌표에서 본인을 지운다.
-                for (int idx = 0; idx < map[i][j].size(); idx++) { // 원래 좌표에 id 삭제
-                    if (map[i][j].get(idx) == id) {
-                        map[i][j].remove(idx);
-                        break;
-                    }
-                }
-                // 그리고 새로운 좌표에 본인을 찍는다.
-                setPos(ni, nj);
+            if (!canMove) {
+                // 움직이는 데 실패했다면 제자리에 찍는다.
+                setMap();
             }
         }
 
-        void setPos(int i, int j) {
-            this.i = i;
-            this.j = j;
-            setMap();
-        }
-
         void setMap() {
-            if (map[i][j] == null) map[i][j] = new ArrayList<>();
-            map[i][j].add(id);
+            map[i][j].add(d);
         }
 
-        void eaten() {
-            dead = true;
+        void setMap(int ni, int nj, int nd) {
+            map[ni][nj].add(nd);
         }
-
     }
 
     static List<Integer>[][] map;
@@ -119,112 +101,131 @@ public class Solution_bj_23290_마법사상어와복제 {
         int M = Integer.parseInt(st.nextToken()); // 물고기 수
         int S = Integer.parseInt(st.nextToken()); // 상어가 마법을 연습한 횟수
         map = new List[5][5];
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                map[i][j] = new ArrayList<>();
+            }
+        }
         smellMap = new int[5][5];
-        List<Fish> fishList = new ArrayList<>();
         List<Fish> copyList = new ArrayList<>();
         int answer = M;
-        int totalId = 0;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int fi = Integer.parseInt(st.nextToken());
             int fj = Integer.parseInt(st.nextToken());
             int fd = Integer.parseInt(st.nextToken());
-            Fish newFish = new Fish(totalId++, fi, fj, fd);
-            fishList.add(newFish);
-            newFish.setMap();
+            map[fi][fj].add(fd);
         }
         st = new StringTokenizer(br.readLine(), " ");
         si = Integer.parseInt(st.nextToken());
         sj = Integer.parseInt(st.nextToken());
 
-        ArrayDeque<int[]> q = new ArrayDeque<>();
 
         for (int time = 0; time < S; time++) {
+            copyFish(copyList);
+            moveFish(copyList);
+            updateSmell();
+            int[][] movingTrace = moveShark();
+            answer = afterSharkMove(answer, movingTrace);
+            answer = pasteCopiedFish(copyList, answer);
+        }
+        System.out.println(answer);
+        br.close();
+    }
 
-            for (Fish curF : fishList) {
-                // 현재 살아있는 물고기를 모두 복제해둔다.
-                if (curF.dead) continue;
-                copyList.add(new Fish(totalId++, curF.i, curF.j, curF.d));
-            }
-            for (Fish f : fishList) {
-                f.move();
-            }
-
-            for (int i = 1; i < 5; i++) {
-                for (int j = 1; j < 5; j++) {
-                    if (smellMap[i][j] != 0)
-                        smellMap[i][j]++;
+    static void copyFish(List<Fish> copyList) {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                if (map[i][j].isEmpty()) continue;
+                for (int d : map[i][j]) {
+                    copyList.add(new Fish(i, j, d));
                 }
+                map[i][j].clear(); // 복제했으니 지도에서는 물고기들을 모두 삭제한다.
             }
+        }
+    }
 
+    static void moveFish(List<Fish> copyList) {
+        for (Fish f : copyList) {
+            f.move();
+        }
+    }
 
-            int[][] movingTrace = new int[3][2]; // 각 순서마다 이동한 좌표들
-            q.offer(new int[]{si, sj, 0, 0, 0, -1, -1}); // 좌표, 잡아먹은 물고기 수, 이동했던 방향들, 처음 좌표
-            // 이동했던 방향은 첫,두번째 방향만 찍는다. 마지막 방향은 찍을 필요 없이 끝나므로 찍을 필요 없다.
-            // 마지막 -1 2개는 첫 번째로 움직였을 때 찍은 좌표다. 두 번째는 세 번째 움직일 때 조회하므로 첫 번째만 기억하면 된다.
-            int maxEat = 0;
-            for (int i = 0; i < 3; i++) {
-                int size = q.size();
-                for (int j = 0; j < size; j++) {
-                    int[] cur = q.poll();
-                    for (int d = 0; d < 4; d++) {
-                        int ni = cur[0] + dsi[d];
-                        int nj = cur[1] + dsj[d];
-                        if (ni < 1 || ni > 4 || nj < 1 || nj > 4) continue;
-                        int dirIdx = 3 + i;
-                        int nCnt = cur[2];
-                        if (i == 0 || cur[dirIdx - 1] != (d + 2) % 4) {
-                            // 처음 움직인 경우나 이전에 이동했던 방향이 지금과 반대방향이 아닌 경우
-                            if (map[ni][nj] != null) {
-                                nCnt += map[ni][nj].size();
-                            }
-                        }
-                        if (i == 0) {
-                            q.offer(new int[]{ni, nj, nCnt, d, 0, ni, nj});
-                        } else if (i == 1) {
-                            q.offer(new int[]{ni, nj, nCnt, cur[3], d, cur[5], cur[6]});
-                        } else {
-                            if (nCnt > maxEat || movingTrace[0][0] == 0) {
-                                // 있을 수 없는 좌표, 즉 상어가 먹을 물고기가 없다고 생각하고 한 번도 이동을 안 했을 때
-                                maxEat = nCnt;
-                                movingTrace[0][0] = cur[5];
-                                movingTrace[0][1] = cur[6];
-                                movingTrace[1][0] = cur[0];
-                                movingTrace[1][1] = cur[1];
-                                movingTrace[2][0] = ni;
-                                movingTrace[2][1] = nj;
-                            }
+    static void updateSmell() {
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                if (smellMap[i][j] != 0)
+                    smellMap[i][j]++;
+            }
+        }
+    }
+
+    static int[][] moveShark() {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int[][] movingTrace = new int[3][2]; // 각 순서마다 이동한 좌표들
+        q.offer(new int[]{si, sj, 0, 0, -1, -1}); // 좌표, 잡아먹은 물고기 수, 두 번째로 이동했던 방향, 처음 움직인 좌표
+        // 이동했던 방향은 두번째 방향만 찍는다. 두 번째로 움직였을 때는 첫 번째로 다시 돌아가더라도 물고기를 먹을 수 있기 때문에 세 번째로 움직였을 때 두 번째로 움직인 장소로 다시 가는 경우만 체크하면 된다.
+        // 마지막 -1 2개는 첫 번째로 움직였을 때 찍은 좌표다. 두 번째는 세 번째 움직일 때 조회하므로 첫 번째만 기억하면 된다.
+        int maxEat = -1;
+        for (int i = 0; i < 3; i++) {
+            int size = q.size();
+            for (int j = 0; j < size; j++) {
+                int[] cur = q.poll();
+                for (int d = 0; d < 4; d++) {
+                    int ni = cur[0] + dsi[d];
+                    int nj = cur[1] + dsj[d];
+                    if (ni < 1 || ni > 4 || nj < 1 || nj > 4) continue;
+                    int nCnt = cur[2];
+                    if (i == 0 || !(i == 2 && cur[3] == (d + 2) % 4)) {
+                        // 처음 움직인 경우나 이전에 이동했던 방향이 지금과 반대방향이 아닌 경우
+                        // 여기에서 간과했던 점! 상어는 처음 본인이 있던 물고기는 먹지 않는다.
+                        // 고로 상하우 => 3 지점을 다 먹는다./ 상상하 => 2지점만 먹는다.
+                        // 즉 무조건 이전과 방향이 다르다고 안 먹는 게 아니라 i가 2일 때만 이전과 방향이 다르면 안 먹게 된다.
+                        nCnt += map[ni][nj].size();
+                    }
+                    if (i == 0) {
+                        q.offer(new int[]{ni, nj, nCnt, 0, ni, nj});
+                    } else if (i == 1) {
+                        q.offer(new int[]{ni, nj, nCnt, d, cur[4], cur[5]});
+                    } else {
+                        if (nCnt > maxEat) {
+                            maxEat = nCnt;
+                            movingTrace[0][0] = cur[4];
+                            movingTrace[0][1] = cur[5];
+                            movingTrace[1][0] = cur[0];
+                            movingTrace[1][1] = cur[1];
+                            movingTrace[2][0] = ni;
+                            movingTrace[2][1] = nj;
                         }
                     }
                 }
             }
-
-            si = movingTrace[2][0];
-            sj = movingTrace[2][1];
-            for (int i = 0; i < 3; i++) {
-                // 상어가 지나간 곳에 흔적을 남기는 곳들
-                int mi = movingTrace[i][0];
-                int mj = movingTrace[i][1];
-                if (map[mi][mj] == null || map[mi][mj].isEmpty()) continue;
-                answer -= map[mi][mj].size();
-                for (int fId : map[mi][mj]) {
-                    Fish f = fishList.get(fId);
-                    f.eaten();
-                }
-                map[mi][mj].clear();
-                smellMap[mi][mj] = -2;
-            }
-
-
-            for (Fish copied : copyList) {
-                // 복제해둔 물고기들을 다 풀어준다.
-                copied.setMap();
-                fishList.add(copied);
-                answer++;
-            }
-            copyList.clear();
         }
-        System.out.println(answer);
-        br.close();
+        return movingTrace;
+    }
+
+    static int afterSharkMove(int answer, int[][] movingTrace) {
+        si = movingTrace[2][0];
+        sj = movingTrace[2][1];
+        for (int i = 0; i < 3; i++) {
+            // 상어가 지나간 곳에 흔적을 남기는 곳들
+            int mi = movingTrace[i][0];
+            int mj = movingTrace[i][1];
+            if (map[mi][mj] == null || map[mi][mj].isEmpty()) continue;
+            answer -= map[mi][mj].size();
+            map[mi][mj].clear();
+            smellMap[mi][mj] = -2;
+        }
+        return answer;
+    }
+
+    static int pasteCopiedFish(List<Fish> copyList, int answer) {
+        for (Fish copied : copyList) {
+            // 복제해둔 물고기들을 다 풀어준다.
+            copied.setMap();
+            answer++;
+        }
+        copyList.clear();
+        return answer;
     }
 }
