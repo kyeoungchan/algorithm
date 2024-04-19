2ff35a92ecb1df86f841983859e7a936ae2e9b86
kyeoungchan <wkc1002@naver.com>
2024-04-19 17:43:46 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 220 ms, Memory: 19812 KB -BaekjoonHub

---
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static class Node implements Comparable<Node> {
+		int v;
+		int dist;
+		
+		public Node(int v, int dist) {
+			this.v = v;
+			this.dist = dist;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(dist, o.dist);
+		}
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int V = Integer.parseInt(st.nextToken()); // 정점의 수
+		int E = Integer.parseInt(st.nextToken()); // 간선의 수
+		int P = Integer.parseInt(st.nextToken()); // 건우가 위치한 정점
+		int[] dist = new int[V + 1];
+		List<int[]>[] edges = new List[V + 1];
+
+		for (int i = 1; i < V + 1; i++ ) {
+			dist[i] = Integer.MAX_VALUE;
+			edges[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			edges[a].add(new int[] {b, c});
+			edges[b].add(new int[] {a, c});
+		}
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		dist[1] = 0;
+		pq.offer(new Node(1, 0));
+			
+		boolean realSaved = false;
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			if (dist[cur.v] < cur.dist) continue;
+			
+			for (int[] edge: edges[cur.v]) {
+				int nextV = edge[0];
+				int cost = edge[1];
+				if (dist[nextV] > cur.dist + cost) {
+					dist[nextV] = cur.dist + cost;
+					pq.offer(new Node(nextV, dist[nextV]));
+				}
+			}
+		}
+		int pDist = dist[P];
+		int gDist = dist[V];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		dist[P] = 0;
+		pq.offer(new Node(P, 0));
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			if (dist[cur.v] < cur.dist) continue;
+			if (cur.v == V) break;
+			
+			for (int[] edge: edges[cur.v]) {
+				int nextV = edge[0];
+				int cost = edge[1];
+				if (dist[nextV] > cur.dist + cost) {
+					dist[nextV] = cur.dist + cost;
+					pq.offer(new Node(nextV, dist[nextV]));
+				}
+			}
+		}
+		
+		if (pDist + dist[V] == gDist) System.out.println("SAVE HIM");
+		else System.out.println("GOOD BYE");
+		br.close();
+	}
+
+}
