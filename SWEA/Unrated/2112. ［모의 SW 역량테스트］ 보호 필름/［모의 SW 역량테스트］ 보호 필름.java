4b8d2e0aa4d1cd3c7f00adff12f607c7d718bde8
kyeoungchan <wkc1002@naver.com>
2024-04-30 21:54:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 318 ms, Memory: 31,184 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,107 +1,107 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+/**
+ * 모든 세로방향에 대해서 동일한 특성의 셀들이 K개 이상 연속적으로 있어야한다.
+ * 약품 투여: 하나의 막을 모두 A 또는 B로 변경시킨다.
+ */
 public class Solution {
-	
-	static int D, W, K, film[][], tempFilm[][], ANS;
 
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("res/input_d9_2112.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc < T + 1; tc++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			D = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			film = new int[D][W];
-			for (int i = 0; i < D; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < W; j++) {
-					film[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			ANS = -1;
-			for (int total = 0; total < D; total++) {
-				int[] dropRow = new int[total];
-				select(0, 0, total, dropRow);
-				if (ANS != -1) {
-					break;
-				}
-			}
-			sb.append("#").append(tc).append(" ").append(ANS).append("\n");
-		}
-		System.out.println(sb.toString());
-		br.close();
-	}
+    static int D, W, K;
+    static int[][] film;
+    static int[] a, b;
+    static boolean passed;
 
-	static void select(int cnt, int start, int total, int[] dropRow) {
-		if (ANS != -1) return;
-		if (cnt == total) {
-			boolean[] isA = new boolean[total];
-			dropAOrB(0, dropRow, isA, total);
-			return;
-		}
-		
-		for (int i = start; i < D; i++) {
-			dropRow[cnt] = i;
-			select(cnt + 1, i + 1, total, dropRow);
-			if (ANS != -1) return;
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            D = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-	static void dropAOrB(int cnt, int[] dropRow, boolean[] isA, int total) {
-		if (ANS != -1) return;
-		if (cnt == total) {
-			tempFilm = new int[D][W];
-			for (int i = 0; i < D; i++) {
-				for (int j = 0; j < W; j++) {
-					tempFilm[i][j] = film[i][j];
-				}
-			}
-			
-			for (int i = 0; i < total; i++) {
-				int row = dropRow[i];
-				if (isA[i]) {
-					for (int j = 0; j < W; j++) {
-						tempFilm[row][j] = 0;
-					}
-				} else {
-					for (int j = 0; j < W; j++) {
-						tempFilm[row][j] = 1;
-					}
-				}
-			}
-			
-			if (passed()) ANS = total;
-			return;
-		}
-		
-		isA[cnt] = true;
-		dropAOrB(cnt + 1, dropRow, isA, total);
-		isA[cnt] = false;
-		dropAOrB(cnt + 1, dropRow, isA, total);
-	}
-	
-	static boolean passed() {
-		for (int j = 0; j < W; j++) {
-			boolean onePass = false;
-			check: for (int i = 0; i < D - K + 1; i++) {
-				int value = tempFilm[i][j];
-//				System.out.println("i: "+i + " j:"+j+" start!");
-				for (int k = i + 1; k < i + K; k++) {
-					if (tempFilm[k][j] != value) continue check;
-				}
-				onePass = true;
-//				System.out.println("i:"+ i + " j:"+ j +" pass!");
-//				System.out.println();
-				break;
-			}
-			if (!onePass) return false;
-		}
-		return true;
-	}
-	
+            // 두께: D, 가로 길이: W
+            film = new int[D][W];
+            for (int i = 0; i < D; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < W; j++) {
+                    film[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            if (K == 1) {
+                // 합격기준이 1이면 어떤 상태든 다 통과이므로 바로 다음 테케 실행
+                sb.append("#").append(tc).append(" ").append(0).append("\n");
+                continue;
+            }
+
+            // A:0, B:1
+            a = new int[W];
+            b = new int[W];
+            Arrays.fill(b, 1);
+            passed = false;
+            int answer = 0;
+            for (int medicine = 0; medicine < K + 1; medicine++) {
+                dropMedicine(0, medicine, 0);
+                if (passed) {
+                    answer = medicine;
+                    break;
+                }
+            }
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        System.out.print(sb.toString());
+        br.close();
+    }
+
+    static void dropMedicine(int cnt, int totalMedicine, int start) {
+        if (cnt == totalMedicine) {
+            if (isValid()) {
+                passed = true;
+            }
+            return;
+        }
+        // 현재 지정한 개수의 약품을 다 투여할 방법이 없는 상태라면 가지치기
+        // 왜냐하면 이미 해당 경우의 수는 이전에 다 해봤을 것이므로!
+        if (totalMedicine - cnt > D - start) return;
+
+        for (int i = start; i < D; i++) {
+            int[] temp = film[i];
+            film[i] = a; // A약품 투여
+            dropMedicine(cnt + 1, totalMedicine, i + 1);
+            if (passed) return;
+
+            film[i] = b; // B약품 투여
+            dropMedicine(cnt + 1, totalMedicine, i + 1);
+            if (passed) return;
+
+            film[i] = temp;
+//            dropMedicine(cnt, totalMedicine, i + 1); // 여기에는 약품 투여 안 함
+//            if (passed) return;
+        }
+    }
+
+    static boolean isValid() {
+        for (int j = 0; j < W; j++) {
+            boolean valid = false;
+            int cnt = 1;
+            for (int i = 0; i < D - 1; i++) {
+                if (film[i][j] == film[i + 1][j]) cnt++;
+                else cnt = 1;
+                if (cnt >= K) {
+                    // K: 합격기준
+                    // K개 이상의 연속된 상태를 발견하면 바로 다음 열로 넘어간다.
+                    valid = true;
+                    break;
+                }
+            }
+            // K개 이상의 연속된 상태를 한 열에서 한 번도 발견한 적 없으면 false 반환
+            if (!valid) return false;
+        }
+        // 모두 K개 이상의 연속된 상태를 발견했으므로 true 반환
+        return true;
+    }
 }
