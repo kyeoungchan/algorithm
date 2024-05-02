c47a0d1edffe632b00ebb2234b4f7297f9c39887
kyeoungchan <wkc1002@naver.com>
2024-05-02 21:12:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 375 ms, Memory: 109,900 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.io.*;
 
+// Solution_d9_2382_미생물격리_서울_20반_우경찬이 더 성능이 좋습니다.
+
+
 /**
  * K개의 미생물 군집
  * 정사각형셀: NxN
@@ -10,13 +13,15 @@ import java.io.*;
 public class Solution {
 
     static class Microscope {
+        int id;
         int r;
         int c;
         int cnt;
         int d;
         int preCnt;
 
-        public Microscope(int r, int c, int cnt, int d) {
+        public Microscope(int id, int r, int c, int cnt, int d) {
+            this.id = id;
             this.r = r;
             this.c = c;
             this.cnt = cnt;
@@ -27,8 +32,9 @@ public class Solution {
 
     static int N, M, K, answer;
     static int[] di = {-1, 1, 0, 0}, dj = {0, 0, -1, 1};
-    static Microscope[][] map;
+    static int[][] map;
     static List<Microscope> microscopes;
+    static boolean[] merged;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -40,10 +46,12 @@ public class Solution {
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            map = new Microscope[N][N];
-            microscopes = new ArrayList<>(K);
+            map = new int[N][N];
+            microscopes = new ArrayList<>(K + 1);
+            microscopes.add(null);
+            merged = new boolean[K + 1];
             answer = 0;
-            for (int i = 0; i < K; i++) {
+            for (int id = 1; id < K + 1; id++) {
                 st = new StringTokenizer(br.readLine(), " ");
                 int r = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
@@ -51,15 +59,16 @@ public class Solution {
                 int cnt = Integer.parseInt(st.nextToken());
                 answer += cnt;
                 int d = Integer.parseInt(st.nextToken()) - 1;
-                Microscope newM = new Microscope(r, c, cnt, d);
+                Microscope newM = new Microscope(id, r, c, cnt, d);
                 microscopes.add(newM);
-                map[r][c] = newM;
+                map[r][c] = id;
             }
 
             for (int time = 0; time < M; time++) {
-                Microscope[][] newMap = new Microscope[N][N];
-                for (int i = 0; i < microscopes.size(); i++) {
-                    Microscope m = microscopes.get(i);
+                int[][] newMap = new int[N][N];
+                for (int id = 1; id < microscopes.size(); id++) {
+                    if (merged[id]) continue;
+                    Microscope m = microscopes.get(id);
                     m.r += di[m.d];
                     m.c += dj[m.d];
                     if (m.r == 0 || m.r == N - 1 || m.c == 0 || m.c == N - 1) {
@@ -68,25 +77,28 @@ public class Solution {
                         m.cnt /= 2;
                         answer -= (m.preCnt - m.cnt);
 //                        m.preCnt = m.cnt;
-                        newMap[m.r][m.c] = m;
-                    } else if (newMap[m.r][m.c] == null) {
-                        newMap[m.r][m.c] = m;
+                        if (m.cnt != 0) {
+                            // 군집이 1이면 0으로 바뀔 수 있다.
+                            newMap[m.r][m.c] = id;
+                        } else merged[id] = true;
+                    } else if (newMap[m.r][m.c] == 0) {
+                        newMap[m.r][m.c] = id;
                     } else {
                         // 다른 군집들이 겹쳐서 합쳐지는 경우
-                        Microscope original = newMap[m.r][m.c];
+                        Microscope original = microscopes.get(newMap[m.r][m.c]);
                         if (original.preCnt > m.cnt) {
                             original.cnt += m.cnt;
-                            microscopes.remove(m);
+                            merged[id] = true;
                         } else {
                             // 군집의 개체 수가 동일하게 겹치는 경우는 주어지지 않는댔다.
                             m.cnt += original.cnt;
-                            newMap[m.r][m.c] = m;
-                            microscopes.remove(original);
+                            newMap[m.r][m.c] = id;
+                            merged[original.id] = true;
                         }
-                        i--;
                     }
                 }
-                for (Microscope m : microscopes) {
+                for (int id = 1; id < microscopes.size(); id++) {
+                    Microscope m = microscopes.get(id);
                     m.preCnt = m.cnt;
                 }
             }
