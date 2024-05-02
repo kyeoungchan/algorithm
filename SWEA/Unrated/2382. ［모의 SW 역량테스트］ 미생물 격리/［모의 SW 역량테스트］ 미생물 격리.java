d09c67bea5d63a62cc82b8e3ac98679780f8d87f
kyeoungchan <wkc1002@naver.com>
2024-05-02 15:01:31 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 337 ms, Memory: 107,940 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,77 +1,98 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+/**
+ * K개의 미생물 군집
+ * 정사각형셀: NxN
+ * 가장자리에는 약품이 칠해져있다.
+ * M시간 후 살아남은 미생물의 총합
+ */
 public class Solution {
+
+    static class Microscope {
+        int r;
+        int c;
+        int cnt;
+        int d;
+        int preCnt;
+
+        public Microscope(int r, int c, int cnt, int d) {
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+            this.d = d;
+            preCnt = cnt;
+        }
+    }
+
+    static int N, M, K, answer;
+    static int[] di = {-1, 1, 0, 0}, dj = {0, 0, -1, 1};
+    static Microscope[][] map;
+    static List<Microscope> microscopes;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int[] di = {0, -1, 1, 0, 0}, dj = {0, 0, 0, -1, 1};
         for (int tc = 1; tc < T + 1; tc++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            List<int[]> microbes = new ArrayList<>();
-            int answer = 0;
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new Microscope[N][N];
+            microscopes = new ArrayList<>(K);
+            answer = 0;
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                int mi = Integer.parseInt(st.nextToken());
-                int mj = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+
                 int cnt = Integer.parseInt(st.nextToken());
-                int d = Integer.parseInt(st.nextToken());
-                microbes.add(new int[]{mi, mj, cnt, d, cnt}); // 좌표, 군집의 미생물 수, 방향, 합쳐질 때 최대 군집의 수 기록용 장소
                 answer += cnt;
+                int d = Integer.parseInt(st.nextToken()) - 1;
+                Microscope newM = new Microscope(r, c, cnt, d);
+                microscopes.add(newM);
+                map[r][c] = newM;
             }
 
             for (int time = 0; time < M; time++) {
-                for (int i = 0; i < microbes.size(); i++) {
-                    int[] microbe = microbes.get(i);
-                    int mi = microbe[0];
-                    int mj = microbe[1];
-                    int cnt = microbe[2];
-                    int md = microbe[3];
-
-                    int ni = mi + di[md];
-                    int nj = mj + dj[md];
-                    if (ni == 0 || ni == N - 1 || nj == 0 || nj == N - 1) {
-                        int nd = md % 2 == 0 ? md - 1 : md + 1;
-                        int nCnt = cnt / 2;
-                        answer -= cnt - nCnt;
-                        microbe[2] = nCnt;
-                        microbe[3] = nd;
+                Microscope[][] newMap = new Microscope[N][N];
+                for (int i = 0; i < microscopes.size(); i++) {
+                    Microscope m = microscopes.get(i);
+                    m.r += di[m.d];
+                    m.c += dj[m.d];
+                    if (m.r == 0 || m.r == N - 1 || m.c == 0 || m.c == N - 1) {
+                        // 약품에 간 경우
+                        m.d = m.d % 2 == 0 ? m.d + 1 : m.d - 1;
+                        m.cnt /= 2;
+                        answer -= (m.preCnt - m.cnt);
+//                        m.preCnt = m.cnt;
+                        newMap[m.r][m.c] = m;
+                    } else if (newMap[m.r][m.c] == null) {
+                        newMap[m.r][m.c] = m;
                     } else {
-                        for (int j = 0; j < i; j++) {
-                            int[] another = microbes.get(j);
-                            if (another[2] == 0) continue;
-                            if (another[0] != ni || another[1] != nj) continue;
-                            if (cnt > another[4]) {
-                                microbe[2] += another[2];
-                                another[2] = 0;
-                            } else {
-                                another[2] += cnt;
-                                microbe[2] = 0;
-                            }
+                        // 다른 군집들이 겹쳐서 합쳐지는 경우
+                        Microscope original = newMap[m.r][m.c];
+                        if (original.preCnt > m.cnt) {
+                            original.cnt += m.cnt;
+                            microscopes.remove(m);
+                        } else {
+                            // 군집의 개체 수가 동일하게 겹치는 경우는 주어지지 않는댔다.
+                            m.cnt += original.cnt;
+                            newMap[m.r][m.c] = m;
+                            microscopes.remove(original);
                         }
+                        i--;
                     }
-                    microbe[0] = ni;
-                    microbe[1] = nj;
                 }
-
-                for (int i = microbes.size() - 1; i > -1; i--) {
-                    int[] microbe = microbes.get(i);
-                    if (microbe[2] == 0) {
-                        microbes.remove(i);
-                    } else {
-                        microbe[4] = microbe[2];
-                    }
+                for (Microscope m : microscopes) {
+                    m.preCnt = m.cnt;
                 }
             }
             sb.append("#").append(tc).append(" ").append(answer).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
         br.close();
     }
 }
