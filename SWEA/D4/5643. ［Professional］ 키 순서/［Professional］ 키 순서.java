b347965157d8b573df0a7f37ab822b7a4543896a
kyeoungchan <wkc1002@naver.com>
2024-05-06 17:01:03 +0900

    [D4] Title: [Professional] 키 순서, Time: 666 ms, Memory: 102,916 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,55 +1,66 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	
-	static int cnt, N;
-	static boolean[] check;
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input_d4_5643.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc < T + 1; tc++) {
-			N = Integer.parseInt(br.readLine());
-			List<Integer>[] uppers = new List[N + 1];  
-			List<Integer>[] lowers = new List[N + 1];
-			for (int i = 1; i < N + 1; i++) {
-				uppers[i] = new ArrayList<>();
-				lowers[i] = new ArrayList<>();
-			}
-			int M = Integer.parseInt(br.readLine());
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				uppers[a].add(b);
-				lowers[b].add(a);
-			}
-			int ANS = 0;
-			for (int s = 1; s < N + 1; s++) {
-				check = new boolean[N + 1];
-				cnt = getCount(s, uppers);
-				cnt += getCount(s, lowers);
-				if (cnt == N - 1) ANS++;
-			}
-			sb.append("#").append(tc).append(" ").append(ANS).append("\n");
-		}
-		System.out.println(sb.toString());
-		br.close();
-	}
-	
+    static int N;
+    static int[][] memo;
 
-	static int getCount(int stu, List<Integer>[] group) {
-		int result = 0;
-		check[stu] = true;
-		for (int next: group[stu]) {
-			if (check[next]) continue;
-			check[next] = true;
-			result += 1 + getCount(next, group);
-		}
-		return result;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+            N = Integer.parseInt(br.readLine());
+            memo = new int[N + 1][N + 1];
+            for (int i = 1; i < N + 1; i++) {
+                memo[i][0] = -1;
+                memo[0][i] = -1;
+            }
+            int M = Integer.parseInt(br.readLine());
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                // a가 b보다 작다.
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                memo[a][b] = 1;
+                /* 열 기준으로 보면 2보다 작은 애는 1, 4보다 작은 애는 1인 것을 알 수 있다.
+                * 0 1 0 1 0 => 1보다 2, 4이 크다.
+                * 0 0 0 0 0*/
+            }
+
+            for (int i = 1; i < N + 1; i++) {
+                if (memo[i][0] == -1) {
+                    update(i);
+                }
+            }
+
+            for (int j = 1; j < N + 1; j++) {
+                memo[0][j]++; // -1 -> 0
+                for (int i = 1; i < N + 1; i++) memo[0][j] += memo[i][j];
+            }
+            int ans = 0;
+            for (int i = 1; i < N + 1; i++) {
+                if (memo[i][0] + memo[0][i] == N - 1) ans++;
+            }
+            sb.append("#").append(tc).append(" ").append(ans).append("\n");
+        }
+        System.out.print(sb.toString());
+        br.close();
+    }
+
+    static void update(int number) {
+        for (int i = 1; i < N + 1; i++) {
+            if (memo[number][i] == 0) continue;
+            if (memo[i][0] == -1) update(i);
+            for (int j = 1; j < N + 1; j++) {
+                if (memo[i][j] == 1) memo[number][j] = 1;
+            }
+        }
+        memo[number][0]++; // -1 -> 0
+        for (int i = 1; i < N + 1; i++) {
+            memo[number][0] += memo[number][i];
+        }
+    }
 }
