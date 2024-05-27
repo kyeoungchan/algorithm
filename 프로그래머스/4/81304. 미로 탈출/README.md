73d2ee906157094a2654d922c35cd52d752d1f46
kyeoungchan <wkc1002@naver.com>
2024-05-27 11:21:23 +0900

    [level 4] Title: 미로 탈출, Time: 67.07 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/4/81304. 미로 탈출/README.md
+++ 프로그래머스/4/81304. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 4] 미로 탈출 - 81304 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81304?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81304) 
 
 ### 성능 요약
 
-메모리: 124 MB, 시간: 71.70 ms
+메모리: 121 MB, 시간: 67.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 26일 20:12:35
+2024년 05월 27일 11:21:20
 
 ### 문제 설명
 
--- 프로그래머스/4/81304. 미로 탈출/미로 탈출.java
+++ 프로그래머스/4/81304. 미로 탈출/미로 탈출.java
@@ -1,98 +1,84 @@
+import java.io.*;
 import java.util.*;
 
-class Solution {
+public class Solution {
+
     static class Road implements Comparable<Road> {
-        int vertex, time, bitMask;
+        int number, cost, status;
 
-        public Road(int vertex, int time) {
-            this.vertex = vertex;
-            this.time = time;
-            bitMask = 0;
-        }
-
-        public Road(int vertex, int time, int bitMask) {
-            this.vertex = vertex;
-            this.time = time;
-            this.bitMask = bitMask;
+        public Road(int number, int cost, int status) {
+            this.number = number;
+            this.cost = cost;
+            this.status = status;
         }
 
         @Override
         public int compareTo(Road o) {
-            return Integer.compare(time, o.time);
-        }
-
-        @Override
-        public String toString() {
-            return "Road{" +
-                    "vertex=" + vertex +
-                    ", time=" + time +
-                    ", bitMask=" + bitMask +
-                    '}';
+            return Integer.compare(cost, o.cost);
         }
     }
-
     public int solution(int n, int start, int end, int[][] roads, int[] traps) {
-        int answer = 0;
-        int trapCnt = traps.length;
+
+        int ans = 0;
         int[][] g = new int[n + 1][n + 1];
-        int[][] dist = new int[n + 1][1 << trapCnt];
+        for (int[] edge : roads) {
+            int from = edge[0], to = edge[1];
+            int cost = edge[2];
+            if (g[from][to] == 0)
+                g[from][to] = cost;
+            else
+                g[from][to] = Math.min(cost, g[from][to]);
+        }
+
+        int[][] dist = new int[n + 1][1 << traps.length];
         for (int i = 1; i < n + 1; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-
-        for (int[] edge : roads) {
-            int from = edge[0];
-            int to = edge[1];
-            int time = edge[2];
-            if (g[from][to] == 0) g[from][to] = time;
-            else g[from][to] = Math.min(g[from][to], time);
-        }
-
-        PriorityQueue<Road> pq = new PriorityQueue<>();
-        pq.offer(new Road(start, 0));
-        Map<Integer, Integer> map = new HashMap<>(); // 번호로 트랩 인덱스를 찾기 위한 맵
+        // 번호로 트랩 인덱스 찾기
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < traps.length; i++) {
             map.put(traps[i], i);
         }
-
+        PriorityQueue<Road> pq = new PriorityQueue<>();
+        pq.offer(new Road(start, 0, 0));
+        dist[start][0] = 0;
         while (!pq.isEmpty()) {
             Road cur = pq.poll();
-            int curVertex = cur.vertex;
-            int curTime = cur.time;
-            int curBitMask = cur.bitMask;
-
-            if (dist[curVertex][curBitMask] < curTime) continue;
-            if (curVertex == end) {
-                answer = curTime;
+            int curNum = cur.number;
+            int curCost = cur.cost;
+            int curStatus = cur.status;
+            if (dist[curNum][curStatus] < curCost) continue;
+            if (curNum == end) {
+                ans = curCost;
                 break;
             }
 
-            boolean curTrapped = map.containsKey(curVertex) ? (curBitMask & (1 << map.get(curVertex))) > 0 : false;
+            boolean isTrap = map.containsKey(curNum);
+            boolean currentTrapped = isTrap && (curStatus & 1 << map.get(curNum)) > 0;
             for (int i = 1; i < n + 1; i++) {
-                if (i == cur.vertex) continue;
-                boolean nextIsTrap = map.containsKey(i);
-                boolean nextTrapped = nextIsTrap && (curBitMask & (1 << map.get(i))) > 0;
-                int newBitMask = curBitMask;
-                if (nextIsTrap && !nextTrapped) {
-                    newBitMask |= (1 << map.get(i));
-                } else if (nextIsTrap && nextTrapped) {
-                    newBitMask &= ~(1 << map.get(i));
+                int nextStatus = curStatus;
+                boolean isNextTrap = map.containsKey(i);
+                boolean nextTrapped = isNextTrap && (curStatus & 1 << map.get(i)) > 0;
+                if (isNextTrap) {
+                    if (!nextTrapped)
+                        nextStatus |= 1 << map.get(i);
+                    else
+                        nextStatus &= ~(1 << map.get(i));
                 }
-                if (curTrapped == nextTrapped) {
-                    if (g[curVertex][i] != 0 && dist[i][newBitMask] > curTime + g[curVertex][i]) {
-                        dist[i][newBitMask] = curTime + g[curVertex][i];
-                        pq.offer(new Road(i, dist[i][newBitMask], newBitMask));
+                if (currentTrapped == nextTrapped) {
+                    if (g[curNum][i] != 0 && dist[i][nextStatus] > curCost + g[curNum][i]) {
+                        dist[i][nextStatus] = curCost + g[curNum][i];
+                        pq.offer(new Road(i, dist[i][nextStatus], nextStatus));
                     }
                 } else {
-
-                    if (g[i][curVertex] != 0 && dist[i][newBitMask] > curTime + g[i][curVertex]) {
-                        dist[i][newBitMask] = curTime + g[i][curVertex];
-                        pq.offer(new Road(i, dist[i][newBitMask], newBitMask));
+                    if (g[i][curNum] != 0 && dist[i][nextStatus] > curCost + g[i][curNum]) {
+                        dist[i][nextStatus] = curCost + g[i][curNum];
+                        pq.offer(new Road(i, dist[i][nextStatus], nextStatus));
                     }
                 }
             }
-
         }
-        return answer;
+
+        return ans;
     }
 }
