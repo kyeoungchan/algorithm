a29a1d397d22f59d7cfb08e0316be3dce00fa0f8
kyeoungchan <wkc1002@naver.com>
2024-05-30 18:12:29 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+//package a0227;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		dp[1] = 1;
+		if (N > 1) {
+			dp[2] = 3;
+			for (int i = 3; i < N + 1; i++) {
+				dp[i] = (2 * dp[i-2] + dp[i-1]) % 10007;
+			}
+		}
+		System.out.println(dp[N]);
+		br.close();
+	}
+
+}
