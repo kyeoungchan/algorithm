b95bc32726d7338c352907e3a962c4ac85b319b3
kyeoungchan <wkc1002@naver.com>
2024-05-30 15:24:43 +0900

    [level 5] Title: 시험장 나누기, Time: 12.71 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/5/81305. 시험장 나누기/README.md
+++ 프로그래머스/5/81305. 시험장 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 5] 시험장 나누기 - 81305 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81305?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81305) 
 
 ### 성능 요약
 
-메모리: 56.8 MB, 시간: 13.81 ms
+메모리: 56.7 MB, 시간: 12.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 29일 21:17:35
+2024년 05월 30일 15:24:40
 
 ### 문제 설명
 
--- 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
@@ -1,88 +1,79 @@
-
 import java.util.*;
 
-class Solution {
-    private int root, size;
-    private Node[] tree;
-    private int[] parent;
+public class Solution {
 
+    int n, root, cutCnt;
+    int[] parents;
+
+    // 시험장이 n개 있다면, 시험장의 고유 번호는 0부터 n-1까지 부여됩니다.
     public int solution(int k, int[] num, int[][] links) {
         int answer = 0;
-        setTree(num, links);
+        // 각 노드들의 부모를 가리키는 배열
+        n = num.length;
+        parents = new int[n];
+        Arrays.fill(parents, -1);
 
-        int low = 0, high = (int) 1e9;
-        //high의 경우 Integer.MAX_VALUE를 하면 원소 중 최댓값을 더해줬을 때
-        //int범위를 벗어남으로 주의해줘야합니다.
-        for (int n : num) low = Math.max(low, n);
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (checkGroup(num, mid) <= k) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return high + 1;
-    }
-
-    private int checkGroup(int[] num, int max) {
-        size = 0; //자르는 개수
-        dfs(num, root, max);
-        return size + 1;
-    }
-
-    private int dfs(int[] num, int cur, int max) {
-        int lv = 0, rv = 0;
-        if (tree[cur].left != -1)
-            lv = dfs(num, tree[cur].left, max);
-        if (tree[cur].right != -1)
-            rv = dfs(num, tree[cur].right, max);
-
-        //자식 노드들과 자신의 값의 합이 max보다 작거나 같으면 자를 필요가 없다.
-        if (num[cur] + lv + rv <= max)
-            return num[cur] + lv + rv;
-
-        //자신 노드들 중 제일 작은 값과 자신의 값의 합이 max보다 작거나 같으면 한개만 자르면 된다.
-        if (num[cur] + Math.min(lv, rv) <= max) {
-            size += 1;
-            return num[cur] + Math.min(lv, rv);
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            int left = links[i][0];
+            if (left != -1) parents[left] = i;
+            int right = links[i][1];
+            if (right != -1) parents[right] = i;
+            start = Math.max(start, num[i]);
+            end += num[i];
         }
 
-        //둘 다 잘라야하는 경우
-        //맨 밑단 노드 값이 max보다 작은 경우는 없습니다.
-        //low가 제일 큰 노드를 기준으로 잡히기 때문에 무조건 첫번째 if에서 통과됩니다.
-        size += 2;
-        return num[cur];
-    }
-
-    private void setTree(int[] num, int[][] links) {
-        tree = new Node[num.length];
-        parent = new int[num.length];
-
-        Arrays.fill(parent, -1);
-
-        for (int i = 0; i < links.length; i++) {
-            tree[i] = new Node(links[i][0], links[i][1]);
-
-            if (tree[i].left != -1) parent[tree[i].left] = i;
-            if (tree[i].right != -1) parent[tree[i].right] = i;
-        }
-
-        for (int i = 0; i < parent.length; i++) {
-            if (parent[i] == -1) {
+        for (int i = 0; i < n; i++) {
+            if (parents[i] == -1) {
                 root = i;
                 break;
             }
         }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // mid 인원수를 최대로 하는 그룹으로 나눴을 때 k개 이하의 그룹이 생성이 가능하지 못하다면
+            if (!canDivide(mid, k, num, links)) {
+                start = mid + 1;
+            } else {
+                answer = mid;
+                end = mid - 1;
+            }
+        }
+        return answer;
     }
 
-    class Node {
-        int left, right;
+    private boolean canDivide(int mid, int k, int[] num, int[][] links) {
+        cutCnt = 0;
+        divide(mid, root, num, links);
+        return cutCnt <= k - 1;
+    }
 
-        public Node(int left, int right) {
-            this.left = left;
-            this.right = right;
+    // 현재노드에서 자식 노드를 자른다고 생각하자.
+    private int divide(int mid, int idx, int[] num, int[][] links) {
+        int left = 0, right = 0;
+        int leftIdx = links[idx][0];
+        int rightIdx = links[idx][1];
+        if (leftIdx != -1) left = divide(mid, leftIdx, num, links);
+        if (rightIdx != -1) right = divide(mid, rightIdx, num, links);
+
+        // 어느쪽도 안 잘라도 되는 경우
+        if (num[idx] + left + right <= mid) {
+            return num[idx] + left + right;
         }
+
+        // 한 쪽만 자르면 해결되는 경우
+        if (num[idx] + Math.min(left, right) <= mid) {
+            cutCnt++;
+            return num[idx] + Math.min(left, right);
+        }
+
+        // 양 쪽 다 잘라야하는 경우
+        // 물론 mid가 하나의 노드의 값보다 더 작은 경우는 여기서 걸러지지 않는다. 그러나 결국 cutCnt가 많아질수밖에 없으므로 정답에 영향을 주지 않게 된다.
+        // 사실 start를 0이 아니라 조건에 맞게 주고 시작해야 옳다.
+        cutCnt += 2;
+        return num[idx];
     }
 }
+
