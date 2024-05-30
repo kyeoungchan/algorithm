588b41103d4f67b538aaaad42a91c5f8d0f80068
kyeoungchan <wkc1002@naver.com>
2024-05-30 13:36:49 +0900

    [Silver IV] Title: 동전 0, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,34 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-/**
- * 준규가 갖고 있는 동전: 총 N 종류
- * 각각의 동전은 무제한
- * 가치의 합 K로 만들려고 한다.
- * 필요한 동전 개수의 최소값
- */
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
         int[] A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
-
-        int answer = 0;
+        int cnt = 0;
         for (int i = N - 1; i > -1; i--) {
             int price = A[i];
-            if (price <= K) {
-                answer += K / price;
-                K %= price;
-            }
+            cnt += K / price;
+            K %= price;
+            if (K == 0) break;
         }
-
-        System.out.println(answer);
+        System.out.println(cnt);
         br.close();
     }
 }
