45eca41f2db02a60a39d08bdc534ac1f3362859c
kyeoungchan <wkc1002@naver.com>
2024-05-30 13:52:29 +0900

    [Silver II] Title: 이동하기, Time: 420 ms, Memory: 72972 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -2,34 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][] map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		int[][] dp = new int[N][M];
-		dp[0][0] = map[0][0];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				int up = 0;
-				int diagonal = 0;
-				int left = 0;
-				if (i > 0) up = dp[i-1][j];
-				if (i > 0 && j > 0) diagonal = dp[i-1][j-1];
-				if (j > 0) left = dp[i][j-1];
-				dp[i][j] = map[i][j] + Math.max(Math.max(up, diagonal), left);
-			}
-		}
-		System.out.println(dp[N-1][M-1]);
-		br.close();
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j < M + 1; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                dp[i][j] = Math.max(Math.max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + value;
+            }
+        }
+        System.out.println(dp[N][M]);
+        br.close();
+    }
 }
