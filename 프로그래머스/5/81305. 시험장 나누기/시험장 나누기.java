f184727d49709c0d50829df37f92c72ca3282466
kyeoungchan <wkc1002@naver.com>
2024-06-01 14:05:45 +0900

    [level 5] Title: 시험장 나누기, Time: 11.22 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
@@ -2,26 +2,30 @@ import java.util.*;
 
 public class Solution {
 
-    int n, root, cutCnt;
+    int n, groupCnt, root;
     int[] parents;
 
-    // 시험장이 n개 있다면, 시험장의 고유 번호는 0부터 n-1까지 부여됩니다.
     public int solution(int k, int[] num, int[][] links) {
-        int answer = 0;
-        // 각 노드들의 부모를 가리키는 배열
         n = num.length;
         parents = new int[n];
         Arrays.fill(parents, -1);
 
+
         int start = 0;
         int end = 0;
         for (int i = 0; i < n; i++) {
-            int left = links[i][0];
-            if (left != -1) parents[left] = i;
-            int right = links[i][1];
-            if (right != -1) parents[right] = i;
-            start = Math.max(start, num[i]);
-            end += num[i];
+            int cur = num[i];
+            if (cur > start) {
+                start = cur;
+            }
+            end += cur;
+
+            if (links[i][0] != -1) {
+                parents[links[i][0]] = i;
+            }
+            if (links[i][1] != -1) {
+                parents[links[i][1]] = i;
+            }
         }
 
         for (int i = 0; i < n; i++) {
@@ -33,47 +37,48 @@ public class Solution {
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            // mid 인원수를 최대로 하는 그룹으로 나눴을 때 k개 이하의 그룹이 생성이 가능하지 못하다면
-            if (!canDivide(mid, k, num, links)) {
-                start = mid + 1;
-            } else {
-                answer = mid;
+            if (canDivide(mid, k, num, links)) {
                 end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        return answer;
+        return start;
     }
+
 
     private boolean canDivide(int mid, int k, int[] num, int[][] links) {
-        cutCnt = 0;
-        divide(mid, root, num, links);
-        return cutCnt <= k - 1;
+        groupCnt = 1;
+        divide(root, mid, num, links);
+        return groupCnt <= k;
     }
 
-    // 현재노드에서 자식 노드를 자른다고 생각하자.
-    private int divide(int mid, int idx, int[] num, int[][] links) {
-        int left = 0, right = 0;
-        int leftIdx = links[idx][0];
-        int rightIdx = links[idx][1];
-        if (leftIdx != -1) left = divide(mid, leftIdx, num, links);
-        if (rightIdx != -1) right = divide(mid, rightIdx, num, links);
 
-        // 어느쪽도 안 잘라도 되는 경우
+    private int divide(int idx, int mid, int[] num, int[][] links) {
+        int left = 0, right = 0;
+        if (links[idx][0] != -1) {
+            left = divide(links[idx][0], mid, num, links);
+        }
+        if (links[idx][1] != -1) {
+            right = divide(links[idx][1], mid, num, links);
+        }
+        
+        // 현재 노드가 mid보다 작으면 그룹을 새로 만들지 않고 바로 재귀호출 종료
         if (num[idx] + left + right <= mid) {
             return num[idx] + left + right;
-        }
-
-        // 한 쪽만 자르면 해결되는 경우
+        } 
+        
+        // 좌우 자식 노드 중 하나만 더했을 때 mid보다 작다면 더 큰 자식 노드를 다른 그룹으로 분리 후 반환
+        // 참고로 여기서 하나의 자식 노드만 있는 경우는 있을 수 없다.
+        // 하나의 자식 노드만 있고 그 자식 노드를 더했을 때 mid보다 작거나 같은 경우는 위의 조건문에서 걸러진다.
         if (num[idx] + Math.min(left, right) <= mid) {
-            cutCnt++;
+            groupCnt++;
             return num[idx] + Math.min(left, right);
         }
 
-        // 양 쪽 다 잘라야하는 경우
-        // 물론 mid가 하나의 노드의 값보다 더 작은 경우는 여기서 걸러지지 않는다. 그러나 결국 cutCnt가 많아질수밖에 없으므로 정답에 영향을 주지 않게 된다.
-        // 사실 start를 0이 아니라 조건에 맞게 주고 시작해야 옳다.
-        cutCnt += 2;
+        // 이젠 양 자식 노드를 모두 다른 그룹으로 보낸 경우다.
+        // 참고로 start를 모든 시험장 중 최대 인원수로 선정하고 시작했으므로 num[idx]가 mid보다 큰 경우는 있을 수 없다.
+        groupCnt += 2;
         return num[idx];
     }
 }
-
