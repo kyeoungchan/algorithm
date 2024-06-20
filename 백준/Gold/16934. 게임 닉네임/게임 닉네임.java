eda1f5ee81279e11fb055a45e08c23c9fb9a38d3
kyeoungchan <wkc1002@naver.com>
2024-06-20 14:35:29 +0900

    [Gold III] Title: 게임 닉네임, Time: 644 ms, Memory: 77508 KB -BaekjoonHub

---
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 닉네임은 알파벳 소문자로만 이루어져있다.
+ * 유저 닉네임을 이용해 저장할 별칭을 만든다.
+ * 접두사 중에서 가장 길이가 짧은 것.
+ * 하지만 이전에 가입한 유저의 닉네임의 접두사면 안 된다.
+ * 가능한 별칭이 없는 경우, 가입한 시점에서 같은 닉네임으로 가입한 사람의 수 x를 계산한다.
+ * 그리고 x가 2 이상이라면 {풀 닉네임x}로 별칭을 정한다.
+ * 닉네임을 파싱해서 각각 별칭의 카운트 정보를 담은 객체를 Map에다가 저장한다.
+ * 저장하면서 처음 저장하는 별칭이 나온다면 그것을 StringBuilder에 담는다.
+ */
+public class Main {
+
+    static class Data {
+        String alias;
+        int count;
+
+        public Data(String alias) {
+            this.alias = alias;
+        }
+
+        public void addAlias() {
+            count++;
+        }
+
+        public String getFullNickName() {
+            if (count == 1) return alias;
+            return alias + count;
+        }
+    }
+
+    static Map<String, Data> map;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            String nickName = br.readLine();
+            parseAndGetAlias(nickName, sb);
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void parseAndGetAlias(String nickName, StringBuilder sb) {
+        boolean gotAlias = false;
+        for (int i = 1; i < nickName.length() + 1; i++) {
+            String candidate = nickName.substring(0, i);
+            if (!map.containsKey(candidate)) {
+                if (!gotAlias) {
+                    sb.append(candidate).append("\n");
+                    gotAlias = true;
+                }
+                map.put(candidate, new Data(candidate));
+            }
+        }
+
+        // 지금까지 같은 닉네임으로 가입한 유저의 수 메모
+        map.get(nickName).addAlias();
+        if (!gotAlias) {
+            sb.append(map.get(nickName).getFullNickName()).append("\n");
+        }
+    }
+}
