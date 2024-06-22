b7d02bde26d439acd0157297916c1a750e53e827
kyeoungchan <wkc1002@naver.com>
2024-06-22 16:37:08 +0900

    feat: SWEA - d3 3304 최장 공통 부분 수열

--- src/swexpertacademy/최장공통부분수열/Solution_d3_3304_최장공통부분수열.java
+++ src/swexpertacademy/최장공통부분수열/Solution_d3_3304_최장공통부분수열.java
@@ -1,2 +1,41 @@
-package swexpertacademy.최장공통부분수열;public class Solution_d3_3304_최장공통부분수열 {
+package swexpertacademy.최장공통부분수열;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 0 1 1 1 1 1
+ * 1 1 1 2 2 2
+ */
+public class Solution_d3_3304_최장공통부분수열 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input_d3_3304.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String a = "_" + st.nextToken();
+            String b = "_" + st.nextToken();
+            int aLength = a.length();
+            int bLength = b.length();
+
+            int[][] dp = new int[aLength][bLength];
+            for (int i = 1; i < aLength; i++) {
+                char aChar = a.charAt(i);
+                for (int j = 1; j < bLength; j++) {
+                    if (aChar == b.charAt(j)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            sb.append("#").append(tc).append(" ").append(dp[aLength-1][bLength-1]).append("\n");
+        }
+        System.out.print(sb.toString());
+        br.close();
+    }
 }
