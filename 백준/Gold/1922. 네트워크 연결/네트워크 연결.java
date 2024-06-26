6fda8b3bba160fcd7998fd436539fff1642fb98c
kyeoungchan <wkc1002@naver.com>
2024-06-27 03:06:22 +0900

    [Gold IV] Title: 네트워크 연결, Time: 488 ms, Memory: 50972 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,56 +1,56 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-/**
- * 프림으로
- */
 public class Main {
+    static class Host implements Comparable<Host> {
+        int number, cost;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		List<int[]>[] edges = new List[N + 1];
-		int[] minEdges = new int[N + 1];
-		for (int i = 1; i < N  + 1; i++) {
-			edges[i] = new ArrayList<>();
-			minEdges[i] = Integer.MAX_VALUE;
-		}
-		int M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			edges[from].add(new int[] {to, cost});
-			edges[to].add(new int[] {from, cost});
-		}
+        public Host(int number, int cost) {
+            this.number = number;
+            this.cost = cost;
+        }
 
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-		boolean[] v = new boolean[N + 1];
-		minEdges[1] = 0;
-		pq.offer(new int[] {1, 0});
-		int result = 0;
-		while (!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int minVertex = cur[0];
-			int min = cur[1];
-			
-			if(v[minVertex]) continue;
-			v[minVertex] = true;
-			result += min;
-			
-			for (int[] edge : edges[minVertex]) {
-				int to = edge[0];
-				int cost = edge[1];
-				if (!v[to] && minEdges[to] > cost) {
-					minEdges[to] = cost;
-					pq.offer(new int[] {to, cost});
-				}
-			}
-		}
-		System.out.println(result);
-		br.close();
-	}
+        @Override
+        public int compareTo(Host o) {
+            return Integer.compare(cost, o.cost);
+        }
+    }
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        List<Host>[] network = new List[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            network[i] = new ArrayList<>();
+        }
+        int min = 0;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            network[a].add(new Host(b, c));
+            if (a != b) network[b].add(new Host(a, c));
+        }
+
+        PriorityQueue<Host> pq = new PriorityQueue<>();
+        boolean[] v = new boolean[N + 1];
+        pq.offer(new Host(1, 0));
+        while (!pq.isEmpty()) {
+            Host cur = pq.poll();
+
+            if (v[cur.number]) continue;
+            v[cur.number] = true;
+            min += cur.cost;
+
+            for (Host next: network[cur.number]) {
+                if (v[next.number]) continue;
+                pq.offer(next);
+            }
+        }
+        System.out.println(min);
+        br.close();
+    }
 }
