e3df7847ccf9a517975158122e477264ab1ec065
wkc1002 <wkc1002@naver.com>
2024-07-03 20:37:00 +0900

    feat: Baekjoon - 19951 태상이의 훈련소 생활

---
+++ src/baekjoon/태상이의훈련소생활/Solution_bj_19951_태상이의훈련소생활.java
@@ -0,0 +1,50 @@
+package baekjoon.태상이의훈련소생활;
+
+import java.io.*;
+
+import java.util.*;
+
+
+/**
+ * N개의 칸으로 이루어져있다.
+ * 각 칸은 1~N번으로 이루어져 있다.
+ * 조교: 총 M명
+ */
+public class Solution_bj_19951_태상이의훈련소생활 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] H = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 1; i < N + 1; i++)
+            H[i] = Integer.parseInt(st.nextToken());
+
+        int[] status = new int[N + 2];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            status[a] += k;
+            status[b + 1] -= k;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int singleStatus = 0;
+
+        for (int i = 1; i < N + 1; i++) {
+            singleStatus += status[i];
+            sb.append(H[i] + singleStatus).append(" ");
+        }
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+
+}
