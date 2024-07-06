2ad9cf57dd8aa45189b4af9de2e2ce3396b1913c
kyeongchan <wkc1002@naver.com>
2024-07-06 10:45:44 +0900

    [D3] Title: 문자열 교집합, Time: 4,571 ms, Memory: 124,940 KB -BaekjoonHub

---
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc < T + 1; tc++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            Set<Long> set = new HashSet<>();
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                long hashCode = hash(st.nextToken());
+                set.add(hashCode);
+            }
+
+            int answer = 0;
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < M; i++) {
+                long hashCode = hash(st.nextToken());
+                if (set.contains(hashCode)) answer++;
+            }
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        System.out.print(sb.toString());
+        br.close();
+    }
+
+    static long hash(String str) {
+        long result = 0;
+        for (int i = str.length() - 1; i > -1; i--) {
+            result += (int) Math.pow(30, i) * str.charAt(i);
+        }
+        return result;
+    }
+}
