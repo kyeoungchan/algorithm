7f9de7c87fe56718beb5d90ad514fce4afd9c8fc
wkc1002 <wkc1002@naver.com>
2024-08-20 15:37:27 +0900

    feat: SWEA 촛불 이벤트 재풀이

--- src/swexpertacademy/촛불이벤트/Solution_d5_9843_촛불이벤트2.java
+++ src/swexpertacademy/촛불이벤트/Solution_d5_9843_촛불이벤트2.java
@@ -1,4 +1,3 @@
-/*
 package swexpertacademy.촛불이벤트;
 
 import java.io.*;
@@ -18,25 +17,30 @@ public class Solution_d5_9843_촛불이벤트2 {
                 continue;
             }
             int left = 1;
-            */
-/* N = k(k+1)/2
+            /* N = k(k+1)/2
             * 2N = k^2 + k
             * 2N + 1/4 = (k + 1/2)^2
             * k + 1/2 = sqrt(2N+1/4)
             * k = sqrt(2N + 1/4) - 1/2
-            * 1 <= k <= sqrt(2 * 10^18 + 1/4) - 1/2*//*
+            * 1 <= k <= sqrt(2 * 10^18 + 1/4) - 1/2*/
 
             int right = 2000000000;
+            int answer = -1;
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                if ()
+                long total = (long) mid * (mid + 1) / 2;
+                if (total == N) {
+                    answer = mid;
+                    break;
+                } else if (total < N) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
+            sb.append(answer).append("\n");
         }
+        System.out.print(sb.toString());
         br.close();
     }
-
-    static boolean allUse(int mid, long N) {
-
-    }
 }
-*/
