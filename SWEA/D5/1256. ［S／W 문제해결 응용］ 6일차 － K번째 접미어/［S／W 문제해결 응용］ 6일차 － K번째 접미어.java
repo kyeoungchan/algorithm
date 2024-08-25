c3f435bc6d7f30e32d13b203464faf1ca3d3e7a2
kyeongchan <wkc1002@naver.com>
2024-08-25 18:21:50 +0900

    [D5] Title: [S/W 문제해결 응용] 6일차 - K번째 접미어, Time: 187 ms, Memory: 50,296 KB -BaekjoonHub

--- SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.java
+++ SWEA/D5/1256. ［S／W 문제해결 응용］ 6일차 － K번째 접미어/［S／W 문제해결 응용］ 6일차 － K번째 접미어.java
@@ -2,28 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+
+    static class Trie {
+        char alphabet;
+        boolean last;
+        int cnt;
+        Map<Character, Trie> children = new HashMap<>();
+
+        public Trie() {
+        }
+
+        public Trie(char alphabet) {
+            this.alphabet = alphabet;
+        }
+    }
+
+    static boolean searched;
+    static int K;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc < T + 1; tc++) {
-            int K = Integer.parseInt(br.readLine());
-            String str = br.readLine();
-            if (str.length() < K) {
-                // 만약 K번째 문자열이 존재하지 않는다면, “none”을 출력한다.
-                sb.append("#").append(tc).append(" ").append("none").append("\n");
+            K = Integer.parseInt(br.readLine());
+            String s = br.readLine();
+            if (s.length() < K) {
+                sb.append("#").append(tc).append(" ").append("none\n");
                 continue;
             }
 
-            List<String> strList = new ArrayList<>();
-            for (int i = 0; i < str.length(); i++) {
-                strList.add(str.substring(i, str.length()));
+            Trie head = new Trie();
+
+            for (int i = 0; i < s.length(); i++) {
+                Trie idxTrie = head;
+                for (int j = i; j < s.length(); j++) {
+                    char cur = s.charAt(j);
+                    if (!idxTrie.children.containsKey(cur)) idxTrie.children.put(cur, new Trie(cur));
+                    idxTrie = idxTrie.children.get(cur);
+                    idxTrie.cnt++;
+                }
+                idxTrie.last = true;
             }
-            Collections.sort(strList);
-            sb.append("#").append(tc).append(" ").append(strList.get(K - 1)).append("\n");
+
+            searched = false;
+            StringBuilder tempSb = new StringBuilder();
+            dfs(head, tempSb);
+            sb.append("#").append(tc).append(" ").append(tempSb.toString()).append("\n");
+
         }
         System.out.print(sb.toString());
         br.close();
+    }
+
+    static void dfs(Trie idxTrie, StringBuilder tempSb) {
+        if (idxTrie.last) {
+            K--;
+            if (K == 0) searched = true;
+            return;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (!idxTrie.children.containsKey(c)) continue;
+            if (idxTrie.children.get(c).cnt < K) {
+                K -= idxTrie.children.get(c).cnt;
+                continue;
+            }
+            tempSb.append(c);
+            dfs(idxTrie.children.get(c), tempSb);
+            if (searched) return;
+            tempSb.deleteCharAt(tempSb.toString().length() - 1);
+        }
     }
 }
