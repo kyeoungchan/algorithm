df804e47c10995d53fc703f90ae52d4e0c672144
wkc1002 <wkc1002@naver.com>
2024-10-09 19:35:20 +0900

    feat: Baekjoon 11779 최소 비용 구하기2

--- src/baekjoon/최소비용구하기2/Solution_bj_11779_최소비용구하기2.java
+++ src/baekjoon/최소비용구하기2/Solution_bj_11779_최소비용구하기2.java
@@ -26,8 +26,10 @@ public class Solution_bj_11779_최소비용구하기2 {
         int n = Integer.parseInt(br.readLine()); // 도시의 개수
         int m = Integer.parseInt(br.readLine()); // 버스의 개수
         List<BusInfo>[] buses = new List[n + 1];
+        List<Integer>[] pathes = new List[n + 1];
         for (int i = 1; i < n + 1; i++) {
             buses[i] = new ArrayList<>();
+            pathes[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -43,33 +45,33 @@ public class Solution_bj_11779_최소비용구하기2 {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
+        pathes[start].add(start);
 
         PriorityQueue<BusInfo> pq = new PriorityQueue<>();
         pq.offer(new BusInfo(start, 0));
-        sb.append(start).append(" ");
 
-        int count = 0;
-        boolean check = false;
+
         while (!pq.isEmpty()) {
             BusInfo cur = pq.poll();
             if (cur.cost > dist[cur.destNum]) continue;
-            if (cur.destNum == end) check = true;
+            if (cur.destNum == end) break;
 
             for (BusInfo next: buses[cur.destNum]) {
                 if (dist[next.destNum] > cur.cost + next.cost) {
                     dist[next.destNum] = cur.cost + next.cost;
                     pq.offer(new BusInfo(next.destNum, dist[next.destNum]));
-                    if (!check) sb.append(next.destNum).append(" ");
+                    pathes[next.destNum].clear();
+                    pathes[next.destNum].addAll(pathes[cur.destNum]);
+                    pathes[next.destNum].add(next.destNum);
                 }
             }
         }
-
-        for (int i = 1; i < n + 1; i++) {
-            if (dist[i] == dist[end]) count++;
+        sb.append(dist[end]).append("\n");
+        sb.append(pathes[end].size()).append("\n");
+        for (int next : pathes[end]) {
+            sb.append(next).append(" ");
         }
-        System.out.println(dist[end]);
-        System.out.println(count);
-        System.out.println(sb.toString());
+        System.out.println(sb);
         br.close();
     }
 }
