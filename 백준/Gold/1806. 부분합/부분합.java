1a4042255a28a3b94c454067708e1003b102318f
kyeongchan <wkc1002@naver.com>
2024-10-10 23:05:32 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22392 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken()); // 수열의 길이
+        int S = Integer.parseInt(st.nextToken()); // 구하고자하는 부분합
+        int answer = N + 1;
+        int[] numbers = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0, end = 0;
+        int sum = 0;
+        while (start < N && end < N) {
+            sum += numbers[end++];
+            while (sum >= S) {
+                answer = Math.min(answer, end - start);
+                sum -= numbers[start++];
+            }
+        }
+
+        if (answer == N + 1) answer = 0;
+        System.out.println(answer);
+        br.close();
+    }
+}
