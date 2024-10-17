ecc6952aa131895a99a24b03d3116c7d8096d9ea
wkc1002 <wkc1002@naver.com>
2024-10-17 19:50:17 +0900

    feat: Baekjoon - 15486 퇴사2

--- src/baekjoon/퇴사2/Solution_bj_15486_퇴사2.java
+++ src/baekjoon/퇴사2/Solution_bj_15486_퇴사2.java
@@ -8,7 +8,7 @@ public class Solution_bj_15486_퇴사2 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
+        long[] dp = new long[N + 1];
         // 50 50 50
         //    20
         //       100
@@ -16,20 +16,12 @@ public class Solution_bj_15486_퇴사2 {
             st = new StringTokenizer(br.readLine(), " ");
             int t = Integer.parseInt(st.nextToken()); // 기간
             int p = Integer.parseInt(st.nextToken()); // 금액
-            if (i + t - 1 > N) {
-                if (dp[i] == 0)
-                    dp[i] = dp[i - 1];
-                continue;
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            if (i + t - 1 <= N) {
+                dp[i + t - 1] = Math.max(dp[i - 1] + p, dp[i + t - 1]);
             }
-            dp[i + t - 1] = Math.max(dp[i - 1] + p, dp[i + t - 1]);
-            if (dp[i] == 0) dp[i] = dp[i - 1];
         }
-        int ans = 0;
-        for (int i = 1; i < N + 1; i++) {
-//            System.out.println(dp[i]);
-            ans = Math.max(ans, dp[i]);
-        }
-        System.out.println(ans);
+        System.out.println(dp[N]);
         br.close();
     }
 }
