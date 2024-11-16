1445cd55a9799a74f6b8157ba7f49fc5e460b056
wkc1002 <wkc1002@naver.com>
2024-11-16 21:50:42 +0900

    feat: Bakejoon 1508 레이스

--- src/baekjoon/레이스/Solution_bj_1508_레이스.java
+++ src/baekjoon/레이스/Solution_bj_1508_레이스.java
@@ -4,64 +4,66 @@ import java.io.*;
 import java.util.*;
 
 public class Solution_bj_1508_레이스 {
-    static int pos1, pos2;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 레이스 트랙 길이
-        // 1~1_000_000
-        int M = Integer.parseInt(st.nextToken()); // 심판 수
-        // 1~K
-        int K = Integer.parseInt(st.nextToken()); // 심판이 정해진 위치의 개수
-        // 2~50
-        int[] positions = new int[K];
-        st = new StringTokenizer(br.readLine());
-        int min = Integer.MAX_VALUE, max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken()); // 레이스 트랙의 길이
+        // 1 ~ 1_000_000
+        int M = Integer.parseInt(st.nextToken()); // 심판의 수
+        // 1 ~ K
+        int K = Integer.parseInt(st.nextToken()); // 심판을 배치시키려는 곳의 개수
+        // 2 ~ 50
+
+        int[] positions = new int[K]; // 심판을 배치시킬 수 있는 위치들
+        // 각 위치는 0 ~ N
+        // 오름차순으로 주어진다.
+        st = new StringTokenizer(br.readLine(), " ");
+        int left = N;
         for (int i = 0; i < K; i++) {
             positions[i] = Integer.parseInt(st.nextToken());
-            min = Math.min(min, positions[i]);
-            max = Math.max(max, positions[i]);
+            if (i != 0) left = Math.min(left, positions[i] - positions[i - 1]);
         }
-        int left = 0, right = max - min;
+
+        int right = positions[K - 1] - positions[0];
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (!canPlace(mid, positions, M, K)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        System.out.println("left = " + left);
-        int[] setStatus = new int[K];
-
-        int lastPos = positions[0];
-        setStatus[0] = 1;
-        M--;
-        for (int i = 1; i < K && M > 0; i++) {
-            if (positions[i] - lastPos >= left) {
-                lastPos = positions[i];
-                M--;
-                setStatus[i] = 1;
-            }
-        }
-
-        for (int i = 0; i < K; i++) {
-            System.out.print(setStatus[i]);
-        }
-        System.out.println();
+            if (!canSet(mid, positions, M, K)) right = mid - 1;
+            else left = mid + 1;
+         }
+        System.out.println(set(right, positions, M, K));
         br.close();
     }
 
-    static boolean canPlace(int minDist, int[] positions, int M, int K) {
-        int lastPos = positions[0];
+    static boolean canSet(int mid, int[] positions, int M, int K) {
         M--;
-        for (int i = 1; i < K && M > 0; i++) {
-            if (positions[i] - lastPos >= minDist) {
-                lastPos = positions[i];
+        int lastPos = positions[0];
+        for (int i = 1; i < K; i++) {
+            if (positions[i] - lastPos >= mid) {
                 M--;
+                lastPos = positions[i];
+                if (M == 0) return true;
             }
         }
-        return M == 0;
+        return false;
+    }
+
+    static StringBuilder set(int mid, int[] positions, int M, int K) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        M--;
+        int lastPos = positions[0];
+        for (int i = 1; i < K; i++) {
+            if (M > 0 && positions[i] - lastPos >= mid) {
+                M--;
+                lastPos = positions[i];
+                sb.append(1);
+            } else {
+                sb.append(0);
+            }
+        }
+        return sb;
     }
 }
