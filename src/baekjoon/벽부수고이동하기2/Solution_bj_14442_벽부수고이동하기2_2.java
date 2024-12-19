d9e96bb9e6fafd1625a5a0285df38efc3df1c06e
wkc1002 <wkc1002@naver.com>
2024-12-19 18:55:47 +0900

    feat: Baekjoon 14442 벽 부수고 이동하기2 재풀이

--- src/baekjoon/벽부수고이동하기2/Solution_bj_14442_벽부수고이동하기2_2.java
+++ src/baekjoon/벽부수고이동하기2/Solution_bj_14442_벽부수고이동하기2_2.java
@@ -1,7 +1,9 @@
 package baekjoon.벽부수고이동하기2;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 /**
  * NxM행렬로 맵 표헌
@@ -11,15 +13,14 @@ import java.util.*;
  * 시작하는 칸과 끝나는 칸도 포함해서 센다.
  * K개의 벽을 부술 수 있다.
  */
-public class Solution_bj_14442_벽부수고이동하기 {
+public class Solution_bj_14442_벽부수고이동하기2_2 {
 
     static class Status {
-        int r, c, destroy, dist;
+        int r, c, dist;
 
-        public Status(int r, int c, int destroy, int dist) {
+        public Status(int r, int c, int dist) {
             this.r = r;
             this.c = c;
-            this.destroy = destroy;
             this.dist = dist;
         }
     }
@@ -31,48 +32,44 @@ public class Solution_bj_14442_벽부수고이동하기 {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[][] map = new int[N + 1][M + 1];
+        int[][] breakCnt = new int[N + 1][M + 1];
+
         for (int i = 1; i < N + 1; i++) {
             String s = br.readLine();
             for (int j = 1; j < M + 1; j++) {
                 map[i][j] = s.charAt(j - 1) - '0';
+                breakCnt[i][j] = Integer.MAX_VALUE;
             }
         }
         int answer = -1;
 
         ArrayDeque<Status> q = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[N + 1][M + 1][K + 1];
-        q.offer(new Status(1, 1, 0, 1));
-        visited[1][1][0] = true;
+        q.offer(new Status(1, 1, 1));
+        breakCnt[1][1] = 0;
 
         int[] dr = new int[]{-1, 0, 1, 0}, dc = new int[]{0, 1, 0, -1};
 
-        end: while (!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Status cur = q.poll();
-            again: for (int d = 0; d < 4; d++) {
+
+            if (cur.r == N && cur.c == M) {
+                answer = cur.dist;
+                break;
+            }
+
+            for (int d = 0; d < 4; d++) {
                 int nr = cur.r + dr[d];
                 int nc = cur.c + dc[d];
-                if (nr < 1 || nr > N || nc < 1 || nc > M || (map[nr][nc] == 1 && cur.destroy == K)) continue;
-                if (nr == N && nc == M) {
-                    answer = cur.dist + 1;
-                    break end;
-                }
 
-                for (int k = 0; k < cur.destroy + 1; k++) {
-                    if (visited[nr][nc][k]) continue again;
-                }
-                if (map[nr][nc] == 1) {
-                    if (visited[nr][nc][cur.destroy + 1]) continue;
-                    visited[nr][nc][cur.destroy + 1] = true;
-                    q.offer(new Status(nr, nc, cur.destroy + 1, cur.dist + 1));
-                } else {
-                    visited[nr][nc][cur.destroy] = true;
-                    q.offer(new Status(nr, nc, cur.destroy, cur.dist + 1));
-                }
+                if (nr < 1 || nr > N || nc < 1 || nc > M) continue;
+                int newBreak = breakCnt[cur.r][cur.c] + map[nr][nc];
 
+                if (newBreak > K || newBreak >= breakCnt[nr][nc]) continue;
+                breakCnt[nr][nc] = newBreak;
+                q.offer(new Status(nr, nc, cur.dist + 1));
             }
         }
 
-        if (N == 1 && M == 1) answer = 1;
         System.out.println(answer);
         br.close();
     }
