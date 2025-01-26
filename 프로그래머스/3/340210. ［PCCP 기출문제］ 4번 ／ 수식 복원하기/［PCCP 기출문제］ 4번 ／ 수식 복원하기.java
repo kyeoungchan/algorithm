1872802faabfba265271b607873af895410c0435
kyeongchan <wkc1002@naver.com>
2025-01-26 17:53:49 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 1.70 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class Solution {
     private String[] expressions;
-    private List<Integer> answerIndexes, completeIndexes;
-    private boolean possbileSystem;
+    private List<Integer> answerIndexes;
+    private boolean possibleSystem;
 
     public String[] solution(String[] expressions) throws Exception {
         this.expressions = expressions;
         answerIndexes = new ArrayList<>(); // X가 주어진 수식들의 인덱스를 담는 리스트
-        completeIndexes = new ArrayList<>(); // 완성된 수식들의 인덱스를 담는 리스트
         setIndexesList();
         String[] answer = new String[answerIndexes.size()];
 
@@ -19,72 +18,51 @@ public class Solution {
         List<Integer> firstDigits = new ArrayList<>();
         List<Integer> secondDigits = new ArrayList<>();
         List<Integer> resultDigits = new ArrayList<>();
-        for (int j = 0; j < expressions.length; j++) {
-            char operator = parseExpression(firstDigits, secondDigits, resultDigits, expressions[j]);
-            for (int i = numeralSystems.size() - 1; i >= 0; i--) {
-                int numeralSystem = numeralSystems.get(i);
-                possbileSystem = true;
+        for (int i = 0; i < expressions.length; i++) {
+            char operator = parseExpression(firstDigits, secondDigits, resultDigits, expressions[i]);
+            for (int j = numeralSystems.size() - 1; j >= 0; j--) {
+                int numeralSystem = numeralSystems.get(j);
+                possibleSystem = true;
                 int firstValueInTenSystem = getValueInTenSystem(firstDigits, numeralSystem);
-                if (!possbileSystem) {
-                    numeralSystems.remove(i);
+                if (!possibleSystem) {
+                    numeralSystems.remove(j);
                     continue;
                 }
                 int secondValueInTenSystem = getValueInTenSystem(secondDigits, numeralSystem);
-                if (!possbileSystem) {
-                    numeralSystems.remove(i);
+                if (!possibleSystem) {
+                    numeralSystems.remove(j);
                     continue;
                 }
-                if (answerIndexes.contains(j)) continue;
+                if (answerIndexes.contains(i)) {
+                    continue;
+                }
                 int resultValueInTenSystem = getValueInTenSystem(resultDigits, numeralSystem);
-                if (!possbileSystem) {
-                    numeralSystems.remove(i);
+                if (!possibleSystem) {
+                    numeralSystems.remove(j);
                     continue;
                 }
                 int calculateVal = calculate(firstValueInTenSystem, operator, secondValueInTenSystem);
                 if (calculateVal != resultValueInTenSystem) {
-                    numeralSystems.remove(i);
-//                    System.out.println("numeralSystems = " + numeralSystems);
+                    numeralSystems.remove(j);
                 }
             }
         }
-//        System.out.println(numeralSystems);
 
         int i = 0;
         StringBuilder sb = new StringBuilder();
         for (int index: answerIndexes) {
             String expression = expressions[index];
-            sb = new StringBuilder();
+            sb.delete(0, sb.length());
             int result = -1;
             boolean calculated = false;
             boolean questioned = false;
-//            sb.append(expression.substring(0, expression.length() - 1));
             sb.append(expression, 0, expression.length() - 1);
-//            System.out.println("sb = " + sb);
-            for (int j = numeralSystems.size() - 1; j >= 0; j--) {
-                int numeralSystem = numeralSystems.get(j);
-/*
-                System.out.println("numeralSystem = " + numeralSystem);
-                System.out.println("result = " + result);
-*/
+            for (int numeralSystem: numeralSystems) {
                 char operator = parseExpression(firstDigits, secondDigits, resultDigits, expression);
-                possbileSystem = true;
                 int firstValueInTenSystem = getValueInTenSystem(firstDigits, numeralSystem);
-                if (!possbileSystem) {
-                    numeralSystems.remove(j);
-                    continue;
-                }
                 int secondValueInTenSystem = getValueInTenSystem(secondDigits, numeralSystem);
-                if (!possbileSystem) {
-                    numeralSystems.remove(j);
-                    continue;
-                }
                 int calculateVal = calculate(firstValueInTenSystem, operator, secondValueInTenSystem);
-//                System.out.println("calculateVal = " + calculateVal);
                 calculateVal = getValueInNumeralSystem(calculateVal, numeralSystem);
-/*
-                System.out.println("calculateVal = " + calculateVal);
-                System.out.println();
-*/
                 if (!calculated) {
                     calculated = true;
                     result = calculateVal;
@@ -98,10 +76,16 @@ public class Solution {
                 sb.append(result);
             }
             answer[i++] = sb.toString();
-//            System.out.println("sb = " + sb);
         }
-//        System.out.println(sb.toString());
         return answer;
+    }
+
+    private void setIndexesList() {
+        for (int i = 0; i < expressions.length; i++) {
+            if (expressions[i].endsWith("X")) {
+                answerIndexes.add(i);
+            }
+        }
     }
 
     private int getValueInNumeralSystem(int valueInTenSystem, int numeralSystem) {
@@ -116,25 +100,17 @@ public class Solution {
     }
 
     private int getValueInTenSystem(List<Integer> digits, int numeralSystem) {
-/*
-        System.out.println("digits = " + digits);
-        System.out.println("numeralSystem = " + numeralSystem);
-*/
         int result = 0;
         int multi = 1;
         for (int i = digits.size() - 1; i >= 0; i--) {
             int digit = digits.get(i);
             if (digit >= numeralSystem) {
-                possbileSystem = false;
+                possibleSystem = false;
                 break;
             }
             result += digit * multi;
             multi *= numeralSystem;
         }
-/*
-        System.out.println("result = " + result);
-        System.out.println();
-*/
         return result;
     }
 
@@ -171,23 +147,7 @@ public class Solution {
                 operator = parsedChar;
             }
         }
-/*
-        System.out.println("firstDigits = " + firstDigits);
-        System.out.println("operator = " + operator);
-        System.out.println("secondDigits = " + secondDigits);
-        System.out.println("resultDigits = " + resultDigits);
-*/
         return operator;
-    }
-
-    private void setIndexesList() {
-        for (int i = 0; i < expressions.length; i++) {
-            if (expressions[i].endsWith("X")) {
-                answerIndexes.add(i);
-            } else {
-                completeIndexes.add(i);
-            }
-        }
     }
 
     private boolean isNumber(char parsed) {
