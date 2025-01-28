9de9cbbb0ff9a3a01954264aca0cbcd2f54c2a1c
wkc1002 <wkc1002@naver.com>
2025-01-28 16:36:14 +0900

    feat: Baekjoon 1325 효율적인 해킹 재풀이 완료

--- src/baekjoon/효율적인해킹/Solution_bj_1325_효율적인해킹.java
+++ src/baekjoon/효율적인해킹/Solution_bj_1325_효율적인해킹.java
@@ -6,58 +6,62 @@ import java.util.*;
 public class Solution_bj_1325_효율적인해킹 {
 
     static int N;
-    static int[] lengthInfo;
-    static Set<Integer>[] hackable;
-    static List<Integer> answers;
+    static int[] lengthInfo, visited;
+    static List<Integer>[] hackable;
+    static ArrayDeque<Integer> q;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        hackable = new Set[N + 1];
-        answers = new ArrayList<>();
+        hackable = new ArrayList[N + 1];
+        lengthInfo = new int[N + 1];
+        visited = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            hackable[i] = new ArrayList<>();
+            lengthInfo[i] = -1;
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if (hackable[b] == null) hackable[b] = new HashSet<>();
             hackable[b].add(a);
         }
 
-        lengthInfo = new int[N + 1];
-        Arrays.fill(lengthInfo, -1);
 
         int length = 0;
+        StringBuilder sb = new StringBuilder();
+        q = new ArrayDeque<>();
         for (int i = 1; i < N + 1; i++) {
             if (lengthInfo[i] != -1) continue;
             setLengthInfo(i);
             if (length == lengthInfo[i]) {
-                answers.add(i);
+                sb.append(i).append(" ");
             } else if (length < lengthInfo[i]) {
-                answers.clear();
-                answers.add(i);
+                sb.delete(0, sb.length());
+                sb.append(i).append(" ");
                 length = lengthInfo[i];
             }
         }
 
-        Collections.sort(answers);
-        StringBuilder sb = new StringBuilder();
-        for (int answer : answers) {
-            sb.append(answer).append(" ");
-        }
         System.out.println(sb.toString());
         br.close();
     }
 
     private static void setLengthInfo(int i) {
-        lengthInfo[i]++;
-        if (hackable[i] == null) return;
-        for (int hack : hackable[i]) {
-            if (lengthInfo[hack] == -1) setLengthInfo(hack);
-            lengthInfo[i] += lengthInfo[hack];
+        lengthInfo[i]++; // 이제 0
+        q.offer(i);
+        visited[i] = i;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
             lengthInfo[i]++;
+            for (int next: hackable[cur]) {
+                if (visited[next] == i) continue;
+                q.offer(next);
+                visited[next] = i;
+            }
         }
     }
 }
