5c8578cfc40d2275c4733945e0277ea0ff2800e2
kyeongchan <wkc1002@naver.com>
2025-08-09 18:44:45 +0900

    [level 4] Title: 도둑질, Time: 29.05 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,18 +1,15 @@
-/*
-* 이 마을에 있는 집은 3개 이상 1,000,000개 이하입니다.
-* money 배열의 각 원소는 0 이상 1,000 이하인 정수입니다.
-* */
 public class Solution {
     public int solution(int[] money) {
-        int[] dpO = new int[money.length];
-        int[] dpX = new int[money.length];
-        dpO[0] = dpO[1] = money[0]; // 0번째 집을 털었다면 1번째 집에 대한 메모도 무조건 0번째 집의 가격
-        dpX[1] = money[1]; // 1번째집부터 털기 시작했다면
+        int[] dp0 = new int[money.length];
+        int[] dp1 = new int[money.length];
+
+        dp0[0] = dp0[1] = money[0];
+        dp1[1] = money[1];
+
         for (int i = 2; i < money.length; i++) {
-            dpO[i] = Math.max(dpO[i - 1], dpO[i - 2] + money[i]);
-            dpX[i] = Math.max(dpX[i - 1], dpX[i - 2] + money[i]);
+            dp0[i] = Math.max(dp0[i - 1], dp0[i - 2] + money[i]);
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
         }
-        // 결국에는 첫번째를 찍었다면 마지막보다 전의 애와 첫번째를 안 찍었을 때의 마지막 애를 비교해야한다!
-        return Math.max(dpO[money.length - 2], dpX[money.length - 1]);
+        return Math.max(dp0[money.length - 2], dp1[money.length - 1]);
     }
 }
