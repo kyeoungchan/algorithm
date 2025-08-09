03454b58fa7d46025e9c58ca3c9dc7e0e201ed01
wkc1002 <wkc1002@naver.com>
2025-08-09 18:12:19 +0900

    feat: 프로그래머스 등굣길 - DP 재풀이 완료

---
+++ src/programmers/등굣길/Solution_pg_등굣길3.java
@@ -0,0 +1,24 @@
+package programmers.등굣길;
+
+public class Solution_pg_등굣길3 {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        dp[1][1] = 1;
+        for (int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (dp[i][j] == -1) {
+                    dp[i][j]++;
+                    continue;
+                }
+                dp[i][j] += dp[i - 1][j];
+                dp[i][j] += dp[i][j - 1];
+                dp[i][j] %= 1_000_000_007;
+            }
+        }
+        return dp[n][m];
+    }
+}
