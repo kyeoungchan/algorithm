62eeec67f97898d1adcf6c1002eed26a6c28488f
kyeongchan <wkc1002@naver.com>
2025-08-09 18:12:13 +0900

    [level 3] Title: 등굣길, Time: 0.87 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,22 +1,20 @@
-class Solution {
-    final int MOD = 1_000_000_007;
-    
+public class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n + 1][m + 1];
-        for (int[] pos : puddles) {
-            dp[pos[1]][pos[0]]--;
-        }
         dp[1][1] = 1;
+        for (int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
                 if (dp[i][j] == -1) {
-                    // 물 웅덩이라면 다음 길에 영향을 주지 않도록 0으로 변경 후 건너뛰기
                     dp[i][j]++;
                     continue;
                 }
                 dp[i][j] += dp[i - 1][j];
                 dp[i][j] += dp[i][j - 1];
-                dp[i][j] %= MOD;
+                dp[i][j] %= 1_000_000_007;
             }
         }
         return dp[n][m];
