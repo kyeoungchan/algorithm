f88bf265820312a53cb1c23b1fb05800732df3ce
wkc1002 <wkc1002@naver.com>
2025-08-09 20:03:08 +0900

    feat: Baekjoon 가장 긴 바이토닉 부분 수열 - DP

---
+++ src/baekjoon/가장긴바이토닉부분수열/Solution_bj_11054_가장긴바이토닉부분수열.java
@@ -0,0 +1,44 @@
+package baekjoon.가장긴바이토닉부분수열;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_11054_가장긴바이토닉부분수열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int[] increasingDp = new int[N]; // 오르막길 길이
+        int[] decreasingDp = new int[N]; // 내리막길 길이
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            increasingDp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    increasingDp[i] = Math.max(increasingDp[i], increasingDp[j] + 1);
+                }
+            }
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            decreasingDp[i] = 1;
+            for (int j = N - 1; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    decreasingDp[i] = Math.max(decreasingDp[i], decreasingDp[j] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, increasingDp[i] + decreasingDp[i] - 1);
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
