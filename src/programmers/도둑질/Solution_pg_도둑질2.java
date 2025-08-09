bf95d7d7648ce2a2ed8111d5ef99c4f72ad22ed5
wkc1002 <wkc1002@naver.com>
2025-08-09 18:44:49 +0900

    feat: 프로그래머스 도둑질 - DP 재풀이 완료

---
+++ src/programmers/도둑질/Solution_pg_도둑질2.java
@@ -0,0 +1,17 @@
+package programmers.도둑질;
+
+public class Solution_pg_도둑질2 {
+    public int solution(int[] money) {
+        int[] dp0 = new int[money.length];
+        int[] dp1 = new int[money.length];
+
+        dp0[0] = dp0[1] = money[0];
+        dp1[1] = money[1];
+
+        for (int i = 2; i < money.length; i++) {
+            dp0[i] = Math.max(dp0[i - 1], dp0[i - 2] + money[i]);
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+        }
+        return Math.max(dp0[money.length - 2], dp1[money.length - 1]);
+    }
+}
