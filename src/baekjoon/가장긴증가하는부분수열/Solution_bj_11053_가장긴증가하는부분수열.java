1e921b0c51aa0db8c717c95abf43b262ff5571c3
wkc1002 <wkc1002@naver.com>
2025-08-24 15:23:44 +0900

    feat: Baekjoon 가장 긴 증가하는 부분수열 DP

---
+++ src/baekjoon/가장긴증가하는부분수열/Solution_bj_11053_가장긴증가하는부분수열.java
@@ -0,0 +1,31 @@
+package baekjoon.가장긴증가하는부분수열;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_11053_가장긴증가하는부분수열 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A  = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        int max = dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        System.out.println(max);
+        br.close();
+    }
+}
