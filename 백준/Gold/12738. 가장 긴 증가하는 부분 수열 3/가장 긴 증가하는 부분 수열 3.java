4d50a376f75ce3c7899c4d46f46f77305e30f927
kyeongchan <wkc1002@naver.com>
2025-09-04 16:59:50 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 496 ms, Memory: 169536 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            int[] A = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int length = 0;
+            int[] dp = new int[N];
+            dp[length++] = A[0];
+            for (int i = 1; i < N; i++) {
+                if (A[i] > dp[length - 1]) {
+                    dp[length++] = A[i];
+                } else if (A[i] < dp[length - 1]) {
+                    int left = 0;
+                    int right = length - 1;
+                    while (left <= right) {
+                        int mid = left + (right - left) / 2;
+                        if (A[i] > dp[mid]) {
+                            left = mid + 1;
+                        } else {
+                            right = mid - 1;
+                        }
+                    }
+                    dp[left] = A[i];
+                }
+            }
+            System.out.println(length);
+        }
+    }
+}
