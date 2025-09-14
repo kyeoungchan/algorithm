ee537d2b0db7dda9d1f6877a5e636f4a4c2a1d94
wkc1002 <wkc1002@naver.com>
2025-09-14 21:00:24 +0900

    feat: 프로그래머스 카카오 기출 - 가장 많이 받은 선물

---
+++ src/programmers/가장많이받은선물/Solution_pg_가장많이받은선물.java
@@ -0,0 +1,45 @@
+package programmers.가장많이받은선물;
+
+import java.util.*;
+
+public class Solution_pg_가장많이받은선물 {
+
+    public int solution(String[] friends, String[] gifts) {
+
+        Map<String, Integer> idxMap = new HashMap<>();
+
+        for (int i = 0; i < friends.length; i++) {
+            idxMap.put(friends[i], i);
+        }
+
+        int[][] gaveGifts = new int[friends.length][friends.length];
+        int[] giftScores = new int[friends.length];
+        StringTokenizer st;
+
+        for (String gift : gifts) {
+            st = new StringTokenizer(gift, " ");
+            int fromIdx = idxMap.get(st.nextToken());
+            int toIdx = idxMap.get(st.nextToken());
+            gaveGifts[fromIdx][toIdx]++;
+            giftScores[fromIdx]++;
+            giftScores[toIdx]--;
+        }
+
+        int[] recGifts = new int[friends.length];
+        int answer = 0;
+
+        for (int i = 0; i < friends.length; i++) {
+            for (int j = 0; j < friends.length; j++) {
+                if (i == j) continue;
+                if (gaveGifts[i][j] > gaveGifts[j][i]) {
+                    recGifts[i]++;
+                } else if (gaveGifts[i][j] == gaveGifts[j][i] && giftScores[i] > giftScores[j]) {
+                    recGifts[i]++;
+                }
+            }
+            answer = Math.max(recGifts[i], answer);
+        }
+
+        return answer;
+    }
+}
