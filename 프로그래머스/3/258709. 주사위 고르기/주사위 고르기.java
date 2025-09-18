a153f8ce5d24da4a1e49761928b8a827f9366016
kyeongchan <wkc1002@naver.com>
2025-09-18 09:12:31 +0900

    [level 3] Title: 주사위 고르기, Time: 754.32 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,93 +1,105 @@
 import java.util.*;
 
-public class Solution {
-
-    private int N, max = Integer.MIN_VALUE;
-    private int[] answer;
-    private int[][] dices;
-    private List<Integer> choice, listA, listB;
+class Solution {
+    
+    private int n, max;
+    private int[] answer, diceA, diceB;
+    private int[][] dice;
+    //private List<Integer> listA, listB;
     
     public int[] solution(int[][] dice) {
-        N = dice.length;
-        dices = dice;
-        answer = new int[N / 2];
-        choice = new ArrayList<>();
-        choosedice(0, 0);
+        n = dice.length;
+        this.dice = dice;
+        answer = new int[n/2];
+        max = 0;
+        
+        diceA = new int[n/2];
+        //listA = new ArrayList<>();
+        //listB = new ArrayList<>();
+        
+        choose(0, 1);
+        
         return answer;
     }
-
-    private void choosedice(int depth, int s) {
-        if (depth == N / 2) {
-            int winning = calculateWinningPercent();
-            if (max < winning) {
-                max = winning;
-                for (int i = 0; i < choice.size(); i++) {
-                    answer[i] = choice.get(i) + 1;
+    
+    private void choose(int depth, int number) {
+        if (depth == n/2) {
+            
+            diceB = new int[n/2];
+            
+            int aIdx = 0, bIdx = 0;
+            for (int num = 1; num <= n; num++) {
+                if (aIdx < n/2 && diceA[aIdx] == num) {
+                    aIdx++;
+                } else {
+                    diceB[bIdx++] = num;
+                }
+            }
+            //printDices();
+            
+            List<Integer> listA = new ArrayList<>();
+            throwDice(0, 0, listA, diceA);
+            //System.out.println("listA: " + listA);
+            List<Integer> listB = new ArrayList<>();
+            throwDice(0, 0, listB, diceB);
+            //System.out.println("listB: " + listB);
+            //System.out.println();
+            
+            Collections.sort(listB);
+            int winningCount = 0;
+            for (int a: listA) {
+                int left = 0, right = listB.size() - 1, idx = 0;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (a > listB.get(mid)) {
+                        idx = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                winningCount += idx;
+            }
+            if (winningCount > max) {
+                max = winningCount;
+                for (int i = 0; i < n/2; i++) {
+                    answer[i] = diceA[i];
                 }
             }
             return;
         }
-
-        for (int i = s; i < N; i++) {
-            choice.add(i);
-            choosedice(depth + 1, i + 1);
-            choice.remove(choice.size() - 1);
-        }
-    }
-
-    private int calculateWinningPercent() {
-        int count = 0;
         
-        makeListAB();
-
-        Collections.sort(listB);
-        for (Integer aNumber : listA) {
-            int left = 0, right = listB.size() - 1;
-
-            int idx = -1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (listB.get(mid) < aNumber) {
-                    idx = mid;
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            count += idx + 1;
-        }
-        return count;
+        if (number > n) return;
+        
+        diceA[depth] = number;
+        choose(depth + 1, number + 1);
+        diceA[depth] = 0;
+        choose(depth, number + 1);
     }
     
-    private void makeListAB() {
-        listA = new ArrayList<>();
-        listB = new ArrayList<>();
-
-        int[][] diceA = new int[N / 2][6];
-        int[][] diceB = new int[N / 2][6];
-        int a = 0, b = 0;
-        for (int i = 0; i < N; i++) {
-            if (choice.contains(i)) {
-                diceA[a] = dices[i];
-                a++;
-            } else {
-                diceB[b] = dices[i];
-                b++;
-            }
-        }
-
-        makeList(0, diceA, 0, listA);
-        makeList(0, diceB, 0, listB);
-    }
-
-    private void makeList(int depth, int[][] dice, int sum, List<Integer> list) {
-        if (depth == N / 2) {
+    private void throwDice(int depth, int sum, List<Integer> list, int[] diceAB) {
+        if (depth == n/2) {
             list.add(sum);
             return;
         }
+        
+        int newSum = sum;
+        int targetNum = diceAB[depth];
         for (int i = 0; i < 6; i++) {
-            int newSum = sum + dice[depth][i];
-            makeList(depth + 1, dice, newSum, list);
+            newSum += dice[targetNum - 1][i];
+            throwDice(depth + 1, newSum, list, diceAB);
+            newSum -= dice[targetNum - 1][i];
         }
+    }
+    
+    private void printDices() {
+        System.out.println("diceA");
+        System.out.println(Arrays.toString(diceA));
+        System.out.println();
+        
+        System.out.println("diceB");
+        System.out.println(Arrays.toString(diceB));
+        System.out.println();
+        System.out.println();
     }
 }
