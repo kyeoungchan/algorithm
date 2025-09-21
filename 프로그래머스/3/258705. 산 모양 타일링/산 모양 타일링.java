6ed7eb083fd01399f5c290c88612ca942ea04b3d
kyeongchan <wkc1002@naver.com>
2025-09-21 16:41:43 +0900

    [level 3] Title: 산 모양 타일링, Time: 19.05 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] tops) {
         int mod = 10_007;
-        // dp[n][0] = 마름모 타일로 끝나지 않은 경우의 수
-        // dp[n][1] = 마름모 타일로 끝나는 경우의 수
         int[][] dp = new int[n][2];
-        dp[0][0] = tops[0] == 1 ? 3: 2;
+        //[n][0]: 윗변이 n일 때, 제일 오른쪽에 마름모를 붙이지 않은 경우
+        //[n][1]: 윗변이 n일 때, 제일 오른쪽에 마름모를 붙인 경우
+        dp[0][0] = tops[0] == 1 ? 3 : 2;
         dp[0][1] = 1;
         for (int i = 1; i < n; i++) {
-            int mul1 = tops[i] == 1? 3 : 2;
+            int mul1 = tops[i] == 1 ? 3 : 2;
             int mul2 = tops[i] == 1 ? 2 : 1;
             dp[i][0] = (dp[i-1][0] * mul1 + dp[i-1][1] * mul2) % mod;
             dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % mod;
