3f6b835ab53a4ed49f79a3bc6b50341f1d1708a2
wkc1002 <wkc1002@naver.com>
2026-02-21 18:51:24 +0900

    ref: 백준 - DP 동전1

---
+++ src/baekjoon/동전1/Solution_bj_2293_동전1.java
@@ -0,0 +1,42 @@
+package baekjoon.동전1;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * n 종류의 동전이 있다.
+ * 그 가치의 합이 k원이 되도록 하고 싶다.
+ * 경우의 수를 구하라.
+ * 구성이 같지만 순서만 다른 것은 같은 경우다.
+ *
+ * 참고: https://lotuslee.tistory.com/113
+ */
+public class Solution_bj_2293_동전1 {
+
+    static int n, k;
+    static int[] coins, dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        dp = new int[k + 1];
+        dp[0] = 1;
+
+        coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+            for (int j = 1; j <= k; j++) {
+                if (j - coins[i] < 0) continue;
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+
+        br.close();
+    }
+
+}
