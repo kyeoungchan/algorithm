414d992096d046c0db98232db0226b8c1ab8c845
kyeongchan <wkc1002@naver.com>
2026-02-26 07:18:32 +0900

    [Gold IV] Title: 타일 채우기, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 3XN 크기의 벽을 2X1, 1X2 크기의 타일로 채우는 경우의 수를 구해보자.
+ * https://www.acmicpc.net/problem/2133
+ * 참고: https://eunchaan.tistory.com/51
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        if (n % 2 == 1) {
+            System.out.println(0);
+        } else if (n >= 2) {
+            dp[0] = 1;
+            dp[2] = 3;
+            for (int i = 4; i <= n; i += 2) {
+                // 우측에 새롭게 dp[2]를 붙인 경우의 수.
+                dp[i] = dp[i - 2] * dp[2];
+                for (int j = i - 4; j > 0; j -= 2) {
+                    /* 우측 2줄에 새로운 타일을 붙이는 경우의 수가 아니라
+                    * 우측 4줄이 서로 연결되어 있는 경우의 수(2개)
+                    * 우측 6줄이 서로 연결되어 있는 경우의 수(2개)
+                    * ...
+                    * 다른 블로그에서는 이 경우를 특별한 경우라고 지칭하고들 있다. */
+                    dp[i] += dp[j] * 2;
+                }
+                // 마지막으로 모든 줄이 연결되어 있는 경우의 수(2개)
+                dp[i] += 2;
+            }
+            System.out.println(dp[n]);
+        }
+        br.close();
+    }
+}
