6d37c6167054fd4932c58e673f505861c2f37e48
wkc1002 <wkc1002@naver.com>
2026-03-02 01:01:50 +0900

    feat: 백준 - DP 타일채우기 재풀이 완료

---
+++ src/baekjoon/타일채우기/Solution_bj_2133_타일채우기2.java
@@ -0,0 +1,26 @@
+package baekjoon.타일채우기;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_2133_타일채우기2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N % 2 == 1) {
+            System.out.println(0);
+        } else {
+            int[] dp = new int[N + 1];
+            dp[0] = 1;
+            dp[2] = 3;
+            for (int i = 4; i <= N; i += 2) {
+                dp[i] = dp[i - 2] * dp[2];
+                for (int j = 0; j < i - 2; j += 2) {
+                    dp[i] += dp[j] * 2;
+                }
+            }
+            System.out.println(dp[N]);
+        }
+        br.close();
+    }
+}
