2f5a9ebc4d9bbe25a87dcfcf99e5ee7d03d22a3a
wkc1002 <wkc1002@naver.com>
2026-03-03 23:21:32 +0900

    feat: 백준 - DP 행렬 곱셈 순서 - 재풀이 완료

---
+++ src/baekjoon/행렬곱셈순서/Solution_bj_11049_행렬곱셈순서2.java
@@ -0,0 +1,48 @@
+package baekjoon.행렬곱셈순서;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_11049_행렬곱셈순서2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+
+        // 각 행렬의 행 수와 마지막 행렬의 열
+        int[][] matrices = new int[N][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            matrices[i][0] = Integer.parseInt(st.nextToken());
+            matrices[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N][N];
+        for (int s = 0; s < N - 1; s++) {
+            dp[s][s + 1] = matrices[s][0] * matrices[s][1] * matrices[s + 1][1];
+        }
+
+        for (int d = 2; d < N; d++) {
+            for (int s = 0; s < N - d; s++) {
+                int min = Integer.MAX_VALUE;
+
+                for (int d2 = 0; d2 < d; d2++) {
+                    int A = matrices[s][0];
+                    int B = matrices[s + d2][1];
+                    int C = matrices[s + d][1];
+
+                    int ops = A * B * C;
+                    ops += dp[s][s + d2];
+                    ops += dp[s + d2 + 1][s + d];
+                    min = Math.min(min, ops);
+                }
+
+                dp[s][s + d] = min;
+            }
+        }
+
+        System.out.println(dp[0][N - 1]);
+        br.close();
+    }
+}
