7ebca9139aa52160049129b5abb3501a3bf71e75
kyeongchan <wkc1002@naver.com>
2026-04-03 23:07:19 +0900

    [Gold IV] Title: 좋다, Time: 188 ms, Memory: 13296 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        Set<Integer> set = new HashSet<>();
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            int target = list.get(i);
+            if (set.contains(target)) {
+                answer++;
+                continue;
+            }
+
+            int left = 0;
+            int right = N - 1;
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+                int sum = list.get(left) + list.get(right);
+                if (target == sum) {
+                    answer++;
+                    set.add(target);
+                    break;
+                }
+                if (sum > target) {
+                    right--;
+                }  else {
+                    left++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
