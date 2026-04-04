7682633224481dc6f3962691344334006a506b5e
kyeongchan <wkc1002@naver.com>
2026-04-04 14:54:51 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 2136 ms, Memory: 26624 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -2,35 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[n + 1][m + 1];
-        int[][] grid = new int[n + 1][m + 1];
 
+        int[][] grid = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i <= n; i++) {
             String s = br.readLine();
             for (int j = 1; j <= m; j++) {
-                grid[i][j] = s.charAt(j-1) - '0';
+                grid[i][j] = s.charAt(j - 1) - '0';
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + grid[i][j];
             }
         }
 
-/*
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                System.out.print(dp[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-*/
-
-        int result = 0;
+        int answer = 0;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
@@ -40,29 +29,17 @@ public class Main {
                 while (i + d <= n && j + d <= m) {
                     int ni = i + d;
                     int nj = j + d;
+                    int temp2 = dp[ni][nj] - dp[ni][j - 1] - dp[i - 1][nj] + dp[i - 1][j - 1];
                     d++;
-                    int temp2 = d * d;
-/*
-                    System.out.println("ni = " + ni);
-                    System.out.println("nj = " + nj);
-                    System.out.println("d = " + d);
-                    System.out.println("temp = " + temp);
-                    System.out.println("temp2 = " + temp2);
-                    System.out.println("cal = " + (dp[ni][nj] - dp[ni][j] - dp[i][nj] + dp[ni][nj]));
-                    System.out.println("result = " + result);
-                    System.out.println();
-*/
-                    if (grid[ni][nj] == 0 || dp[ni][nj] - dp[ni][j-1] - dp[i-1][nj] + dp[i-1][j-1] != temp2) {
-//                        result = Math.max(result, temp);
+                    if (grid[ni][nj] == 0 || temp2 != d * d) {
                         break;
                     }
                     temp = temp2;
                 }
-                result = Math.max(result, temp);
+                answer = Math.max(answer, temp);
             }
         }
-
-        System.out.println(result);
+        System.out.println(answer);
         br.close();
     }
 }
