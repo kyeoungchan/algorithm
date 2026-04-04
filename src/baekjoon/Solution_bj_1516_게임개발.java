32441e358e55148665fd599aa5632f0e4884c5f3
wkc1002 <wkc1002@naver.com>
2026-04-04 17:31:04 +0900

    feat: 백준 - DP 게임 개발

---
+++ src/baekjoon/Solution_bj_1516_게임개발.java
@@ -0,0 +1,61 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_bj_1516_게임개발 {
+
+    static int N;
+    static int[] time, dp;
+    static List<Integer>[] before;
+
+    static int INF = 100_000 * 500 + 1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        time = new int[N + 1];
+        before = new List[N + 1];
+        dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            before[i] = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            while (true) {
+                int b =  Integer.parseInt(st.nextToken());
+                if (b == -1) {
+                    break;
+                }
+                before[i].add(b);
+            }
+            dp[i] = INF;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            if (dp[i] != INF) continue;
+            dp[i] = calculate(i);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(dp[i]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+    static int calculate(int num) {
+        if (dp[num] != INF) return dp[num];
+
+        int max = 0;
+        for (int bNum : before[num]) {
+            if (dp[bNum] == INF) {
+                dp[bNum] = calculate(bNum);
+            }
+            max = Math.max(max, dp[bNum]);
+        }
+        return dp[num] = max + time[num];
+    }
+}
