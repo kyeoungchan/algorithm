bf11c6cae8361f9607d18d19fd674f514932f368
kyeongchan <wkc1002@naver.com>
2026-04-28 17:15:14 +0900

    [level 2] Title: 타겟 넘버, Time: 6.60 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 70.9 MB, 시간: 7.76 ms
+메모리: 80.5 MB, 시간: 6.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 13일 21:28:40
+2026년 04월 28일 17:15:10
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,32 +1,31 @@
-import java.util.*;
+class Solution {
+     private int target, sum, answer;
+    private int[] numbers;
 
-/**
- * n개의 음이 아닌 정수들이 있다.
- * 이 정수들을 순설르 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만드려고 한다.
- * 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성하라.
- * 주어지는 숫자는 2개 이상 20개 이하
- * 1 <= target <= 1000
- */
-public class Solution {
-
-    private int answer;
-    private int n;
-    
     public int solution(int[] numbers, int target) {
+        this.target = target;
+        this.numbers = numbers;
         answer = 0;
-        this.n = numbers.length;
-        backtracking(0, numbers, target, 0);
-        
+        sum = 0;
+
+        check(0);
         return answer;
     }
-    
-    private void backtracking(int index, int[] numbers, int target, int result) {
-        if (index == n) {
-            if (result == target) answer++;
+
+    private void check(int idx) {
+        if (idx == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
             return;
         }
 
-        backtracking(index + 1, numbers, target, result + numbers[index]);
-        backtracking(index + 1, numbers, target, result - numbers[index]);
+        sum += numbers[idx];
+        check(idx + 1);
+        sum -= numbers[idx];
+
+        sum -= numbers[idx];
+        check(idx + 1);
+        sum += numbers[idx];
     }
 }
