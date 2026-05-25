87da61fb2a1a25051e706bbef241019be2586563
kyeongchan <wkc1002@naver.com>
2026-05-26 07:01:53 +0900

    [level 2] Title: 선인장 숨기기, Time: 418.26 ms, Memory: 463 MB -BaekjoonHub

---
+++ 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    
+    int m, n, h, w;
+    int[][] grid;
+    
+    public int[] solution(int m, int n, int h, int w, int[][] drops) {
+        this.m = m;
+        this.n = n;
+        this.h = h;
+        this.w = w;
+        grid = new int[m][n];
+        for (int[] row: grid) {
+            Arrays.fill(row, drops.length);
+        }
+        
+        for (int i = 0; i < drops.length; i++) {
+            int r = drops[i][0];
+            int c = drops[i][1];
+            if (grid[r][c] != drops.length) continue;
+            grid[r][c] = i;
+        }
+        
+        int[] answer = {0,0};
+        
+        int left = 0;
+        int right = drops.length;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int[] pos = findDriedZone(mid);
+            if (pos != null) {
+                answer = pos;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+    
+    int[] findDriedZone(int mid) {
+        int[][] pSum = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j] <= mid ? 1 : 0;
+                pSum[i + 1][j + 1] = val + pSum[i][j + 1] + pSum[i + 1][j] - pSum[i][j];
+            }
+        }
+        
+        for (int i = h; i <= m; i++) {
+            for (int j = w; j <= n; j++) {
+                int sum = pSum[i][j] - pSum[i-h][j] - pSum[i][j-w] + pSum[i-h][j-w];
+                if (sum == 0) return new int[] {i-h, j-w};
+            }
+        }
+        return null;
+    }
+}
