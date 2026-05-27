34c37d124a4cb8e5cdca7130f32a81b9947372f9
kyeongchan <wkc1002@naver.com>
2026-05-28 06:55:26 +0900

    [level 3] Title: 기차 선로, Time: 19.22 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/3/468381. 기차 선로/기차 선로.java
+++ 프로그래머스/3/468381. 기차 선로/기차 선로.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     
     int n, m, answer;
@@ -5,9 +7,9 @@ class Solution {
     int[][] grid, visited;
     
     public int solution(int[][] grid) {
-        this.grid = grid;
         n = grid.length;
         m = grid[0].length;
+        this.grid = grid;
         visited = new int[n][m];
         
         answer = 0;
@@ -19,91 +21,93 @@ class Solution {
     }
     
     void backtracking(int r, int c, int d) {
-        // System.out.println("d:" + d);
         // printGrid();
         if (r == n - 1 && c == m - 1) {
+            // System.out.println("도착!");
             if (allVisited()) {
                 answer++;
+                // System.out.println("answer: " + answer);
             }
-            // printGrid();
-            // System.out.println("answer: " + answer);
             return;
         }
         
         int nr = r + dr[d];
         int nc = c + dc[d];
-        // 낭떠러지면 리턴
         if (nr < 0 || nr >= n || nc < 0 || nc >= m) return;
-        // System.out.println("nr: " + nr + ", nc: " + nc);
-        int nextType = grid[nr][nc];
+        int newType = grid[nr][nc];
+        if (newType == -1) return;
         
-        // 벽이면 리턴
-        if (nextType == -1) return;
-        
-        if (nextType == 0) {
+        if (newType == 0) {
             for (int i = 1; i <= 7; i++) {
-                // 저쪽에서 이쪽으로 연결될 수 없는 경우라면 스킵
                 if (!canOut(i, (d + 2) % 4)) continue;
                 int nd = getNextDir(i, d);
-                // System.out.println("nd: " + nd);
-                grid[nr][nc] = i;
                 visited[nr][nc]++;
+                grid[nr][nc] = i;
                 backtracking(nr, nc, nd);
-                visited[nr][nc]--;
                 grid[nr][nc] = 0;
+                visited[nr][nc]--;
             }
         } else {
-            // 3이 아니고 한 번 이상 방문한적이 있다면 리턴
-            if (nextType != 3 && visited[nr][nc] > 0) return;
-            
-            // 3일 때 2번 방문한적이 있다면 리턴
-            if (nextType == 3 && visited[nr][nc] >= 2) return;
-
-            // 저쪽에서 이쪽으로 연결될 수 없는 경우라면 리턴
-            if (!canOut(nextType, (d + 2) % 4)) return;
-            
-            int nd = getNextDir(nextType, d); 
+            if (!canOut(newType, (d + 2) % 4)) return;
+            if (newType == 3 && visited[nr][nc] == 2) return;
+            if (newType != 3 && visited[nr][nc] == 1) return;
+            int nd = getNextDir(newType, d);
             visited[nr][nc]++;
             backtracking(nr, nc, nd);
             visited[nr][nc]--;
         }
-        
-        
-    }
-    
-    int getNextDir(int type, int curD) {
-        // 0과 -1의 경우는 생각지 않는다.
-        if (type <= 3) {
-            return curD;
-        } else if (type == 4) {
-            return curD == 1 ? 0 : 3;
-        } else if (type == 5) {
-            return curD == 2 ? 1 : 0;
-        } else if (type == 6) {
-            return curD == 3 ? 2 : 1;
-        } else {
-            return curD == 1 ? 2 : 3;
-        }
-    }
-    
-    boolean canOut(int curType, int d) {
-        if (curType < 3) return (curType + d) % 2 == 0;
-        else if (curType == 3) return true;
-        else return (curType - d) % 4 == 0 || (curType - d) % 4 == 1;
     }
     
     boolean allVisited() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 0 || grid[i][j] == -1) continue;
+                if (grid[i][j] == -1 || grid[i][j] == 0) continue;
                 if (grid[i][j] == 3 && visited[i][j] != 2) return false;
-                if (visited[i][j] == 0) return false;
+                if (grid[i][j] != 3 && visited[i][j] != 1) return false;
             }
         }
         return true;
     }
     
-    private void printGrid() {
+    boolean canOut(int type, int d) {
+        if (type == 1) {
+            return d % 2 == 1;
+        } else if (type == 2) {
+            return d % 2 == 0;
+        } else if (type == 3) {
+            return true;
+        } else if (type == 4) {
+            return d == 0 || d == 3;
+        } else if (type == 5) {
+            return d == 0 || d == 1;
+        } else if (type == 6) {
+            return d == 1 || d == 2;
+        } else if (type == 7) {
+            return d == 2 || d == 3;
+        }
+
+        // type이 0이거나 -1인 경우로, 호출되면 안되는 시점
+        throw new RuntimeException("잘못된 호출1");
+    }
+    
+    int getNextDir(int type, int inDir) {
+        if (type <= 0) {
+            // type이 0이거나 -1인 경우로, 호출되면 안되는 시점
+            throw new RuntimeException("잘못된 호출2");
+        } else if (type <= 3) {
+            return inDir;
+        } else if (type == 4) {
+            return inDir == 2 ? 3 : 0;
+        } else if (type == 5) {
+            return inDir == 2 ? 1 : 0;
+        } else if (type == 6) {
+            return inDir == 0 ? 1 : 2;
+        } else {
+            return inDir == 0 ? 3 : 2;
+        }
+    }
+    
+    void printGrid() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.printf("%2d", grid[i][j]);
