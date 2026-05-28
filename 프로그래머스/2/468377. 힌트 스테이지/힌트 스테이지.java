bc75a748123faf1d2a9026c78108386e71876680
kyeongchan <wkc1002@naver.com>
2026-05-28 21:37:52 +0900

    [level 2] Title: 힌트 스테이지, Time: 4.06 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -2,47 +2,49 @@ import java.util.*;
 
 class Solution {
     
-    int answer, n;
-    int[] havingHints;
+    int answer, n, k;
+    int[] hintCount;
     int[][] cost, hint;
     
     public int solution(int[][] cost, int[][] hint) {
+        
         answer = Integer.MAX_VALUE;
-        // 스테이지 개수
         n = cost.length;
+        k = hint[0].length - 1;
+        hintCount = new int[n];
         this.cost = cost;
         this.hint = hint;
         
-        havingHints = new int[n];
-        
-        solve(0, 0);
+        backtracking(0, 0);
         
         return answer;
     }
     
-    private void solve(int idx, int costVal) {
-        if (idx == n) {
-            answer = Math.min(answer, costVal);
+    void backtracking(int curCost, int stageIdx) {
+        if (stageIdx == n) {
+            answer = Math.min(curCost, answer);
             return;
         }
         
-        // 힌트 번들을 사지 않은 경우
-        solve(idx + 1, costVal + cost[idx][Math.min(havingHints[idx], n-1)]);
         
-        if (idx != n - 1) {
-            // 힌트 번들을 산 경우
-            costVal += hint[idx][0];
-            for (int i = 1; i < hint[idx].length; i++) {
-                havingHints[hint[idx][i] - 1]++;
-            }
-
-            solve(idx + 1, costVal + cost[idx][Math.min(havingHints[idx], n-1)]);
-
-            // 원복
-            for (int i = 1; i < hint[idx].length; i++) {
-                havingHints[hint[idx][i] - 1]--;
-            }    
+        // 힌트를 사용하지 않고 깨는 경우
+        int curHintCount = Math.min(hintCount[stageIdx], n - 1);
+        backtracking(curCost + cost[stageIdx][curHintCount], stageIdx + 1);
+        
+        // 마지막 스테이지에서는 힌트를 구매할 수 없다.(의미도 없고)
+        if (stageIdx == n - 1) return;
+        
+        // 힌트를 사용하는 경우
+        curCost += hint[stageIdx][0];
+        for (int i = 1; i <= k; i++) {
+            int hintNum = hint[stageIdx][i];
+            hintCount[hintNum-1]++;
         }
-        
+        curHintCount = Math.min(hintCount[stageIdx], n - 1);
+        backtracking(curCost + cost[stageIdx][curHintCount], stageIdx + 1);
+        for (int i = 1; i <= k; i++) {
+            int hintNum = hint[stageIdx][i];
+            hintCount[hintNum-1]--;
+        }
     }
 }
