95b63e0f6e13cb4941324dc5788830cfa1e4d366
kyeongchan <wkc1002@naver.com>
2026-06-04 08:13:36 +0900

    [level 3] Title: 최고 속도, Time: 264.22 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/3/468376. 최고 속도/최고 속도.java
+++ 프로그래머스/3/468376. 최고 속도/최고 속도.java
@@ -2,7 +2,15 @@ import java.util.*;
 
 class Solution {
     
-    static final int INF = 1_000_000_001;
+    static class Point {
+        int x, y, limit;
+        
+        Point(int x, int y, int limit) {
+            this.x = x;
+            this.y = y;
+            this.limit = limit;
+        }
+    }
     
     static class Road {
         int x1, y1, x2, y2, limit;
@@ -30,186 +38,191 @@ class Solution {
         }
     }
     
-    static class State implements Comparable<State> {
-        int node, limit;
+    static class Status implements Comparable<Status> {
+        int id, limit;
         
-        State(int node, int limit) {
-            this.node = node;
+        Status (int id, int limit) {
+            this.id = id;
             this.limit = limit;
         }
         
         @Override
-        public int compareTo(State o) {
+        public int compareTo(Status o) {
             return Integer.compare(o.limit, limit);
         }
     }
     
-    Map<Long, Integer> idMap;
-    List<Integer> xs, ys, limits;
-    List<Integer>[] graph;
+    static final int INF = 1_000_000_001;
+    int n, m;
     int[] cityNodes;
+    List<Point> points;
+    Map<Long, Integer> idMap;
+    Road[] roads;
     
     public int[] solution(int[][] city, int[][] road) {
-        int n = city.length;
-        int m = road.length;
         
-        Road[] roads = new Road[m];
-        for (int i = 0; i < m; i++) {
-            roads[i] = new Road(road[i][0], road[i][1], road[i][2], road[i][3], road[i][4]);
-        }
-        
-        idMap = new HashMap<>();
-        xs = new ArrayList<>();
-        ys = new ArrayList<>();
-        limits = new ArrayList<>();
+        n = city.length;
+        m = road.length;
         cityNodes = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            cityNodes[i] = getNode(city[i][0], city[i][1]);
-        }
-        
-        List<Integer>[] roadPoints = new List[m];
-        for (int i = 0; i < m; i++) roadPoints[i] = new ArrayList<>();
-        
-        for (int i = 0; i < m; i++) {
-            int mx = (roads[i].x1 + roads[i].x2) / 2;
-            int my = (roads[i].y1 + roads[i].y2) / 2;
-            int id = getNode(mx, my);
-            updateCamera(id, roads[i].limit);
-            roadPoints[i].add(id);
-        }
+        points = new ArrayList<>();
+        idMap = new HashMap<>();
+        roads = new Road[m];
         
         for (int i = 0; i < n; i++) {
             int x = city[i][0];
             int y = city[i][1];
-            for (int j = 0; j < m; j++) {
-                if (onRoad(x, y, roads[j])) {
-                    roadPoints[j].add(cityNodes[i]);
-                }
-            }
+            cityNodes[i] = getNode(x, y, INF);
+        }
+        
+        List<Integer>[] nodeRoads = new List[m];
+        
+        for (int i = 0; i < m; i++) {
+            int x1 = road[i][0];
+            int y1 = road[i][1];
+            int x2 = road[i][2];
+            int y2 = road[i][3];
+            int limit = road[i][4];
+            roads[i] = new Road(x1, y1, x2, y2, limit);
+            
+            nodeRoads[i] = new ArrayList<>();
+            int mx = (x1 + x2) / 2;
+            int my = (y1 + y2) / 2;
+            int id = getNode(mx, my, limit);
+            nodeRoads[i].add(id);
         }
         
         for (int i = 0; i < m - 1; i++) {
             for (int j = i + 1; j < m; j++) {
-                int[] point = getIntersect(roads[i], roads[j]);
-                if (point == null) continue;
-                
-                int id = getNode(point[0], point[1]);
-                roadPoints[i].add(id);
-                roadPoints[j].add(id);
+                Road r1 = roads[i];
+                Road r2 = roads[j];
+                int id = getIntersect(r1, r2);
+                if (id == -1) continue;
+                nodeRoads[i].add(id);
+                nodeRoads[j].add(id);
             }
         }
         
-        int nodeCount = xs.size();
-        graph = new List[nodeCount];
+        for (int i = 0; i < n; i++) {
+            int id = cityNodes[i];
+            Point point = points.get(id);
+            for (int j = 0; j < m; j++) {
+                if (isOnRoad(point, roads[j])) {
+                    nodeRoads[j].add(id);
+                }
+            }
+        }
+        
+        int nodeCount = points.size();
+        List<Integer>[] graph = new List[nodeCount];
         for (int i = 0; i < nodeCount; i++) graph[i] = new ArrayList<>();
         
+        List<PointOnRoad> pointOnRoads = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            List<PointOnRoad> points = new ArrayList<>();
-            for (int id: roadPoints[i]) {
-                points.add(new PointOnRoad(id, xs.get(id), ys.get(id)));
+            pointOnRoads.clear();
+            for (int id: nodeRoads[i]) {
+                int x = points.get(id).x;
+                int y = points.get(id).y;
+                int limit = points.get(id).limit;
+                pointOnRoads.add(new PointOnRoad(id, x, y));
             }
-            
             if (roads[i].isVertical()) {
-                points.sort((a, b) -> a.y != b.y ? Integer.compare(a.y, b.y) : Integer.compare(a.id, b.id));
+                pointOnRoads.sort((p1, p2) -> Integer.compare(p1.y, p2.y));
             } else {
-                points.sort((a, b) -> a.x != b.x ? Integer.compare(a.x, b.x) : Integer.compare(a.id, b.id));
+                pointOnRoads.sort((p1, p2) -> Integer.compare(p1.x, p2.x));
             }
             
-            PointOnRoad prev = null;
-            for (PointOnRoad cur: points) {
-                if (prev != null && prev.id != cur.id) {
-                    addEdge(prev.id, cur.id);
+            int prev = pointOnRoads.get(0).id;
+            for (int j = 1; j < pointOnRoads.size(); j++) {
+                int cur = pointOnRoads.get(j).id;
+                if (prev != cur) {
+                    graph[prev].add(cur);
+                    graph[cur].add(prev);
                 }
                 prev = cur;
             }
         }
         
-        int start = cityNodes[0];
-        int[] best = new int[nodeCount];
-        Arrays.fill(best, -1);
+        int[] dist = new int[nodeCount];
+        Arrays.fill(dist, -1);
         
-        PriorityQueue<State> pq = new PriorityQueue<>();
-        best[start] = INF;
-        pq.offer(new State(start, INF));
+        PriorityQueue<Status> pq = new PriorityQueue<>();
+        int initId = cityNodes[0];
+        int initLimit = points.get(initId).limit;
+        dist[initId] = initLimit;
+        pq.offer(new Status(initId, initLimit));
         
         while (!pq.isEmpty()) {
-            State cur = pq.poll();
-            if (cur.limit < best[cur.node]) continue;
-            
-            for (int nextNode: graph[cur.node]) {
-                int nextLimit = Math.min(cur.limit, limits.get(nextNode));
-                if (nextLimit > best[nextNode]) {
-                    best[nextNode] = nextLimit;
-                    pq.offer(new State(nextNode, nextLimit));
-                }
+            Status cur = pq.poll();
+            if (cur.limit < dist[cur.id]) continue;
+            for (int nextId: graph[cur.id]) {
+                int nextLimit = points.get(nextId).limit;
+                nextLimit = Math.min(nextLimit, cur.limit);
+                if (dist[nextId] >= nextLimit) continue;
+                dist[nextId] = nextLimit;
+                pq.offer(new Status(nextId, nextLimit));
             }
         }
+
         
         int[] answer = new int[n - 1];
-        for (int i = 1; i < n; i++) {
-            answer[i - 1] = best[cityNodes[i]] == INF ? 0 : best[cityNodes[i]];
+        for (int i = 0; i < n - 1; i++) {
+            int nodeId = cityNodes[i + 1];
+            if (dist[nodeId] == INF) answer[i] = 0;
+            else answer[i] = dist[nodeId];
         }
         return answer;
     }
     
-    int getNode(int x, int y) {
+    int getNode(int x, int y, int limit) {
         long key = generateKey(x, y);
-        if (idMap.containsKey(key)) return idMap.get(key);
+        if (idMap.containsKey(key)) {
+            int id = idMap.get(key);
+            Point point = points.get(id);
+            // limits.set(id, Math.min(limit, limits.get(id)));
+            point.limit = Math.min(limit, point.limit);
+            return id;
+        }
         
-        int id = xs.size();
+        int id = points.size();
         idMap.put(key, id);
-        xs.add(x);
-        ys.add(y);
-        limits.add(INF);
+        points.add(new Point(x, y, limit));
         return id;
     }
     
     long generateKey(int x, int y) {
-        // int는 32비트, y가 음수면 상위 32비트까지 다 1로 채워지기 때문에 하위 32비트에 대해서만 and 연산 필요
-        return (((long) x) << 32) | (y & 0xffffffffL);
+        return (((long) x << 32)) | (y & 0xffffffffL);
     }
     
-    void updateCamera(int id, int limit) {
-        limits.set(id, Math.min(limits.get(id), limit));
-    }
-    
-    boolean onRoad(int x, int y, Road r) {
-        // Road를 입력받을 때 항상 x1 <= x2, y1 <= y2가 문제 조건
-        return r.x1 <= x && x <= r.x2 && r.y1 <= y && y <= r.y2;
-    }
-    
-    int[] getIntersect(Road a, Road b) {
-        if (a.isVertical() && !b.isVertical()) {
-            int x = a.x1;
-            int y = b.y1;
-            if (onRoad(x, y, a) && onRoad(x, y, b)) return new int[] {x, y};
-            return null;
+    int getIntersect(Road r1, Road r2) {
+        if (r1.isVertical() && !r2.isVertical()) {
+            if (r2.x1 <= r1.x1 && r1.x1 <= r2.x2 && r1.y1 <= r2.y1 && r2.y1 <= r1.y2) {
+                return getNode(r1.x1, r2.y1, INF);
+            }
+        } else if (!r1.isVertical() && r2.isVertical()) {
+            if (r1.x1 <= r2.x1 && r2.x1 <= r1.x2 && r2.y1 <= r1.y1 && r1.y1 <= r2.y2) {
+                return getNode(r2.x1, r1.y1, INF);
+            }
+        } else if (r1.isVertical() && r2.isVertical() && r1.x1 == r2.x1) {
+            if (r1.y2 == r2.y1) {
+                return getNode(r1.x1, r1.y2, INF);
+            }
+            if (r1.y1 == r2.y2) {
+                return getNode(r1.x1, r1.y1, INF);
+            }
+        } else if (!r1.isVertical() && !r2.isVertical() && r1.y1 == r2.y1) {
+            if (r1.x1 == r2.x2) {
+                return getNode(r1.x1, r1.y1, INF);
+            }
+            if (r1.x2 == r2.x1) {
+                return getNode(r1.x2, r1.y1, INF);
+            }
         }
-        
-        if (!a.isVertical() && b.isVertical()) {
-            int x = b.x1;
-            int y = a.y1;
-            if (onRoad(x, y, a) && onRoad(x, y, b)) return new int[] {x, y};
-            return null;
-        }
-        
-        if (a.isVertical() && b.isVertical()) {
-            if (a.x1 != b.x1) return null;
-            if (a.y2 == b.y1) return new int[] {a.x1, a.y2};
-            if (a.y1 == b.y2) return new int[] {a.x1, a.y1};
-            return null;
-        }
-        
-        if (a.y1 != b.y1) return null;
-        if (a.x1 == b.x2) return new int[] {a.x1, a.y1};
-        if (a.x2 == b.x1) return new int[] {a.x2, a.y1};
-        return null;
+        return -1;
     }
     
-    void addEdge(int a, int b) {
-        if (a == b) return;
-        graph[a].add(b);
-        graph[b].add(a);
+    boolean isOnRoad(Point point, Road road) {
+        int x = point.x;
+        int y = point.y;
+        return road.x1 <= x && x <= road.x2 && road.y1 <= y && y <= road.y2;
     }
 }
