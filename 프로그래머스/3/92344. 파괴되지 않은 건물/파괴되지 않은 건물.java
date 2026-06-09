24cf3a4d3c8e739675a083be5112b10232978386
kyeongchan <wkc1002@naver.com>
2026-06-10 07:12:12 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 29.97 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,45 +1,42 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0;
         int N = board.length;
         int M = board[0].length;
-        int[][] status = new int[N + 1][M + 1]; // 오른쪽 칸을 만들어주기 위해 M+1
-        for (int i = 0; i < skill.length; i++) {
-            int type = skill[i][0];
-            int startR = skill[i][1];
-            int startC = skill[i][2];
-            int endR = skill[i][3];
-            int endC = skill[i][4];
-            int degree = skill[i][5];
-            if (type == 1)
-                // 적의 공격
-                degree *= -1;
-            status[startR][startC] += degree;
-            status[startR][endC + 1] -= degree;
-            status[endR + 1][startC] -= degree;
-            status[endR + 1][endC + 1] += degree;
-        }
-        
-        int singleStatus = 0;
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                status[i][j] += status[i-1][j];
-            }
+        int[][] status = new int[N + 1][M + 1];
+        for (int[] s: skill) {
+            int type = s[0];
+            int r1 = s[1];
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            int degree = s[5];
+            if (type == 1) degree *= -1;
+            status[r1][c1] += degree;
+            status[r1][c2 + 1] -= degree;
+            status[r2 + 1][c1] -= degree;
+            status[r2 + 1][c2 + 1] += degree;
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 1; j < M; j++) {
-                status[i][j] += status[i][j-1];
+                status[i][j] += status[i][j - 1];
             }
         }
         
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (status[i][j] + board[i][j] > 0) answer++;
+        for (int j = 0; j < M; j++) {
+            for (int i = 1; i < N; i++) {
+                status[i][j] += status[i - 1][j];
             }
         }
+        int answer = 0;
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                board[i][j] += status[i][j];
+                if (board[i][j] >= 1) answer++;
+            }
+        }
+        
         return answer;
     }
 }
