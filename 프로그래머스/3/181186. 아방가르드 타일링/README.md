8d7b30ccb85bc6a9ed9db1aed87bd86fe7ee74ce
kyeongchan <wkc1002@naver.com>
2026-06-16 22:24:26 +0900

    [level 3] Title: 아방가르드 타일링, Time: 6.42 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/README.md
+++ 프로그래머스/3/181186. 아방가르드 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아방가르드 타일링 - 181186 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181186#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181186) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 4.03 ms
+메모리: 76 MB, 시간: 6.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 16일 21:17:54
+2026년 06월 16일 22:24:23
 
 ### 문제 설명
 
--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -3,14 +3,34 @@ class Solution {
     final int MOD = 1_000_000_007;
     
     public int solution(int n) {
-        long[] dp = new long[Math.max(n + 1, 7)];
-        dp[1] = 1; dp[2] = 3; dp[3] = 10;
-        dp[4] = 23; dp[5] = 62; dp[6] = 170;
+//         long[] dp = new long[Math.max(n + 1, 7)];
+//         dp[1] = 1; dp[2] = 3; dp[3] = 10;
+//         dp[4] = 23; dp[5] = 62; dp[6] = 170;
 
-        for (int i = 7; i <= n; i++) {
-            dp[i] = dp[i-1] + 2*dp[i-2] + 6*dp[i-3] + dp[i-4] - dp[i-6];
-            dp[i] = ((dp[i] % MOD) + MOD) % MOD;  // -dp[i-6] 때문에 음수 가능
+//         for (int i = 7; i <= n; i++) {
+//             dp[i] = dp[i-1] + 2*dp[i-2] + 6*dp[i-3] + dp[i-4] - dp[i-6];
+//             dp[i] = ((dp[i] % MOD) + MOD) % MOD;  // -dp[i-6] 때문에 음수 가능
+//         }
+//         return (int) dp[n];
+        long[] dp = new long[Math.max(n + 1, 4)];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 10;
+        
+        long[] h = new long[3];
+        
+        for (int i = 4; i <= n; i++) {
+            dp[i] += dp[i - 1] + 2 * dp[i - 2] + 5 * dp[i - 3];
+            dp[i] %= MOD;
+            
+            h[(i - 4) % 3] += dp[i - 4];
+            h[(i - 4) % 3] %= MOD;
+            
+            dp[i] += (h[0] + h[1] + h[2] + h[i % 3]) * 2;
+            dp[i] %= MOD;
         }
+        
         return (int) dp[n];
     }
 }
