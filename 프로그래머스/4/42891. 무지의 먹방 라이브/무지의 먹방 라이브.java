f4a0917e069bbd2694b5474a6aa6362480ff63ec
kyeongchan <wkc1002@naver.com>
2026-07-06 22:32:34 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 176.81 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -1,53 +1,64 @@
 import java.util.*;
 
-
 class Solution {
-    class Food implements Comparable<Food> {
-        int num;
-        int sec;
+    
+    static class Food implements Comparable<Food> {
+        int time, idx;
         
-        public Food(int num, int sec){
-            this.num = num;
-            this.sec = sec;
+        Food(int time, int idx) {
+            this.time = time;
+            this.idx = idx;
         }
         
         @Override
-        public int compareTo(Food f){
-            return this.sec == f.sec? Integer.compare(this.num, f.num) : Integer.compare(this.sec, f.sec);
+        public int compareTo(Food o) {
+            return time == o.time ? Integer.compare(idx, o.idx) : Integer.compare(time, o.time);
         }
-        
     }
+    
     public int solution(int[] food_times, long k) {
+        int answer = 0;
         PriorityQueue<Food> pq = new PriorityQueue<>();
-        long sumTime = 0;
         
-        for(int i=0;i<food_times.length;i++){
-            pq.offer(new Food(i + 1, food_times[i]));
+        long sumTime = 0;
+        int len = food_times.length;
+        
+        for (int i = 0; i < len; i++) {
+            pq.offer(new Food(food_times[i], i));
             sumTime += food_times[i];
         }
         
-        if(sumTime <= k){
-            return -1;
+        if (sumTime <= k) return -1;
+        
+        long prevTotalTime = 0;
+        long totalTime = 0;
+        int prevTime = 0;
+        
+        int lastIdx = -1;
+        while (!pq.isEmpty()) {
+            
+            Food cur = pq.poll();
+            totalTime += (cur.time - prevTime) * len;
+            lastIdx = cur.idx;
+            
+            if (totalTime > k) {
+                List<Integer> idxList = new ArrayList<>();
+                idxList.add(cur.idx);
+                while (!pq.isEmpty()) {
+                    idxList.add(pq.poll().idx);
+                }
+                Collections.sort(idxList);
+                
+                answer = idxList.get((int) (k - prevTotalTime) % len) + 1;
+                
+            }
+            
+            prevTime = cur.time;
+            prevTotalTime = totalTime;
+            len--;
         }
         
-        sumTime = 0;
-        long previous = 0; 
-        long length = food_times.length; 
-        
-        while(sumTime + ((pq.peek().sec - previous) * length) <= k){
-            int now = pq.poll().sec;
-            sumTime += (now - previous) * length;
-            previous = now;
-            length--;
-        }
-        
-        ArrayList<Food> result = new ArrayList<>();
-        while(!pq.isEmpty()){
-            result.add(pq.poll());
-        }
-        
-        result.sort(Comparator.comparing(f -> f.num));
-        
-        return result.get((int) ((k-sumTime) % length)).num;
+        if (answer == 0) answer = lastIdx + 1;
+        return answer;
     }
 }
