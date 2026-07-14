ed45d4b6f4e29a38ad297202fd9937a64363c2fe
kyeongchan <wkc1002@naver.com>
2026-07-14 23:14:42 +0900

    [level 3] Title: 제곱 개수 배열, Time: 32.39 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/3/468380. 제곱 개수 배열/제곱 개수 배열.java
+++ 프로그래머스/3/468380. 제곱 개수 배열/제곱 개수 배열.java
@@ -1,99 +1,101 @@
-import java.util.*;
-
 class Solution {
     
     int N;
     int[] arr;
-    long[] prefixSum, prefixLen;
+    long[] prefixLen, prefixSum;
     
     public long[] solution(int[] arr, long l, long r) {
-    
-        this.arr = arr;
         N = arr.length;
-        prefixSum = new long[N];
+        this.arr = arr;
+        
         prefixLen = new long[N];
+        prefixSum = new long[N];
         
-        
-        prefixSum[0] = (long) arr[0] * arr[0];
         prefixLen[0] = arr[0];
+        prefixSum[0] = (long)arr[0] * arr[0];
+        
         for (int i = 1; i < N; i++) {
-            prefixSum[i] = (long) arr[i] * arr[i] + prefixSum[i - 1];
-            prefixLen[i] = arr[i] + prefixLen[i - 1];
+            prefixLen[i] = prefixLen[i - 1] + arr[i];
+            prefixSum[i] = prefixSum[i - 1] + (long)arr[i] * arr[i];
         }
-        // System.out.println(Arrays.toString(prefixSum));
-        // System.out.println(Arrays.toString(prefixLen));
+        
         
         long K = genK(l, r);
-        
-        long C = 0L;
-        
-        long tempL = 1;
-        long tempR = r - l + 1;
-        
-        while (tempR <= prefixLen[N - 1]) {
-            int leftIdx = getIdx(tempL);
-            int rightIdx = getIdx(tempR);
-            long tempK = genK(tempL, tempR);
-            
-            long rightMove = prefixLen[rightIdx] - tempR + 1;
-            long leftMove = prefixLen[leftIdx] - tempL + 1;
-            long move = Math.min(leftMove, rightMove);
-            long diff = arr[rightIdx] - arr[leftIdx];
-            
-            if (diff == 0) {
-                if (tempK == K)
-                    C += move;
-            } else if ((K - tempK) % diff == 0) {
-                long mod = (K - tempK) / diff;
-                if (mod >= 0 && mod < move) C++;
-            }
-            tempL += move;
-            tempR += move;
-        }
+        // System.out.println("K: " + K);
+        long C = genC(K, r - l + 1);
         
         return new long[] {K, C};
     }
     
-    long genK(long l, long r) {
-        long rightSum = getSum(r);
-        
-        long leftSum = 0L;
-        if (l != 1) {
-            leftSum = getSum(l - 1);   
-        }
-        
-        return rightSum - leftSum;
-    }
-    
-    long getSum(long seq) {
-        // 처음부터 seq번째까지의 합 반환
-        int seqIdx = getIdx(seq);
-        
-        long sum = 0L;
-        if (seqIdx > 0) {
-            sum = prefixSum[seqIdx - 1];
-            sum += (seq - prefixLen[seqIdx - 1]) * arr[seqIdx];
-        } else {
-            sum = arr[0] * seq;
-        }
-        return sum;
-    }
-    
     int getIdx(long seq) {
-        // seq번째 값이 어느 인덱스에 있는지 반환
         int left = 0;
-        int right = N - 1;
-        int seqIdx = -1;
-        
+        int right = N;
+        int idx = N - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (prefixLen[mid] >= seq) {
-                seqIdx = mid;
+                idx = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return seqIdx;
+        return idx;
+    }
+    
+    long sumFromZero(long seq, int idx) {
+        // int idx = getIdx(seq);
+        if (idx == 0) return seq * arr[0];
+        
+        long result = prefixSum[idx - 1];
+        result += (seq - prefixLen[idx - 1]) * arr[idx];
+        return result;
+    }
+    
+    long genK(long l, long r) {
+        int lIdx = getIdx(l);
+        long lSum = sumFromZero(l-1, lIdx);
+        
+        int rIdx = getIdx(r);
+        long rSum = sumFromZero(r, rIdx);
+        
+        return rSum - lSum;
+    }
+    
+    long genC(long K, long len) {
+        long result = 0L;
+        
+        long lSeq = 1L;
+        long rSeq = len;
+        int lIdx = 0;
+        int rIdx = getIdx(rSeq);
+        
+        
+        while (rSeq <= prefixLen[N - 1]) {
+            long leftMove = prefixLen[lIdx] - lSeq + 1;
+            long rightMove = prefixLen[rIdx] - rSeq + 1;
+            long move = Math.min(leftMove, rightMove);
+            
+            long window = genK(lSeq, rSeq);
+            
+            if (arr[lIdx] == arr[rIdx]) {
+                if (window == K)
+                    result += move;
+            } else {
+                long diff = arr[rIdx] - arr[lIdx];
+                // window, window + diff, window + 2*diff +... + (move-1) * diff
+                long div = (K - window) / diff;
+                
+                long mod = (K - window) % diff;
+                if (div >= 0 && div < move && mod == 0) result++;
+            }
+            
+            lSeq += move;
+            rSeq += move;
+            if (lSeq > prefixLen[lIdx]) lIdx++;
+            if (rSeq > prefixLen[rIdx]) rIdx++;
+        }
+        
+        return result;
     }
 }
