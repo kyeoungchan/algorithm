37a72c75deea995ad2bd7a581a490060904ffd30
kyeongchan <wkc1002@naver.com>
2026-07-20 07:17:06 +0900

    [level 3] Title: 등산코스 정하기, Time: 86.44 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -2,99 +2,92 @@ import java.util.*;
 
 class Solution {
     
-    static class Path implements Comparable<Path> {
-        int num, cost;
+    static class Road implements Comparable<Road> {
+        int num, intensity;
         
-        Path(int num, int cost) {
+        Road(int num, int intensity) {
             this.num = num;
-            this.cost = cost;
+            this.intensity = intensity;
         }
         
         @Override
-        public int compareTo(Path o) {
-            return Integer.compare(cost, o.cost);
+        public int compareTo(Road o) {
+            return Integer.compare(intensity, o.intensity);
         }
     }
     
-    int n, INF = 123456789;
+    int INF = 123456789, n;
     int[] answer;
-    List<Path>[] graph;
     Set<Integer> gateSet, summitSet;
+    List<Road>[] graph;
     
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        this.n = n;
         gateSet = new HashSet<>();
+        summitSet = new HashSet<>();
+        
         for (int gate: gates) {
             gateSet.add(gate);
         }
         
-        summitSet = new HashSet<>();
         for (int summit: summits) {
             summitSet.add(summit);
         }
         
-        this.n = n;
         graph = new List[n + 1];
         for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
         
         for (int[] path: paths) {
-            int s = path[0];
-            int e = path[1];
+            int i = path[0];
+            int j = path[1];
             int w = path[2];
-            
-            if (gateSet.contains(s)) {
-                // s가 출입구
-                // s->e 단방향
-                if (!gateSet.contains(e)) {
-                    graph[s].add(new Path(e, w));
+            if (gateSet.contains(i)) {
+                // i가 gate라면
+                if (!gateSet.contains(j)) {
+                    // 단방향 그래프
+                    graph[i].add(new Road(j, w));
                 }
-            } else if (gateSet.contains(e)) {
-                // e가 출입구
-                // e->s 단방향
-                graph[e].add(new Path(s, w));
-            } else if (summitSet.contains(s)) {
-                // s가 산봉우리
-                if (!summitSet.contains(e)) {
-                    // e->s 단방향
-                    graph[e].add(new Path(s, w));
+            } else if (gateSet.contains(j)) {
+                graph[j].add(new Road(i, w));
+            } else if (summitSet.contains(i)) {
+                // i가 산봉우리라면
+                if (!summitSet.contains(j)) {
+                    graph[j].add(new Road(i, w));
                 }
-            } else if (summitSet.contains(e)) {
-                // e가 산봉우리
-                graph[s].add(new Path(e, w));
+            } else if (summitSet.contains(j)) {
+                graph[i].add(new Road(j, w));
             } else {
-                // 그 외 코스
-                graph[s].add(new Path(e, w));
-                graph[e].add(new Path(s, w));
+                graph[i].add(new Road(j, w));
+                graph[j].add(new Road(i, w));
             }
         }
         
-        
         answer = new int[] {-1, INF};
-        
         dijkstra();
-        
         return answer;
     }
     
     void dijkstra() {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, INF);
-        
-        PriorityQueue<Path> pq = new PriorityQueue<>();
+        PriorityQueue<Road> pq = new PriorityQueue<>();
+
         for (int gate: gateSet) {
             dist[gate] = 0;
-            pq.offer(new Path(gate, 0));    
+            pq.offer(new Road(gate, 0));
         }
         
-        
         while (!pq.isEmpty()) {
-            Path cur = pq.poll();
-            if (dist[cur.num] < cur.cost) continue;
+            Road cur = pq.poll();
             
-            for (Path next: graph[cur.num]) {
-                int nCost = Math.max(cur.cost, next.cost);
-                if (dist[next.num] <= nCost) continue;
-                dist[next.num] = nCost;
-                pq.offer(new Path(next.num, nCost));
+            if (dist[cur.num] < cur.intensity) continue;
+            
+            for (Road next: graph[cur.num]) {
+                int nIntensity = Math.max(cur.intensity, next.intensity);
+                if (dist[next.num] > nIntensity) {
+                    dist[next.num] = nIntensity;
+                    pq.offer(new Road(next.num, nIntensity));
+                }
             }
         }
         
@@ -103,10 +96,7 @@ class Solution {
                 answer[0] = summit;
                 answer[1] = dist[summit];
             } else if (dist[summit] == answer[1]) {
-                if (summit < answer[0]) {
-                    answer[0] = summit;
-                    answer[1] = dist[summit];
-                }
+                answer[0] = Math.min(answer[0], summit);
             }
         }
     }
