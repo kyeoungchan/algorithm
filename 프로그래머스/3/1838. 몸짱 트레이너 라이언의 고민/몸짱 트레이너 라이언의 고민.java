1d9059eb3f090363e93e99a765d78b1dea993759
kyeongchan <wkc1002@naver.com>
2026-07-20 06:55:12 +0900

    [level 3] Title: 몸짱 트레이너 라이언의 고민, Time: 21.01 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
+++ 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
@@ -2,50 +2,48 @@ import java.util.*;
 
 class Solution {
     
-    
-    int n, max, INF = 123456789;
-    boolean[][] used;
+    int n, INF = 123456789;
     
     public int solution(int n, int m, int[][] timetable) {
         this.n = n;
-        // 600~1,320 -> 0~720
-        int[] time = new int[722];
-        for (int[] tt: timetable) {
-            int start = tt[0] - 600;
-            int end = tt[1] - 600;
-            time[start]++;
-            time[end + 1]--;
+        // 600~1320 -> 0~720
+        int startTime = INF;
+        int endTime = 0;
+        int[] timeSum = new int[722];
+        for (int i = 0; i < m; i++) {
+            int t1 = timetable[i][0]-600;
+            int t2 = timetable[i][1]-600;
+            timeSum[t1]++;
+            timeSum[t2+1]--;
+            startTime = Math.min(startTime, t1);
+            endTime = Math.max(endTime, t2);
         }
-        
-        max = time[0];
-        for (int i = 1; i < 722; i++) {
-            time[i] += time[i - 1];
-            max = Math.max(max, time[i]);
+        int max = timeSum[startTime];
+        for (int i = startTime+1; i <= endTime; i++) {
+            timeSum[i] += timeSum[i-1];
+            max = Math.max(max, timeSum[i]);
         }
         if (max == 1) return 0;
+        // System.out.println(max);
         
-        List<Integer> list = new ArrayList<>();
-        for (int dist = 2 * (n-1); dist >= 1; dist--) {
+        List<Integer> poses = new ArrayList<>();
+        end: for (int dist = 2 * (n-1); dist > 0; dist--) {
             for (int sc = 0; sc < n; sc++) {
-                list.clear();
-                list.add(sc);
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        // System.out.println(list);
-                        if (i == 0 && j <= sc) continue;
-                        
-                        boolean flag = false;
-                        int pos = i * n + j;
-                        for (int pos2: list) {
-                            if (getDist(pos, pos2) < dist) {
-                                flag = true;
-                                break;
-                            }
+                poses.clear();
+                poses.add(sc);
+                for (int next = sc + 1; next < n * n; next++) {
+                    boolean flag = false;
+                    for (int prev: poses) {
+                        int d = getDist(prev, next);
+                        if (d < dist) {
+                            flag = true;
+                            break;
                         }
-                        
-                        if (!flag) {
-                            list.add(pos);
-                            if (list.size() >= max) return dist;
+                    }
+                    if (!flag) {
+                        poses.add(next);
+                        if (poses.size() == max) {
+                            return dist;
                         }
                     }
                 }
@@ -54,12 +52,7 @@ class Solution {
         return 1;
     }
     
-    
-    int getDist(int pos1, int pos2) {
-        int r1 = pos1/n;
-        int c1 = pos1%n;
-        int r2 = pos2/n;
-        int c2 = pos2%n;
-        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    int getDist(int pose1, int pose2) {
+        return Math.abs(pose1/n - pose2/n) + Math.abs(pose1%n - pose2 % n);
     }
 }
