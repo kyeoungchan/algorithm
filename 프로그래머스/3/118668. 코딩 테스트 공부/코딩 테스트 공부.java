d80a741f243c31548640c6177ebd0d66c548e9a4
kyeongchan <wkc1002@naver.com>
2026-07-21 08:50:11 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 10.10 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,42 +1,43 @@
 import java.util.*;
 
 class Solution {
+    
     int INF = 123456789;
+    
     public int solution(int alp, int cop, int[][] problems) {
-        
-        // Arrays.sort(problems, (p1, p2) -> p1[0] == p2[0] ? Integer.compare(p1[1], p2[1]) : Integer.compare(p1[0], p2[0]));
+        int answer = 0;
         int alpMax = 0;
         int copMax = 0;
         for (int[] problem: problems) {
             alpMax = Math.max(alpMax, problem[0]);
             copMax = Math.max(copMax, problem[1]);
         }
-        int[][] dp = new int[alpMax + 1][copMax + 1];
         
-        for (int i = 0; i <= alpMax; i++) Arrays.fill(dp[i], INF);
+        
+        if (alp >= alpMax && cop >= copMax) return 0;
+        int[][] dp = new int[Math.max(alp, alpMax) + 1][Math.max(cop, copMax) + 1];
+        
+        for (int i = 0; i <= Math.max(alp, alpMax); i++) Arrays.fill(dp[i], INF);
         
         int startAlp = Math.min(alp, alpMax);
         int startCop = Math.min(cop, copMax);
-        // dp[startAlp][startCop] = 0;
-        
+        dp[startAlp][startCop] = 0;
         for (int i = startAlp; i <= alpMax; i++) {
-            int studyCost = i-startAlp;
+            
             for (int j = startCop; j <= copMax; j++) {
-                dp[i][j] = Math.min(dp[i][j], studyCost++);
+                // if (i == alp && j == cop) continue;
+                if (i > startAlp) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j] + 1);
+                }
+                if (j > startCop) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j-1] + 1);
+                }
                 for (int[] problem: problems) {
-                    int alpReq = problem[0];
-                    int copReq = problem[1];
-                    int alpRwd = problem[2];
-                    int copRwd = problem[3];
-                    int problemCost = problem[4];
-                    if (i < alpReq || j < copReq) continue;
-                    int afterAlp = i + alpRwd;
-                    afterAlp = Math.min(afterAlp, alpMax);
-                    int afterCop = j + copRwd;
-                    afterCop = Math.min(afterCop, copMax);
+                    if (i < problem[0] || j < problem[1]) continue;
+                    int afterAlp = Math.min(alpMax, i + problem[2]);
+                    int afterCop = Math.min(copMax, j + problem[3]);
+                    dp[afterAlp][afterCop] = Math.min(dp[afterAlp][afterCop], dp[i][j] + problem[4]);
                     
-                    
-                    dp[afterAlp][afterCop] = Math.min(dp[afterAlp][afterCop], dp[i][j] + problemCost);
                 }
             }
         }
