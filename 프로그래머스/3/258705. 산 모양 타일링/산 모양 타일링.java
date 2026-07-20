909f91f150f51a21670aa33a7dca463811fbda4b
kyeongchan <wkc1002@naver.com>
2026-07-20 19:41:50 +0900

    [level 3] Title: 산 모양 타일링, Time: 8.63 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,19 +1,41 @@
 import java.util.*;
 
 class Solution {
+    
+    int MOD = 10007;
     public int solution(int n, int[] tops) {
-        int mod = 10_007;
-        int[][] dp = new int[n][2];
-        //[n][0]: 윗변이 n일 때, 제일 오른쪽에 마름모를 붙이지 않은 경우
-        //[n][1]: 윗변이 n일 때, 제일 오른쪽에 마름모를 붙인 경우
-        dp[0][0] = tops[0] == 1 ? 3 : 2;
-        dp[0][1] = 1;
-        for (int i = 1; i < n; i++) {
-            int mul1 = tops[i] == 1 ? 3 : 2;
-            int mul2 = tops[i] == 1 ? 2 : 1;
-            dp[i][0] = (dp[i-1][0] * mul1 + dp[i-1][1] * mul2) % mod;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % mod;
+        // 두번째 수는 오른쪽 아래가 잘린 경우의 수
+        int[][] dp = new int[n + 1][2];
+        if (tops[0] == 0) {
+            dp[1][0] = 3;
+            dp[1][1] = 2;
+        } else {
+            dp[1][0] = 4;
+            dp[1][1] = 3;
         }
-        return (dp[n-1][0] + dp[n-1][1]) % mod;
+        if (n == 1) {
+            return dp[1][0];
+        }
+        dp[0][0] = 1;
+        /*
+        top[n] = 1
+        dp[n][0] = dp[n-1][0] * 3  + dp[n-1][1];
+        dp[n][1] = dp[n-1][1] * 3 + 2 * dp[n-2][0];
+        
+        top[n] = 0
+        dp[n][0] = dp[n-1][0] * 2 + dp[n-1][1];
+        dp[n][1] = dp[n-1][1] * 2 + dp[n-2][0];
+        */
+        for (int i = 2; i <= n; i++) {
+            if (tops[i-1] == 1) {
+                dp[i][0] = (3 * dp[i-1][0] % MOD + dp[i-1][1]) % MOD;
+                dp[i][1] = (3 * dp[i-1][1] % MOD + 2 * dp[i-2][0] % MOD) % MOD;
+            } else {
+                dp[i][0] = (2 * dp[i-1][0] % MOD + dp[i-1][1]) % MOD;
+                dp[i][1] = (2 * dp[i-1][1] % MOD + dp[i-2][0]) % MOD;
+            }
+        }
+
+        return dp[n][0];
     }
 }
