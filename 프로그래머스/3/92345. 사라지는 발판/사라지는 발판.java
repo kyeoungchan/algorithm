60971bdc42b2fbbd6da3c64f84641374288c5833
kyeongchan <wkc1002@naver.com>
2026-07-22 06:42:44 +0900

    [level 3] Title: 사라지는 발판, Time: 7.79 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,74 +1,62 @@
-import java.util.*;
-
 class Solution {
     
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static int[][] map;
-    static int R, C;
-
     static class Result {
         boolean win;
-        int count;
-        Result(boolean win, int count) {
-            this.win = win;
-            this.count = count;
-        }
-    }
-
-    public int solution(int[][] board, int[] aloc, int[] bloc) {
-        map = board;
-        R = board.length;
-        C = board[0].length;
-        return dfs(aloc[0], aloc[1], bloc[0], bloc[1], 0).count;
-    }
-
-    private Result dfs(int ar, int ac, int br, int bc, int turn) {
-        // 현재 턴의 플레이어 위치가 발판이 사라진 경우 패배
-        boolean curTurnA = (turn % 2 == 0);
-        int r = curTurnA ? ar : br;
-        int c = curTurnA ? ac : bc;
+        int move;
         
-        if (map[r][c] == 0) return new Result(false, turn);
-
-        boolean canMove = false;
-        int minWin = Integer.MAX_VALUE;
-        int maxLose = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            if (!isValid(nr, nc) || map[nr][nc] == 0) continue;
-            canMove = true;
-
-            // 발판 소모 처리
-            map[r][c] = 0;
+        Result(boolean win, int move) {
+            this.win = win;
+            this.move = move;
+        }
+    }
+    
+    int n, m, INF = 123456789;
+    int[] dr = new int[] {-1, 0, 1, 0}, dc = new int[] {0, 1, 0, -1};
+    int[][] board;
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        n = board.length;
+        m = board[0].length;
+        this.board = board;
+        return dfs(aloc[0], aloc[1], bloc[0], bloc[1], 0).move;
+    }
+    
+    Result dfs(int ar, int ac, int br, int bc, int move) {
+        boolean isATurn = move % 2 == 0;
+        int r = isATurn ? ar : br;
+        int c = isATurn ? ac : bc;
+        if (board[r][c] == 0) {
+            return new Result(false, move);
+        }
+        boolean canWin = false;
+        int winMove = INF;
+        int loseMove = -1;
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m || board[nr][nc] == 0) continue;
+            board[r][c] = 0;
             Result next;
-            if (curTurnA) {
-                next = dfs(nr, nc, br, bc, turn + 1);
+            if (isATurn) {
+                next = dfs(nr, nc, br, bc, move + 1);
             } else {
-                next = dfs(ar, ac, nr, nc, turn + 1);
+                next = dfs(ar, ac, nr, nc, move + 1);
             }
-            // 원복
-            map[r][c] = 1;
-
-            // 미니맥스 로직
-            if (!next.win) { // 상대가 지면 내가 이김
-                minWin = Math.min(minWin, next.count);
-            } else { // 상대가 이기면 내가 짐
-                maxLose = Math.max(maxLose, next.count);
+            board[r][c] = 1;
+            if (!next.win) {
+                // 상대방이 졌다면 내가 이긴 경우
+                canWin = true;
+                winMove = Math.min(winMove, next.move);
+            } else {
+                // 상대방이 이겼다면 내가 진 경우
+                loseMove = Math.max(loseMove, next.move);
             }
         }
-
-        if (!canMove) return new Result(false, turn);
-        if (minWin != Integer.MAX_VALUE) return new Result(true, minWin);
-        return new Result(false, maxLose);
+        if (canWin) {
+            return new Result(true, winMove);
+        } else if (loseMove == -1) {
+            return new Result(false, move);
+        } else {
+            return new Result(false, loseMove);
+        }
     }
-
-    private boolean isValid(int r, int c) {
-        return r >= 0 && r < R && c >= 0 && c < C;
-    }
-    
-    
 }
