bb05e0aebdb81f605afbc969f070e33771539ba0
kyeongchan <wkc1002@naver.com>
2026-07-23 06:16:28 +0900

    [level 3] Title: 억억단을 외우자, Time: 180.52 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,30 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int e, int[] starts) {
-        int[] dp = new int[e+1];
-        // dp[1] = 1;
-        // 1부터 e까지 각 숫자의 약수 개수 구하기
+        int[] count = new int[e + 1];
         for (int i = 1; i <= e; i++) {
             for (int j = i; j <= e; j += i) {
-                dp[j]++;
+                count[j]++;
             }
         }
-        // System.out.println(Arrays.toString(dp));
-        int[] answerArr = new int[e + 1];
-        int maxCnt = 0;
-        int maxNum = 0;
+        int max = 0;
+        int num = 0;
+        int[] dp = new int[e + 1];
         for (int i = e; i >= 1; i--) {
-            if (dp[i] >= maxCnt) {
-                maxCnt = dp[i];
-                maxNum = i;
+            if (max <= count[i]) {
+                max = count[i];
+                num = i;
             }
-            answerArr[i] = maxNum;
+            dp[i] = num;
         }
-        
-        int[] answer = new int[starts.length];
-        for (int i = 0; i < starts.length; i++) {
-            answer[i] = answerArr[starts[i]];
+        int n = starts.length;
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = dp[starts[i]];
         }
         return answer;
     }
